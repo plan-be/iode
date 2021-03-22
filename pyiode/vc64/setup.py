b87b8d9d1b731b824b265fdee5250f42d8c8e8e0
Alix Damman <ald@plan.be>
2021-03-22 14:19:38 +0100

    pyiode: updated paths in /pyiode/vl64/setup.py

--- pyiode/vc64/setup.py
+++ pyiode/vc64/setup.py
@@ -6,14 +6,22 @@ except ImportError:
     from distutils.extension import Extension
 
 import numpy as np
+from pathlib import Path
+
+
+compiler = 'vc64'
+
+root_dir = Path(__file__).resolve().parents[2]
+api_dir = str(root_dir / 'api')
+scr4_dir = str(root_dir / 'scr4')
 
 iodemodule = Extension('iode',
-    include_dirs = [np.get_include(), 'c:\\usr\\iode_src\\api', 'c:\\usr\\scr4_src'],
-    library_dirs = ['c:\\usr\\iode_src\\api\\vc64', 'c:\\usr\\scr4_src\\vc64'],
+    include_dirs = [np.get_include(), api_dir, scr4_dir],
+    library_dirs = [f'{api_dir}/{compiler}', f'{scr4_dir}/{compiler}'],
     libraries = ['iodeapi', 's4iode', 'odbc32', 'odbccp32', 'ws2_32', 'gdi32', 'user32', 'advapi32', 'shell32', 'comdlg32', 'comctl32', 'Winspool', 'legacy_stdio_definitions'],
 	extra_compile_args=["-Zi", "/Od", "/DVC", "/DSCRPROTO", "/DREALD"],
 	extra_link_args=["-debug"],					
-    sources = ["..\\iode.pyx", "..\\sb_gui.c", "..\\s_iode.c", "..\\b_api.c"]
+    sources = ["../iode.pyx", "../sb_gui.c", "../s_iode.c", "../b_api.c"]
     #sources = ["iode.pyx"]
     )
                     
