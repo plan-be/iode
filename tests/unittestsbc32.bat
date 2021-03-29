c64cf9ba88deaa9e10a86cddb290b0c127f19744
Alix Damman <ald@plan.be>
2021-03-29 11:06:34 +0200

    - moved test files from /cmd directory to /tests directory - moved test file from /pyiode to /tests/pyiode - moved data test fies to a subdirectory /tests/data

---
+++ tests/unittestsbc32.bat
@@ -0,0 +1,26 @@
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\args.rep 10
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\Collectifs_inputs.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\datacmp.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\eviews.rep hermreg_5.txt
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\calc_ratio.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\example.bug.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\example.convert.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\example.csv.rep
+::iodecmd -y -alloclog test.log .\data\example.excel.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\example.foreach.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\example.foreach2.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\example.proc.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\hh25.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\hhidt.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\ldo.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\PROJECTION_HH_2013.rep
+::iodecmd -y -alloclog test.log .\data\resfun.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\sh_dec.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\test.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\test2.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\test3.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\test4.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\test5.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\testapi.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\testlong.rep
+..\cmd\bc32\iodecmd -y -alloclog test.log .\data\testrtf.rep                    
