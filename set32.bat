30a8a15b49a94c6bf1426c3eca85399404d6867a
Alix Damman <ald@plan.be>
2021-04-09 09:38:29 +0200

    fixed set32/64.bat files

--- set32.bat
+++ set32.bat
@@ -1,4 +1,4 @@
-#call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+:: call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" 
 set COMPILER=VC32
 title VS x86 (32 bits)
