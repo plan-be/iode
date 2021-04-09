eb60afe1956472453ac45ad699a9480fdd94fc50
Alix Damman <ald@plan.be>
2021-04-09 09:38:29 +0200

    fixed set32/64.bat files

--- set64.bat
+++ set64.bat
@@ -1,4 +1,4 @@
-# call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+:: call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" 
 set COMPILER=VC64
 Title VS x64 
