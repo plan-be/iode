81a5bd00364a7ee5c29e1e7942785c742dbf1ec5
Alix Damman <ald@plan.be>
2021-08-06 15:08:47 +0200

    updated path to iode.h in k_errorv.c and sb_gui.c

--- gui/legacy/k_errorv.c
+++ gui/legacy/k_errorv.c
@@ -1,7 +1,7 @@
 // copied/pasted from the /cmd directory
 
 #define _IODEVARG_
-#include <iode.h>
+#include "../../api/iode.h"
 
 kerror(level, st1, st2, st3, st4, st5, st6, st7, st8, st9, st10)
 int     level;
