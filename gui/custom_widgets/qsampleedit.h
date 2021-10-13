cc064a5869265cb5ce13dd4369d912b821c56e39
Alix Damman <ald@plan.be>
2021-10-13 08:41:48 +0200

    added custom widgets QIodeFileChooser and QIodeSampleEdit

---
+++ gui/custom_widgets/qsampleedit.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QObject>
+#include <QRegularExpression>
+#include <QRegularExpressionValidator>
+#include <QLineEdit>
+
+#include "../utils.h"
+
+
+class QIodeSampleEdit : public QLineEdit
+{
+	Q_OBJECT
+
+public:
+	QIodeSampleEdit(QWidget* parent = nullptr) : QLineEdit(parent)
+	{
+		this->setPlaceholderText("yyyyPppp");
+#if QT_CONFIG(tooltip)
+		this->setToolTip("-yyyy is the year\n"
+			"-P is the periodicity (Y=yearly, Q=quaterly, M=monthly, W=weekly)\n"
+			"-ppp is the period of the year");
+#endif
+		// https://doc.qt.io/qt-5/qregexp.html
+		QRegularExpression re("\\d{4}[YQMW]\\d{1,3}");
+		QValidator* validator = new QRegularExpressionValidator(re, this);
+		this->setValidator(validator);
+	}
+};
