6bf88ac381e6181deb1d8a326fe0d6f327462b7f
Jean-Marc Paul <jmp@plan.be>
2021-10-15 17:27:32 +0200

    k_ccscl.c:         - rewrite functions headers à la C++, full doc, scope static for internal functions         - replace KS_read_val() by K_read_real()

--- api/k_ccscl.c
+++ api/k_ccscl.c
@@ -1,12 +1,91 @@
+/**
+ * @header4iode
+ *
+ * Functions to load and save ascii definitions of IODE SCL objects.
+ *
+ *      KDB *KS_load_asc(char* filename)
+ *      int KS_save_asc(KDB* kdb, char* filename)
+ *      int KS_save_csv(KDB *kdb, char *filename)
+ *
+ */
+
 #include "iode.h"
 
-extern  IODE_REAL    KS_read_val();
-extern  IODE_REAL    K_read_real();
+/**
+ *  Reads on an open YY stream (file or string) the ascii definition of an IODE SCL and adds the new SCL to kdb. 
+ *  Subfn of KS_load_asc().
+ *  
+ *  Reads 3 values on yy. Each token must be a number (long or double). If not : 
+ *      the first value is replaced by 0.9 (value of the scalar)
+ *      the second value (relax) is replaced by 1.0
+ *      the 3d value (stderr) is replaced by L_NAN
+ *  
+ *  @param [in, out ]   kdb     KDB*    KDB of SCLs
+ *  @param [in, out]    yy      YYFILE* stream (file or string) to be read 
+ *  @param [in]         name    char*   SCL name
+ *  @return                     int     0 if the SCL is read and saved, -1 if it can't be created.
+ *  
+ */
 
-/* Read ascii file and add to DB */
+static int KS_read_scl(KDB* kdb, YYFILE* yy, char* name)
+{
+    int     i, keyw, pos;
+    SCL     scl;
 
-KDB *KS_load_asc(filename)
-char    *filename;
+    /* READ AT MOST 3 REALS */
+    scl.val = K_read_real(yy);
+    if(!L_ISAN(scl.val)) scl.val = 0.9;
+    scl.relax = K_read_real(yy);
+    if(!L_ISAN(scl.relax)) scl.relax = 1.0;
+    scl.std = K_read_real(yy);
+
+    /* CONTINUE READING UNTIL END OF VALUES */
+    while(1) {
+        keyw = YY_lex(yy);
+        if(keyw == YY_WORD || keyw == YY_EOF) break;
+    }
+    YY_unread(yy);
+
+    pos = K_add(kdb, name, &scl);
+    if(pos < 0) {
+        kerror(0, "%s : unable to create %s", YY_error(yy), name);
+        return(-1);
+    }
+
+    return(0);
+}
+
+
+/**
+ *  Loads SCLs from an ASCII file and saves the SCLs into a new KDB.
+ *  
+ *  Syntax of SCL ascii definitions 
+ *  -------------------------------
+ *      sclname [value [relax [stderr]]]
+ *  
+ *  Example
+ *  -------
+ *      acaf1 0.0157684069126844 1 0.00136871379800141 
+ *      acaf2 -7.9615019785706e-06 1 1.48283402268135e-06 
+ *      acaf3 2.50255966186523 1 0.873010456562042 
+ *      acaf4 -0.00850518420338631 1 0.00208330410532653 
+ *      dlnpaf 
+ *      dpuh_1 0.0182728692889214 
+ *  
+ *  Error messages are sent to the function kerror().
+ *  For each read CMT, kmsg() is called to send a message to the user. 
+ *  
+ *  The implementations of kerror() and kmsg() depend on the context.
+ *  
+ *  @param [in] filename    char*   name of the ascii file to be read or 
+ *                                  string containing the definition of the SCLs
+ *  @return                 KDB*    new KDB of SCL or NULL on error
+ *  
+ *  TODO: what if KS_read_cmt returns an error code ?
+ *  
+ */
+
+KDB *KS_load_asc(char* filename)
 {
     int     cmpt = 0;
     KDB     *kdb = 0;
@@ -51,46 +130,37 @@ err:
     return((KDB *)0);
 }
 
-KS_read_scl(kdb, yy, name)
-KDB   *kdb;
-YYFILE  *yy;
-char    *name;
+
+/**
+ *  Writes the definition of a SCL to a file. L_NAN are represented by na.
+ *  
+ *  @param [in] fd  pointer to a FILE
+ *  @param [in] scl pointer to the SCL
+ *  
+ */
+static void KS_print_scl(FILE* fd, SCL* scl)
 {
-    int     i, keyw, pos;
-    SCL     scl;
+    if(L_ISAN(scl->val)) fprintf(fd, "%.15lg ", (double)(scl->val)); /* JMP 09-04-98 */
+    else fprintf(fd, "na ");
 
-    /* READ AT MOST 3 REALS */
-    scl.val = KS_read_val(yy);
-    if(!L_ISAN(scl.val)) scl.val = 0.9;
-    scl.relax = KS_read_val(yy);
-    if(!L_ISAN(scl.relax)) scl.relax = 1.0;
-    scl.std = KS_read_val(yy);
+    if(L_ISAN(scl->relax)) fprintf(fd, "%.15lg ", (double)(scl->relax)); /* JMP 09-04-98 */
+    else fprintf(fd, "na ");
 
-    /* CONTINUE READING UNTIL END OF VALUES */
-    while(1) {
-        keyw = YY_lex(yy);
-        if(keyw == YY_WORD || keyw == YY_EOF) break;
-    }
-    YY_unread(yy);
-
-    pos = K_add(kdb, name, &scl);
-    if(pos < 0) {
-        kerror(0, "%s : unable to create %s", YY_error(yy), name);
-        return(-1);
-    }
-
-    return(0);
+    if(L_ISAN(scl->std)) fprintf(fd, "%.15lg ", (double)(scl->std));     /* JMP 09-04-98 */
+    else fprintf(fd, "na ");
 }
 
-IODE_REAL KS_read_val(yy)
-YYFILE  *yy;
-{
-    return(K_read_real(yy));
-}
-
-KS_save_asc(kdb, filename)
-KDB   *kdb;
-char  *filename;
+/**
+ *  Saves a KDB of SCLs into an ascii file (.as) or to the stdout.
+ *  
+ *  @see KS_load_asc() for the syntax. 
+ *  
+ *  @param [in] kdb         KDB*    KDB of SCLs
+ *  @param [in] filename    char*   name of the output file or "-" to write the result on the stdout.
+ *  @return                 int     0 on success, -1 if the file cannot be written.
+ *  
+ */
+int KS_save_asc(KDB* kdb, char* filename)
 {
     FILE    *fd;
     int     i, j;
@@ -117,22 +187,10 @@ char  *filename;
 }
 
 
-KS_print_scl(fd, scl)
-FILE    *fd;
-SCL     *scl;
-{
-    if(L_ISAN(scl->val)) fprintf(fd, "%.15lg ", (double)(scl->val)); /* JMP 09-04-98 */
-    else fprintf(fd, "na ");
-
-    if(L_ISAN(scl->relax)) fprintf(fd, "%.15lg ", (double)(scl->relax)); /* JMP 09-04-98 */
-    else fprintf(fd, "na ");
-
-    if(L_ISAN(scl->std)) fprintf(fd, "%.15lg ", (double)(scl->std));     /* JMP 09-04-98 */
-    else fprintf(fd, "na ");
-
-    return(0);
-}
-
+/* 
+ * Save a KDB of SCL in a .csv file.
+ * NOT IMPLEMENTED.
+ */
 
 int KS_save_csv(KDB *kdb, char *filename)
 {
