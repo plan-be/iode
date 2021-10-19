a99d40fd8d2348b6730943b77018bbf927f0f39e
Alix Damman <ald@plan.be>
2021-10-19 15:18:08 +0200

    prepared Iode object tables to be editable (the method setDataCcell() must be implemented in the sub-classes of IODEAbstractTableModel)

--- gui/tabs/abstract_table_model.h
+++ gui/tabs/abstract_table_model.h
@@ -15,6 +15,11 @@
  * Because of how Qt is implemented, it is NOT possible for a template class
  * to define/override signals and slots (using Q_OBJECT)
  * ( see https://doc.qt.io/qt-5/moc.html#limitations )
+ * 
+ * Read-only model: https://doc.qt.io/qt-6/model-view-programming.html#read-only-access
+ * Editable model:  https://doc.qt.io/qt-6/model-view-programming.html#editable-items
+ * Resizable model: https://doc.qt.io/qt-6/model-view-programming.html#resizable-models
+ * 
  */
 
 
@@ -41,18 +46,12 @@ public:
 		return columnNames.size();
 	}
 
-	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const
+	Qt::ItemFlags flags(const QModelIndex& index) const
 	{
 		if (!index.isValid())
-			return QVariant();
+			return Qt::ItemIsEnabled;
 
-		if (role == Qt::TextAlignmentRole)
-			return int(Qt::AlignLeft);
-
-		if (role == Qt::DisplayRole)
-			return dataCell(index.row(), index.column());
-		else
-			return QVariant();
+		return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
 	}
 
 	QVariant headerData(int section, Qt::Orientation orientation, int role) const
@@ -66,6 +65,42 @@ public:
 		return QVariant("  ");
 	}
 
+	QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const
+	{
+		if (!index.isValid())
+			return QVariant();
+
+		if (role == Qt::TextAlignmentRole)
+			return int(Qt::AlignLeft);
+
+		if (role == Qt::DisplayRole || role == Qt::EditRole)
+			return dataCell(index.row(), index.column());
+		else
+			return QVariant();
+	}
+
+	bool setData(const QModelIndex& index, const QVariant& value, int role)
+	{
+		if (index.isValid() && role == Qt::EditRole) 
+		{
+
+			bool success = setDataCcell(index.row(), index.column(), value);
+			if (success)
+			{
+				emit dataChanged(index, index, { role });
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+
 protected:
 	void resetModel()
 	{
@@ -74,4 +109,5 @@ protected:
 	}
 
 	virtual QVariant dataCell(const int row, const int col) const = 0;
+	bool setDataCcell(const int row, const int column, const QVariant& value) { return false; }
 };
