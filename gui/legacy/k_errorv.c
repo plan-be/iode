650729e51cd9873cad9d26757c6dd39d446fbbf4
Alix Damman <ald@plan.be>
2021-12-16 09:04:22 +0100

    k_errorv.c: updated functions signatures (because their signatures have changed in the new Iode API)

--- gui/legacy/k_errorv.c
+++ gui/legacy/k_errorv.c
@@ -3,6 +3,7 @@
 #define _IODEVARG_
 #include "iode.h"
 
+/*
 kerror(level, st1, st2, st3, st4, st5, st6, st7, st8, st9, st10)
 int     level;
 char* st1, * st2, * st3, * st4, * st5, * st6, * st7, * st8, * st9, * st10;
@@ -10,10 +11,23 @@ char* st1, * st2, * st3, * st4, * st5, * st6, * st7, * st8, * st9, * st10;
     printf(st1, st2, st3, st4, st5, st6, st7, st8, st9, st10);
     printf("\n");
 }
+*/
 
+void kerror(int level, char* fmt, ...)
+{
+    return;
+}
+
+/*
 kmsg(st1, st2, st3, st4, st5, st6, st7, st8, st9, st10)
 char* st1, * st2, * st3, * st4, * st5, * st6, * st7, * st8, * st9, * st10;
 {
     printf(st1, st2, st3, st4, st5, st6, st7, st8, st9, st10);
     printf("\n");
+}
+*/
+
+void kmsg(char* fmt, ...)
+{
+    return;
 }
