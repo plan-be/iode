5b8762ec9e2a26f4cfcb8a846e9412b9ec6734ce
Jean-Marc Paul <jmp@plan.be>
2022-01-20 11:18:01 +0100

    s_iode.c: new file to cancel the s4issrv (FILE * over TCP/IP) interface

---
+++ scr4/s_iode.c
@@ -0,0 +1,50 @@
+/**
+ *  Replacement functions for IODE implementation. 
+ *  These fns are generally a simplified but adequate version of the full fledge scr4 implementation.
+ *  
+ *  
+ */
+
+#include "scr4.h"
+
+FILE *ISC_fopen(char *filename, char *mode)
+{
+    return(fopen(filename, mode));
+}
+
+int ISC_fclose(FILE *fd)
+{
+    return(fclose(fd));
+}
+
+ISC_unlink(char *filename)
+{
+    return(unlink(filename));
+}
+ISC_rename(char *oldname, char *newname)
+{
+    return(rename(oldname, newname));
+}
+
+ISC_read_line(fd, buf, lg)
+FILE            *fd;
+unsigned char   *buf;
+int             lg;
+{
+    return(SCR_read_line(fd, buf, lg));
+}
+
+ISC_fwrite(char *buf, int lg, int nb, FILE *fd)
+{
+    return(fwrite(buf, lg, nb, fd));
+}
+
+ISC_getc(FILE *fd)
+{
+    return(getc(fd));
+}
+
+ISC_putc(int ch, FILE *fd)
+{
+    return(putc(ch, fd));
+}
