6972e4d61eeec4af757e85809bc9191b9ff7190e
Alix Damman <ald@plan.be>
2022-01-25 20:23:08 +0100

    - utils.h: replaced #elif by #else - test_utils.cpp: added special chars (>127) in Utf8_oem_convertion() unittest

--- tests/test_cpp_api/test_utils.cpp
+++ tests/test_cpp_api/test_utils.cpp
@@ -4,7 +4,7 @@
 
 TEST(TestUtils, Utf8_oem_convertion)
 {
-	std::string str_utf8 = u8"aA!@^$jndq256";
+	std::string str_utf8 = u8"aA!@^$jndq256ιοµηθ";
 
 	std::string str_oem = convert_utf8_to_oem(str_utf8);
 	std::string str_res = convert_oem_to_utf8(str_oem);
