db269c5e6a67d2682405d0da68d11b91dd5c5388
Alix Damman <ald@plan.be>
2022-01-25 14:35:47 +0100

    changed order of arguments of the Period constructor : Period(year, periodicity, position) + test their validity

--- tests/test_cpp_api/test_sample.cpp
+++ tests/test_cpp_api/test_sample.cpp
@@ -5,8 +5,8 @@
 TEST(TestSample, Create) 
 {
 	// passing 2 Period instances
-	Period start(2015, 1, 'Y');
-	Period end(2020, 1, 'Y');
+	Period start(2015, 'Y', 1);
+	Period end(2020, 'Y', 1);
 	Sample sample(start, end);
 
 	// passing 2 strings
@@ -15,55 +15,40 @@ TEST(TestSample, Create)
 	Sample sample2(str_start, str_end);
 
 	// passing 2 C PERIOD structures
-	PERIOD c_start = start.to_c_period();
-	PERIOD c_end = end.to_c_period();
-	Sample sample3(&c_start, &c_end);
+	PERIOD* c_start = start.c_period;
+	PERIOD* c_end = end.c_period;
+	Sample sample3(c_start, c_end);
 
 	// passing a C SAMPLE structure
-	SAMPLE* c_sample = PER_pertosmpl(&c_start, &c_end);
+	SAMPLE* c_sample = PER_pertosmpl(c_start, c_end);
 	Sample sample4(c_sample);
-}
-
-TEST(TestSample, To_c_sample) 
-{
-	Period start(2015, 1, 'Y');
-	Period end(2020, 1, 'Y');
-	Sample sample(start, end);
-
-	SAMPLE c_sample = sample.to_c_sample();
-
-	PERIOD c_start = start.to_c_period();
-	PERIOD c_end = end.to_c_period();
-	EXPECT_EQ(Period(&c_sample.s_p1), start);
-	EXPECT_EQ(Period(&c_sample.s_p2), end);
-	EXPECT_EQ(c_sample.s_nb, sample.nb_periods);
 }
 
 TEST(TestSample, Intersection) 
 {
-	Period start1(2015, 1, 'M');
-	Period end1(2020, 6, 'M');
+	Period start1(2015, 'M', 1);
+	Period end1(2020, 'M', 6);
 	Sample sample1(start1, end1);
 
-	Period start2(2017, 2, 'M');
-	Period end2(2022, 1, 'M');
+	Period start2(2017, 'M', 2);
+	Period end2(2022, 'M', 1);
 	Sample sample2(start2, end2);
 
 	Sample sample_intersection = sample1.intersection(sample2);
 
-	Period inter_start_period(2017, 2, 'M');
-	Period inter_end_period(2020, 6, 'M');
+	Period inter_start_period(2017, 'M', 2);
+	Period inter_end_period(2020, 'M', 6);
 	int nb_periods = inter_end_period.difference(inter_start_period) + 1;
 
-	EXPECT_EQ(*sample_intersection.start_period, inter_start_period);
-	EXPECT_EQ(*sample_intersection.end_period, inter_end_period);
-	EXPECT_EQ(sample_intersection.nb_periods, nb_periods);
+	EXPECT_EQ(sample_intersection.start_period(), inter_start_period);
+	EXPECT_EQ(sample_intersection.end_period(), inter_end_period);
+	EXPECT_EQ(sample_intersection.nb_periods(), nb_periods);
 }
 
 TEST(TestSample, ToString)
 {
-	Period start(2015, 1, 'Y');
-	Period end(2020, 1, 'Y');
+	Period start(2015, 'Y', 1);
+	Period end(2020, 'Y', 1);
 	Sample sample(start, end);
 
 	std::string str_sample = sample.to_string();
