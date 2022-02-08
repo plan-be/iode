9ad2dbbe01cd9745b80bae4dedfa494880089013
Alix Damman <ald@plan.be>
2022-02-08 10:18:20 +0100

    tests/test_cpp_api: added test for function check_filepath()

--- tests/test_cpp_api/test_utils.cpp
+++ tests/test_cpp_api/test_utils.cpp
@@ -1,5 +1,16 @@
 #include "pch.h"
 
+
+class KDBUtilsTest : public KDBTest, public ::testing::Test
+{
+protected:
+
+	void SetUp() override {}
+
+	// void TearDown() override {}
+};
+
+
 TEST(TestUtils, Utf8_oem_convertion)
 {
 	std::string str_utf8 = u8"aA!@^$jndq256ιοµηθ";
@@ -8,4 +19,31 @@ TEST(TestUtils, Utf8_oem_convertion)
 	std::string str_res = convert_oem_to_utf8(str_oem);
 
 	EXPECT_EQ(str_res, str_utf8);
+}
+
+
+TEST_F(KDBUtilsTest, checkFilepath)
+{
+	std::string filepath;
+
+	// fake directory
+	filepath = "C:\\Users\\ald\\wrong\\path\\fun.cmt";
+	EXPECT_THROW(check_filepath(filepath, I_COMMENTS, "save", false), std::runtime_error);
+
+	// wrong extension
+	filepath = output_test_dir + "fun.eqs";
+	EXPECT_THROW(check_filepath(filepath, I_COMMENTS, "save", false), std::runtime_error);
+
+	// file does not exist
+	filepath = input_test_dir + "fun2.cmt";
+	EXPECT_THROW(check_filepath(filepath, I_COMMENTS, "load", true), std::runtime_error);
+
+	// file does not exist (no extension given)
+	filepath = input_test_dir + "fun2";
+	EXPECT_THROW(check_filepath(filepath, I_COMMENTS, "load", true), std::runtime_error);
+
+	// extension added automatically
+	filepath = input_test_dir + "fun";
+	filepath = check_filepath(filepath, I_COMMENTS, "load", true);
+	EXPECT_EQ(filepath, input_test_dir + "fun.cmt");
 }
