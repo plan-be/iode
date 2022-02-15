b6fb5867f6bc07963054a989ab15ba9ddc125676
Alix Damman <ald@plan.be>
2022-02-15 10:37:20 +0100

    tests/test_cpp_api: updated test functions for KDBTables

--- tests/test_cpp_api/test_kdb_tables.cpp
+++ tests/test_cpp_api/test_kdb_tables.cpp
@@ -31,6 +31,17 @@ TEST_F(KDBTablesTest, Save)
     kdb.save(output_test_dir + "fun.at");
 }
 
+TEST_F(KDBTablesTest, Get)
+{
+    int pos = K_find(K_WS[I_TABLES], "GFRPC");
+
+    // by position
+    Table table = kdb.get(pos);
+
+    // by name
+    Table table2 = kdb.get("GFRPC");
+}
+
 TEST_F(KDBTablesTest, GetTitle)
 {
     int pos = 0;
@@ -45,4 +56,33 @@ TEST_F(KDBTablesTest, GetTitle)
     std::string name = kdb.getName(pos);
     title = kdb.getTitle(name);
     EXPECT_EQ(expected_title, title);
+}
+
+TEST_F(KDBTablesTest, AddRemove)
+{
+    KDBVariables kdb_var;
+    kdb_var.load(input_test_dir + "fun.var");
+
+    // add empty table with 2 columns
+    Table table1 = kdb.add_table("table1", 2);
+
+    // add tables and initialize it
+    std::string def = "A title";
+    std::vector<std::string> vars = { "GOSG", "YDTG", "DTH", "DTF", "IT", "YSSG+COTRES", "RIDG", "OCUG" };
+    bool mode = true;
+    bool files = true;
+    bool date = true;
+    Table table2 = kdb.add_table("table2", 2, def, vars, mode, files, date);
+
+    // remove added tables
+    kdb.remove_table("table1");
+    kdb.remove_table("table2");
+}
+
+TEST_F(KDBTablesTest, Copy)
+{
+    Table original_table = kdb.get("GFRPC");
+
+    Table copy_table = kdb.copy("GFRPC_COPY", "GFRPC");
+    EXPECT_EQ(copy_table, original_table);
 }
