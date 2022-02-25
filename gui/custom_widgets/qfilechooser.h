5cd6be654dab1522a280bd01dd9ca9afd4b8e4aa
Alix Damman <ald@plan.be>
2022-02-25 16:26:09 +0100

    GUI: removed the no longer needed QIodeFileChooser.verify() method -> the verification of filepaths has been delegated to C++ API (check_filepath() function)

--- gui/custom_widgets/qfilechooser.h
+++ gui/custom_widgets/qfilechooser.h
@@ -72,40 +72,6 @@ public:
 	QString getFilepath() { return lineEdit->text(); }
 	void setFilepath(const QString& filepath) { lineEdit->setText(filepath); }
 
-	void verify(const QString& name, const EnumItemType& type)
-	{
-		QString error = QString("ERROR in field %1: ").arg(name);
-		QString file = lineEdit->text();
-		// check if empty
-		if (file.isEmpty())
-		{
-			if (type == OPTIONAL_FIELD) return;
-			throw std::runtime_error(error.toStdString() + "No file provided");
-		}
-
-		QFileInfo fileInfo = QFileInfo(file);
-		// check if file is not a directory
-		if (fileInfo.isDir())  throw std::runtime_error((error + QString("The filepath %1 represents a directory, not a file").arg(file)).toStdString());
-		// check if correct/must provide extension
-		QString extension = fileInfo.suffix();
-		QString ext = QString::fromStdString(vFileExtensions[fileType].ext);
-		if (extension.isEmpty())
-		{
-			if (ext == anyExt) throw std::runtime_error(error.toStdString() + "You must provide a file extension");
-			fileInfo = QFileInfo(file + ext);
-		}
-		else
-		{
-			QString ascii = QString::fromStdString(vFileExtensions[fileType].ascii);
-			if (ext != anyExt && extension != ext && extension != ascii)
-				throw std::runtime_error((error + QString("Wrong file extension. Expected %1 or %2 but got %3")
-					.arg(ext).arg(ascii).arg(extension)).toStdString());
-		}
-		// check if file exists
-		if (fileMode == EXISTING_FILE && !fileInfo.exists())
-			throw std::runtime_error((error + QString("The file %1 does not exist !").arg(file)).toStdString());
-	}
-
 public slots:
 	void updateFileType(const EnumIodeFile& fileType)
 	{
