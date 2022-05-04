9099a64eaf03c96806f924cadb503ea714079041
Alix Damman <ald@plan.be>
2022-05-04 14:51:55 +0200

    C++ API: overloaded add() and update() methods of KDBScalars

--- cpp_api/KDB/kdb_scalars.cpp
+++ cpp_api/KDB/kdb_scalars.cpp
@@ -12,6 +12,24 @@ void KDBScalars::add_or_update(const std::string& name, const Scalar& scalar)
 	if (pos == -2) throw std::runtime_error("Cannot create or update table with name " + name);
 }
 
+void KDBScalars::add(const std::string& name, const IODE_REAL value, const IODE_REAL relax)
+{
+	Scalar scalar;
+	scalar.val = value;
+	if (relax < 0.0 || relax > 1.0) throw std::runtime_error("Value for the parameter relax must be in range [0, 1].");
+	scalar.relax = relax;
+	KDBAbstract::add(name, scalar);
+}
+
+void KDBScalars::update(const std::string& name, const IODE_REAL value, const IODE_REAL relax)
+{
+	Scalar scalar;
+	scalar.val = value;
+	if (relax < 0.0 || relax > 1.0) throw std::runtime_error("Value for the parameter relax must be in range [0, 1].");
+	scalar.relax = relax;
+	KDBAbstract::update(name, scalar);
+}
+
 Scalar KDBScalars::copy_obj(const Scalar& original) const
 {
 	Scalar scalar_copy;
