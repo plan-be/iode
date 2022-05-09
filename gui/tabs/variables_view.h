7500ee44ffdf91b0f0a8a3e8e686c72a3aa88a78
Alix Damman <ald@plan.be>
2022-05-09 15:29:23 +0200

    GUI: updated selection in VariablesView

--- gui/tabs/variables_view.h
+++ gui/tabs/variables_view.h
@@ -15,8 +15,27 @@ class VariablesView : public AbstractTableView<VariablesModel>
 {
 	Q_OBJECT
 
+protected:
+	void selectionChanged(const QItemSelection& selected, const QItemSelection& deselected) override;
+
 public:
-	VariablesView(QWidget* parent = nullptr) : AbstractTableView(I_VARIABLES, new VariablesDelegate(parent), parent) {};
+	VariablesView(QWidget* parent = nullptr) : AbstractTableView(I_VARIABLES, new VariablesDelegate(parent), parent) 
+	{
+		// ---- Selection ----
+		// See: - https://doc.qt.io/qt-5/model-view-programming.html#handling-selections-in-item-views
+		//      - https://doc.qt.io/qt-5/model-view-programming.html#selections
+		
+		// -- Behavior --
+		// Selecting single items.
+		// See: https://doc.qt.io/qt-5/qabstractitemview.html#SelectionBehavior-enum
+		setSelectionBehavior(QTableView::SelectItems);
+		// -- Mode --
+		// When the user selects an item in the usual way, the selection is cleared and the new item selected. 
+		// However, if the user presses the Shift key while clicking on an item, all items between the current 
+		// item and the clicked item are selected or unselected, depending on the state of the clicked item.
+		// See: https://doc.qt.io/qt-5/qabstractitemview.html#SelectionMode-enum
+		setSelectionMode(QTableView::ContiguousSelection);
+	};
 
 public slots:
 	void new_variable();
