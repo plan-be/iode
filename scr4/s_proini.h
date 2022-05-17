890eba5e55108a3bfb3531d6415f2bf699c0b921
Jean-Marc Paul <jmp@plan.be>
2022-05-17 16:17:44 +0200

    scr4/s_proini.h:     - added prototypes of         int IniReadYNParm(char* filename, char* section, char* parm);         int IniWriteParm(char* filename, char* section, char* parm, char* val);

--- scr4/s_proini.h
+++ scr4/s_proini.h
@@ -11,11 +11,13 @@ extern int IniSetData(unsigned char **adr,unsigned char *txt,int ucase);
 extern int IniSetParam(INIFILE *inif,unsigned char *txt);
 extern int IniSetSection(INIFILE *inif,unsigned char *txt);
 extern INIFILE *IniOpenSection(unsigned char *filename,unsigned char *section,int usection,int uparam);
+extern int IniReadYNParm(char* filename, char* section, char* parm);
 extern unsigned char **IniReadSection(char *filename,char *section);
 extern unsigned char **IniReadCurSection(INIFILE *inif);
 extern unsigned char *IniError(INIFILE *inif);
 extern int IniReadTxtParm(unsigned char *filename,unsigned char *section,unsigned char *parm,unsigned char *val,int lg);
 extern int IniReadNumParm(unsigned char *filename,unsigned char *section,unsigned char *parm,int *num);
+extern int IniWriteParm(char* filename, char* section, char* parm, char* val);
 extern unsigned char **IniReadAllSections(unsigned char *filename);
 
 #ifdef SCRCPP
