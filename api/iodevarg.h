6f2d1e9ffee12cc562759b260237460ee90dcaa1
Jean-Marc Paul <jmp@plan.be>
2022-06-15 17:28:12 +0200

    api/iodevarg.h: deleted declarations of L_debug() and K_add()

--- api/iodevarg.h
+++ api/iodevarg.h
@@ -9,23 +9,16 @@ extern "C" {
 //extern void kerror(int level, char* msg, ...);
 //extern void kmsg(char *msg, ...);
 
-/* l_debug.c */
-extern void L_debug(char *,...);
 
 /* e_errorv.c */
-extern void E_msg(char *,...);
-extern void E_error(char *,...);
+//extern void E_msg(char *,...);
+//extern void E_error(char *,...);
 
 /* b_basev.c */
 //extern void B_seterror(char *,...);
 //extern void B_seterrn(int ,...);
 
 
-/* w_wrtv.c  */
-extern int W_printf(char *c1,...);
-
-/* objsv.c */
-extern int K_add(KDB *kdb, ...);
 	
 /* o_miscv.c */
 extern int ODE_error(int ,...);
