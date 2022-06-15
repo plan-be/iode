80dc79a447e8f76c702de832e5154c40c9a7e3d1
Jean-Marc Paul <jmp@plan.be>
2022-06-15 17:26:16 +0200

    api/l_debug.c:     - rewriting L_debug() with va_arg. See comments in source file.     - new variable L_DEBUG_FILENAME (default simul.dbg)

--- api/l_debug.c
+++ api/l_debug.c
@@ -1,32 +1,61 @@
 /**
  *  @header4iode
  *
- *  Function called by Newton-Raphson sub iteration.
+ *  Function called by the Newton-Raphson sub-iteration and by the step wise algorithm 
+ *  to save debugging info in a text file.
+ *
+ *  Function
+ *  --------
+ *      void L_debug(char* fmt, ...) : appends a message to the file L_DEBUG_FILENAME ("simul.dbg" by default).
+ *
+ *  Global variable
+ *  ---------------
+ *      char* L_DEBUG_FILENAME = 0;  :  name of the output file (default "simul.dbg")
  *  
- *      void L_debug(char* txt, char* a1, char* a2, char* a3, char* a4, char* a5, char* a6)  Saves a message in simul.dbg.
- *  
- *  TODO: replace by a "super" function and replace "simul.dbg" by a global variable
+ *  TODO: replace by a "super" function 
  */
 
 #include <stdio.h>
+#include <stdarg.h>
+
+char    *L_DEBUG_FILENAME = 0;  // Name of the output file
 
 /**
- *  Saves a message in simul.dbg.
+ *  Appends a message to the file L_DEBUG_FILENAME ("simul.dbg" by default).
  *  
- *  @param [in] char*   txt  format of the message (1st fprintf parameter)
- *  @param [in] char*   a1   param 1
- *  @param [in] char*   a2   param 2
- *  @param [in] char*   a4   param 3
- *  @param [in] char*   a5   param 4
- *  @param [in] char*   a6   param 5
- *  @param [in] char*   a6   param 6 
+ *  @param [in] char*   fmt  format of the message (1st fprintf parameter)
+ *  @param [in] ...          arbitrary number of args (depends on fmt contents)
  */
-void L_debug(char* txt, char* a1, char* a2, char* a3, char* a4, char* a5, char* a6)
+void L_debug(char* fmt,...)
 {
-    FILE *fd = 0;
-
-    fd = fopen("simul.dbg", "a+");
-    if(fd) fprintf(fd, txt, a1, a2, a3, a4, a5, a6);
-    fclose(fd);
+    char    *filename = L_DEBUG_FILENAME;
+    FILE    *fd = 0;
+    char    buf[512];
+    va_list vargs;
+    
+    va_start(vargs, fmt);
+    if(filename == 0) filename = "simul.dbg";
+    fd = fopen(filename, "a+");
+    if(fd) {
+#ifdef _MSC_VER   
+        vsnprintf_s(buf, sizeof(buf) - 1, _TRUNCATE, fmt, vargs);
+#else
+        vsnprintf_s(buf, sizeof(buf) - 1, fmt, vargs);
+#endif    
+        fprintf(fd, "%s", buf);
+        fclose(fd);
+    }    
+    va_end(vargs);
 }
+
+// OLD VERSION
+//void L_debug(char* txt, char* a1, char* a2, char* a3, char* a4, char* a5, char* a6)
+//{
+//    FILE *fd = 0;
+//
+//    fd = fopen("simul.dbg", "a+");
+//    if(fd) fprintf(fd, txt, a1, a2, a3, a4, a5, a6);
+//    fclose(fd);
+//}
+
 
