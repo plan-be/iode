802aa8c0f2d00d35b845d038cb7e77dcdf6a52f5
Alix Damman <ald@plan.be>
2022-09-26 15:21:50 +0200

    C++ API: added EnumIodeEquationTest and vEquationTests

--- cpp_api/objects/equation.h
+++ cpp_api/objects/equation.h
@@ -5,6 +5,53 @@
 #include "time/sample.h"
 
 
+// TODO: replace K by I as below in C api + group them in an enum
+enum EnumIodeEquationMethod
+{
+    IE_LSQ = KE_LSQ,
+    IE_ZELLNER = KE_ZEL,
+    IE_INSTRUMENTAL = KE_INF,
+    IE_GLS = KE_GLS,
+    IE_MAX_LIKELIHOOD = KE_MAXLIK 
+};
+
+const static int I_NB_EQ_METHODS = 5;
+
+const static std::array<std::string, I_NB_EQ_METHODS> vEquationMethods = { "LSQ", "ZELLNER", "INSTRUMENTAL", "GLS (3SLS)", "MAX_LIKELIHOOD" };
+
+
+enum EnumIodeEquationTest
+{
+    IE_CORR,
+    IE_STDEV,
+    IE_MEANY, 
+    IE_SSRES, 
+    IE_STDERR,
+    IE_STDERRP,
+    IE_FSTAT, 
+    IE_R2,
+    IE_R2ADJ, 
+    IE_DW,
+    IE_LOGLIK,
+};
+
+const static int I_NB_EQ_TESTS = 11;
+
+const static std::array<std::string, I_NB_EQ_TESTS> vEquationTests = { 
+    "Correlation",
+    "St dev of residuals",
+    "Mean of YOBS",
+    "õ Residuals²",
+    "Standard error",
+    "Standard error in %",
+    "F-stat",
+    "R²",
+    "R² adjusted",
+    "Durbin-Watson",
+    "Log Likelihood"
+};
+
+
 EQ* create_equation_deep_copy(EQ* original_equation);
 bool equation_equal(EQ* c_eq1, EQ* c_eq2);
 EQ* prepare_equation(const std::string& name, const bool add_obj, const std::string& lec, const std::string& comment,
