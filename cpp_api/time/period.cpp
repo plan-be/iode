e7be9f042dfe66696bc402c3e28a39c3fb592b2c
Alix Damman <ald@plan.be>
2022-09-28 11:48:57 +0200

    C++ API: added period.cpp and sample.cpp

---
+++ cpp_api/time/period.cpp
@@ -0,0 +1,80 @@
+#include "period.h"
+
+
+Period::Period()
+{
+    c_period = nullptr;
+}
+
+Period::Period(const int year, const char periodicity, const int position)
+{
+    IodeExceptionInvalidArguments error("Cannot create new Period");
+    int max_position;
+    // check periodicity
+    if (mPeriodicities.count(periodicity) == 0)
+    {
+        error.add_argument("periodicity", std::string(1, periodicity) + 
+                            " (possible values for the periodicity are " + std::string(L_PERIOD_CH) + ")");
+        throw error;
+    }
+    // check position
+    max_position = mPeriodicities.at(periodicity);
+    if (position < 1 || position > max_position)
+    {
+        error.add_argument("position", std::to_string(position) + 
+                            " (position argument must be in range [1, " + std::to_string(max_position) + "])");
+        throw error;
+    }
+    // initialize class members
+    c_period = (PERIOD*) SW_nalloc(sizeof(PERIOD));
+    c_period->p_y = year;
+    c_period->p_p = periodicity;
+    c_period->p_s = position;
+}
+
+Period::Period(const std::string str_period)
+{ 
+    c_period = PER_atoper(to_char_array(str_period));
+    if (c_period == NULL)
+    {
+        IodeExceptionInitialization error("Period", "Unknown"); 
+        error.add_argument("period", str_period);
+        throw error;
+    } 
+}
+
+Period::Period(PERIOD* c_period) : c_period(c_period) 
+{
+    this->c_period = (PERIOD*) SW_nalloc(sizeof(PERIOD));
+    memcpy(this->c_period, c_period, sizeof(PERIOD));
+}
+
+Period::~Period()
+{
+    SW_nfree(c_period);
+}
+
+int Period::difference(const Period& other) const
+{
+    PERIOD* c_other = other.c_period;
+    if (c_other->p_p != c_period->p_p) 
+    {
+        IodeExceptionFunction error("Cannot calculate the difference between the periods " + 
+            to_string() + " and " + other.to_string());
+        error.set_reason("The two periods must share the same periodicity");
+        error.add_argument("left  period periodicity", std::string(1, c_period->p_p));
+        error.add_argument("right period periodicity", std::string(1, c_other->p_p));
+        throw error;
+    }
+    return PER_diff_per(c_period, c_other);
+}
+
+Period Period::shift(const int nb_periods)
+{
+    // WARNING: PER_addper() returns a pointer to a STATIC structure
+    PERIOD* static_period = PER_addper(c_period, nb_periods);
+    // needed because the destructor of Period calls SW_nfree(c_period)
+    PERIOD shifted_period;
+    memcpy(&shifted_period, static_period, sizeof(PERIOD));
+    return Period(&shifted_period);
+}
--- cpp_api/time/sample.h
+++ cpp_api/time/sample.h
@@ -7,52 +7,26 @@ struct Sample
 	SAMPLE* c_sample;
 
 public:
-	Sample()
-	{
-		c_sample = nullptr;
-	}
+	Sample();
 
-	Sample(const Period& start_period, const Period& end_period)
-	{
-		c_sample = PER_pertosmpl(start_period.c_period, end_period.c_period);
-	}
+	Sample(const Period& start_period, const Period& end_period);
 
 	/**
      * same as PER_atosmpl() function
      */
-	Sample(const std::string str_start_period, std::string str_end_period)
-	{
-		c_sample = PER_atosmpl(to_char_array(str_start_period), to_char_array(str_end_period));
-		if (c_sample == NULL)
-		{
-			IodeExceptionInitialization error("Sample", "Unknown");
-			error.add_argument("start period", str_start_period);
-			error.add_argument("end   period", str_end_period);
-			throw error;
-		}
-	}
+	Sample(const std::string str_start_period, std::string str_end_period);
 
 	/**
 	 * see PER_pertosmpl() for more details
 	 */
-	Sample(PERIOD* c_start_period, PERIOD* c_end_period)
-	{
-		c_sample = PER_pertosmpl(c_start_period, c_end_period);
-	}
+	Sample(PERIOD* c_start_period, PERIOD* c_end_period);
 
 	// We assume that the C sample is valid (i.e. generated via the C API)
 	// NOTE : making a copy of the passed C structure to avoid Heap errors when the
 	//        destructor is called (the C structure may be already freed)
-	Sample(SAMPLE* c_sample)
-	{
-		this->c_sample = (SAMPLE*) SW_nalloc(sizeof(SAMPLE));
-		memcpy(this->c_sample, c_sample, sizeof(SAMPLE));
-	}
+	Sample(SAMPLE* c_sample);
 
-	~Sample()
-	{
-		SW_nfree(c_sample);
-	}
+	~Sample();
 
 	Period start_period() const
 	{
@@ -82,26 +56,7 @@ public:
 	/**
 	 * same as PER_common_smpl() function
 	 */
-	Sample intersection(const Sample& other)
-	{
-		IodeExceptionFunction error("Cannot calculate the intersection between the samples " + 
-			to_string() + " and " + other.to_string());
-
-		SAMPLE* c_other = other.c_sample;
-		if (c_sample->s_p1.p_p != c_other->s_p1.p_p)
-		{
-			error.set_reason("The periodicity of the starting period of the two samples must be same");
-			throw error;
-		}
-		SAMPLE c_res;
-		int res = PER_common_smpl(c_sample, c_other, &c_res);
-		if (res < 0)
-		{
-			error.set_reason("Unknown");
-			throw error;
-		}
-		return Sample(&c_res);
-	}
+	Sample intersection(const Sample& other);
 
 	std::string to_string() const
 	{
