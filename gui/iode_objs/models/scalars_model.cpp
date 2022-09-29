c011fbdfea228e66463868d45f8c1ac0b1e18c6d
Alix Damman <ald@plan.be>
2022-09-29 11:35:13 +0200

    GUI: use getters of Scalar class to get value, relax and std (because of the recent changes in the C++ API)

--- gui/iode_objs/models/scalars_model.cpp
+++ gui/iode_objs/models/scalars_model.cpp
@@ -13,17 +13,17 @@ QVariant ScalarsModel::dataCell(const int row, const int col) const
 		value = QString::fromStdString(kdb->get_name(row));;
 		break;
 	case 1:
-		value = L_ISAN(scalar.val) ? QString::number(scalar.val, 'g', 3) : NAN_REP;
+		value = L_ISAN(scalar.value()) ? QString::number(scalar.value(), 'g', 3) : NAN_REP;
 		break;
 	case 2:
-		value = L_ISAN(scalar.relax) ? QString::number(scalar.relax, 'g', 3) : NAN_REP;
+		value = L_ISAN(scalar.relax()) ? QString::number(scalar.relax(), 'g', 3) : NAN_REP;
 		break;
 	case 3:
-		value = L_ISAN(scalar.std) ? QString::number(scalar.std, 'g', 3) : NAN_REP;
+		value = L_ISAN(scalar.std()) ? QString::number(scalar.std(), 'g', 3) : NAN_REP;
 		break;
 	case 4:
-		if (L_ISAN(scalar.val) && L_ISAN(scalar.std) && !L_IS0(scalar.std))
-			value = QString::number(scalar.val / scalar.std, 'g', 3);
+		if (L_ISAN(scalar.value()) && L_ISAN(scalar.std()) && !L_IS0(scalar.std()))
+			value = QString::number(scalar.value() / scalar.std(), 'g', 3);
 		else
 			value = NAN_REP;
 		break;
