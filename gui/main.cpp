7dd1df178503046a0c52890824b6a8817852c211
Alix Damman <ald@plan.be>
2022-12-06 15:08:43 +0100

    GUI: a splash screen is shown when starting the IODE GUI
    fix #124

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,4 +1,6 @@
 #include <QtWidgets/QApplication>
+#include <QPixmap>
+#include <QSplashScreen>
 
 #include "utils.h"
 #include "super.h"
@@ -12,7 +14,17 @@ int main(int argc, char* argv[])
 
     gui_assign_super_API();
 
+    // For the path given in pixmap, see "The Qt Resource System"
+    // https://doc.qt.io/qt-6/resources.html#qt-resource-collection-file-qrc
+    QPixmap pixmap(":/images/iode_splash_screen.png");
+    // see QSplashScreen documentation:
+    // https://doc.qt.io/qt-6/qsplashscreen.html#details
+    QSplashScreen splash(pixmap);
+    splash.show();
+    app.processEvents();
+
     MainWindow main_window;
     main_window.show();
+    splash.finish(&main_window);
     return app.exec();
 }
