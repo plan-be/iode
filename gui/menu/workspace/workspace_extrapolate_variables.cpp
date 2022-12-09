4e66e10c4cedaba6dc785f6f9c0d7336ea31cc76
Alix Damman <ald@plan.be>
2022-12-09 11:58:31 +0100

    GUI: (QIodeMenuWorkspaceExtrapolateVariables) implemented Menu Workspace > Extrapolate Variables

---
+++ gui/menu/workspace/workspace_extrapolate_variables.cpp
@@ -0,0 +1,61 @@
+#include "workspace_extrapolate_variables.h"
+
+
+QIodeMenuWorkspaceExtrapolateVariables::QIodeMenuWorkspaceExtrapolateVariables(const QString& settings_filepath, QWidget* parent, Qt::WindowFlags f) : 
+    QIodeSettings(settings_filepath, parent, f)
+{
+    setupUi(this);
+
+    QList<QString> methodsList; 
+    for(const std::string& method : v_simulation_initialization) methodsList << QString::fromStdString(method);
+
+    wFrom = new WrapperSampleEdit(label_sample_from->text(), *sampleEdit_sample_from, REQUIRED_FIELD);
+    wTo = new WrapperSampleEdit(label_sample_to->text(), *sampleEdit_sample_to, REQUIRED_FIELD);
+    wMethod = new WrapperComboBox(label_method->text(), *comboBox_method, REQUIRED_FIELD, methodsList);
+    wVarsList = new WrapperQTextEdit(label_variables_list->text(), *textEdit_variables_list, OPTIONAL_FIELD);
+
+    mapFields["From"] = wFrom;
+    mapFields["To"] = wTo;
+    mapFields["Method"] = wMethod;
+    mapFields["VarsList"] = wVarsList;
+
+    // TODO: if possible, find a way to initialize className inside MixingSettings
+    // NOTE FOR DEVELOPPERS: we cannot simply call the line below from the constructor of MixingSettings 
+    //                       since in that case this refers to MixingSettings and NOT the derived class
+    className = QString::fromStdString(typeid(this).name());
+    loadSettings();
+}
+
+QIodeMenuWorkspaceExtrapolateVariables::~QIodeMenuWorkspaceExtrapolateVariables()
+{
+    delete wFrom;
+    delete wTo;
+    delete wMethod;
+    delete wVarsList;
+}
+
+void QIodeMenuWorkspaceExtrapolateVariables::extrapolate_variables()
+{
+    try
+    {
+        std::string s_from = wFrom->extractAndVerify().toStdString();
+        std::string s_to = wTo->extractAndVerify().toStdString();
+        EnumSimulationInitialization method = (EnumSimulationInitialization) wMethod->extractAndVerify();
+        std::string vars_list = wVarsList->extractAndVerify().toStdString();
+
+        KDBVariables kdb;
+        kdb.extrapolate(method, s_from, s_to, vars_list);
+    }
+    catch (const std::exception& e)
+    {
+        QMessageBox::critical(this, tr("ERROR"), tr(e.what()));
+        return;
+    }
+    
+    this->accept();
+}
+
+void QIodeMenuWorkspaceExtrapolateVariables::help()
+{
+	QDesktopServices::openUrl(url_manual);
+}
---
+++ gui/menu/workspace/workspace_extrapolate_variables.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QWidget>
+#include <QList>
+
+#include "ui_workspace_extrapolate_variables.h"
+#include "utils.h"
+#include "settings.h"
+#include "wrapper_classes.h"
+
+
+/* NOTE FOR THE DEVELOPERS:
+ * All Menu Dialog classes MUST
+ * 1. inherit from QIodeSettings,
+ * 2. use the extractAndVerify() method to extract input data from fields,
+ * 3. call the Q_OBJECT macro at the beginning of the class to allow slots and signals (see documentation of Qt).
+ */
+
+
+class QIodeMenuWorkspaceExtrapolateVariables : public QIodeSettings, public Ui::QIodeMenuWorkspaceExtrapolateVariables
+{
+    Q_OBJECT
+
+    WrapperSampleEdit*   wFrom;
+    WrapperSampleEdit*   wTo;
+    WrapperComboBox*     wMethod;
+    WrapperQTextEdit*    wVarsList;
+
+public:
+    QIodeMenuWorkspaceExtrapolateVariables(const QString& settings_filepath, QWidget* parent = nullptr, Qt::WindowFlags f = Qt::WindowFlags());
+    ~QIodeMenuWorkspaceExtrapolateVariables();
+
+public slots:
+	void extrapolate_variables();
+    void help();
+};
