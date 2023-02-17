68c35ca4aa5f99eb534d91e2b1f55085030cc866
alixdamman <ald@plan.be>
2023-02-17 10:39:28 +0100

    GUI: (ReportEditor) Enabled syntax highlighting      (QIodeHighlighter) New class dedicated to highlight Iode keywords and functions in reports

---
+++ gui/custom_widgets/qiode_highlighter.cpp
@@ -0,0 +1,53 @@
+#include "qiode_highlighter.h"
+
+
+QIodeHighlighter::QIodeHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent)
+{
+    // report command -> $ or # (group 0 -> Report internal fns)
+    addRuleCommands(0, Qt::red);
+
+    // report command -> $ or # (group 1)
+    addRuleCommands(1, Qt::darkBlue);
+
+    // report function -> @
+    addRule(R"(@\w+\([^\)]*\))", Qt::darkGreen);
+
+    // report comment
+    // color = olive
+    addRule(R"([$#]\s[^\n]*)", QColor::fromString("#808000"));
+
+    // %<expression>%
+    addRule(R"(%[^%]+%)", Qt::darkMagenta);
+}
+
+void QIodeHighlighter::addRuleCommands(const int group, const QColor& color)
+{
+    QString q_func;
+
+    // $-functions
+    for(const std::string& func: build_command_functions_list(group))
+    {
+        q_func = "[$]!?\\b"  + QString::fromStdString(func).remove(0, 1) + "\\b";
+        addRule(q_func, color);
+    }
+
+    // #-functions
+    for(const std::string& func: build_command_functions_list(group, true))
+    {
+        q_func = "[#]!?\\b"  + QString::fromStdString(func).remove(0, 1) + "\\b";
+        addRule(q_func, color);
+    }
+}
+
+void QIodeHighlighter::highlightBlock(const QString &text)
+{
+    foreach(const HighlightingRule& rule, highlightingRules) 
+    {
+        QRegularExpressionMatchIterator matchIterator = rule.pattern.globalMatch(text);
+        while (matchIterator.hasNext()) 
+        {
+            QRegularExpressionMatch match = matchIterator.next();
+            setFormat(match.capturedStart(), match.capturedLength(), rule.format);
+        }
+    }
+}
--- gui/custom_widgets/report_editor.h
+++ gui/custom_widgets/report_editor.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "utils.h"
+#include "qiode_highlighter.h"
 #include "text_editor.h"
 
 
 class ReportEditor : public TextEditor
 {
+    Q_OBJECT
+
+    QIodeHighlighter *highlighter;
 
 public: 
     ReportEditor(QWidget *parent = nullptr);
