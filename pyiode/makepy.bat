5478c6609060a32e61dcb8ba2e602c3dd41c5724
Jean-Marc Paul <jmp@plan.be>
2023-03-01 00:32:21 +0100

    pyiode/CMakeLists.txt:     - iode_for_py.py renamed to test_iode.py     - setup.py renamed cythonize_iode.py
    pyiode/makepy.bat:
        - full doc
        - 2 modes: visual studio (call makepy -vs) or no vs (call makepy.bat)
        - if -vs, remake iodeapi.lib and s4iode.lib 64 bits
        - set env ISVS = arg 1 of call to makepy (%1%) to be used in cythonize_iode.py
        - error msg if python non activated
        - if current python env = base, use py39 as name
        - create py{pyversion} sub dir
    
    pyiode/setup.py (->cythonize_iode.py):
        - renamed into cythonize_iode.py
        - read the env variable ISVS
        - if VSIS == '-vs', adapt the paths and lib names to the Visual Studio dir structure
        - else, adapt the paths and lib names to command line dir structure

--- pyiode/makepy.bat
+++ pyiode/makepy.bat
@@ -1,42 +1,69 @@
 @echo off
+:: MAKEPY.BAT
+:: ----------
+:: Create iode.pyd, the iode module for python, for the active version of python.
+:: The generated module is copied in the sub directory /py{vers}, where {vers} is running python version, 
+:: for example /py38, py39 or /py310.
+:: 
+:: Syntax : makepy [-vs]
+::    where -vs indicates the environment where the command is executed 
+::     
+::   If -vs is passed as arguement, the program is run in the subdir iode/pyiode of the Visual Studio directory 
+::      structure. In this case, the libraries iodeapi.lib and scr4iode.lib must have been updated **before**
+::      starting the program.
+::   
+::   If -vs is **not** present, the libs iodeapi.lib and s4iode.lib are updated before cythoning iode.pyx.
+::   
+:: Note that :
+::  - the MSVC 64 bits environment must be initialised before starting this program (set64.bat will do the trick)
+::  - the python environment must be initiated by a call to the conda command "activate", for ex. "activate py39".
+::      If the python env is "base", the version name is replaced by py39 (change it for future python versions)
+
 SETLOCAL
+
 SET DISTUTILS_USE_SDK=1
 SET MSSdk=1
 
-:: Current default value (python 3.9)
-set src=iode.cp39-win_amd64.pyd
-set dest=py39\iode.pyd
+:: The next line sets the environment variable ISVS that will be used in setupiode.py.
+SET ISVS=%1%
 
-::if "%CONDA_DEFAULT_ENV%" == "base" (
-::		set src=iode.cp38-win_amd64.pyd
-::		set dest=py38\iode.pyd
-::)
+:: Set the environment vars for MSVC 
+::call set64.bat
 
-if "%CONDA_DEFAULT_ENV%" == "py35" (
-		set src=iode.cp35-win_amd64.pyd
-		set dest=py35\iode.pyd
+:: Only for the non Visual Studio version, remake the 64 bits libs iodeapi.lib and s4iode.lib
+
+if "%ISVS%" == "" (
+    :: Make iodeapi.lib
+    cd ..\api\vc64
+    nmake 
+    
+    :: Make scr4iode.lib
+    cd ..\..\..\scr4_src\vc64
+    nmake s4iode.lib
+    
+    :: Return to the current directory
+    cd ..\..\iode_src\pyiode
 )
 
-if "%CONDA_DEFAULT_ENV%" == "py36" (
-        set src=iode.cp36-win_amd64.pyd
-        set dest=py36\iode.pyd
-)
+:: Define python_name and python_version
+set python_name=%CONDA_DEFAULT_ENV%
+if "%python_name%" == "base" set python_name=py39
+if "%python_name%" == "" (
+    echo Python is not activated.  Please call activate [python version.
+    goto :EOF
+)    
+set python_version=%python_name:~2%
 
-if "%CONDA_DEFAULT_ENV%" == "py37" (
-        set src=iode.cp37-win_amd64.pyd
-        set dest=py37\iode.pyd
-)
+:: Create the sub directory for the current python version
+md %python_name%
 
-if "%CONDA_DEFAULT_ENV%" == "py39" (
-        set src=iode.cp39-win_amd64.pyd
-        set dest=py39\iode.pyd
-)
+:: Cythonize the file iode.pyx
+python.exe cythonize_iode.py build_ext --inplace --compiler=msvc
 
-python.exe setup.py build_ext --inplace --compiler=msvc
+:: Copy the generated file locally and in py<nn>
+set src=iode.cp%python_version%-win_amd64.pyd
+set dest=%python_name%\iode.pyd
 copy /y %src% %dest%
-del iode.pyd
-move /y %src% iode.pyd
+copy /y %src% iode.pyd
 
-::echo src : %src%
-::echo dest : %dest% 
 ENDLOCAL
