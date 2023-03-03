a4a333ab71e8fa1881f5c4b7e5c60eec566e764f
Jean-Marc Paul <jmp@plan.be>
2023-03-03 01:19:43 +0100

    pyiode/*.py* + *.pxi: review the code style to align (mostly) on PEP8.
    pyiode_objs.pyx: refactoring
        - py_to_*() renamed set_*()
        - *_to_py() renamed get_*()
        - var_to_ndarray() renamed get_var_as_ndarray()
    
    pyiode_sample.pyx:
        - ws_sample_get(): return ['', ''] instead of [None, None] when sample is not set
    
    test_iode.py:
        - corrections after refactoring of pyiode_objs.pyx

--- pyiode/pyiode_larray.pyx
+++ pyiode/pyiode_larray.pyx
@@ -7,9 +7,9 @@
 # ---------------------------------------------
 #   la_to_ws_pos(la_input, int* la_pos, int* ws_pos, int* la_lg, time_axis_name = 'time')
 #  
-#   larray_to_ws(la_input, time_axis_name:str='time', sep:str="_")   | Copies LArray la_input into IODE KV_WS.      | Copies LArray la_input into IODE KV_WS.
-#   ws_to_larray(vars_pattern:str='*', vars_axis_name:str='vars', time_axis_name:str='time', split_axis_names='', regex = None, split_sep = None, time_as_floats: bool=False) -> la.Array | Creates an LArray from the current KV_WS content
-#   ws_load_var_to_larray(filename:str, vars_pattern='*', vars_axis_name='vars', time_axis_name='time', split_axis_names=None, regex=None, split_sep=None)->la.Array | Load an IODE var file into an Larray object with 2 axes (vars and time)  
+#   larray_to_ws(la_input, time_axis_name: str = 'time', sep: str = "_")   | Copies LArray la_input into IODE KV_WS.      | Copies LArray la_input into IODE KV_WS.
+#   ws_to_larray(vars_pattern: str = '*', vars_axis_name: str = 'vars', time_axis_name: str = 'time', split_axis_names = '', regex = None, split_sep = None, time_as_floats: bool = False) -> la.Array | Creates an LArray from the current KV_WS content
+#   ws_load_var_to_larray(filename: str, vars_pattern = '*', vars_axis_name = 'vars', time_axis_name = 'time', split_axis_names = None, regex = None, split_sep = None) -> la.Array | Load an IODE var file into an Larray object with 2 axes (vars and time)  
 #   larray_get_sample(la_input, time_axis_name = 'time')             | Return the first and last time axis labels as a list of 2 strings
 
     
@@ -25,12 +25,12 @@ cdef la_to_ws_pos(la_input, int* la_pos, int* ws_pos, int* la_lg, time_axis_name
 
     time = la_input.axes[time_axis_name]
     IodeCalcSamplePosition(cstr(time.labels[0]), cstr(time.labels[-1]), la_pos, ws_pos, la_lg)
-    #print(f"la_pos:{la_pos} - ws_pos:{ws_pos} - la_lg:{la_lg} ")
+    #print(f"la_pos: {la_pos} - ws_pos: {ws_pos} - la_lg: {la_lg} ")
 
 
 
 # TODO: check return values and return an error code 
-def larray_to_ws(la_input, time_axis_name:str='time', sep:str="_"):
+def larray_to_ws(la_input, time_axis_name: str = 'time', sep: str = "_"):
     '''
     Copies Array la_input into IODE KV_WS.
     
@@ -52,7 +52,7 @@ def larray_to_ws(la_input, time_axis_name:str='time', sep:str="_"):
     
     # Push the time axis as last axis and combine all other axes 
     la_input = la_input.transpose(..., time_axis_name)
-    la_input = la_input.combine_axes(la_input.axes[:-1], sep=sep)
+    la_input = la_input.combine_axes(la_input.axes[ : -1], sep = sep)
 
     # Calculate:
     #   - la_pos: the position of the first element of la_input to be copied into WS
@@ -88,28 +88,28 @@ def larray_to_ws(la_input, time_axis_name:str='time', sep:str="_"):
 
  
 # TODO: review this function logic / parameters 
-def ws_to_larray(vars_pattern:str='*', 
-                 vars_axis_name:str='vars',     
-                 time_axis_name:str='time', 
-                 split_axis_names='', 
+def ws_to_larray(vars_pattern: str = '*', 
+                 vars_axis_name: str = 'vars',     
+                 time_axis_name: str = 'time', 
+                 split_axis_names = '', 
                  regex = None, 
                  split_sep = None, 
-                 time_as_floats: bool=False) -> la.Array:
+                 time_as_floats: bool = False) -> la.Array:
     '''
     Creates an LArray from the current KV_WS content with time axis labels in the IODE syntax ['2000Q2',...].
     If time_as_floats is True, the time axis labels are doubles [2000.25,...]
     '''
     cdef:
         int i
-        np.ndarray[np.double_t, ndim=2] la_data
-        double[:,::1] data_view
+        np.ndarray[np.double_t, ndim = 2] la_data
+        double[ : ,: : 1] data_view
         str v
     
     var_list = ws_content_var(vars_pattern)         # 6 = K_VAR
     
     # Define 2 original axes var and time
     axis_var = la.Axis(var_list, vars_axis_name)
-    axis_time = ws_sample_to_larray_axis(axis_name=time_axis_name, as_floats=time_as_floats)
+    axis_time = ws_sample_to_larray_axis(axis_name = time_axis_name, as_floats = time_as_floats)
 
     # Create LArray with 0
     la_res = la.zeros([axis_var, axis_time])
@@ -120,33 +120,33 @@ def ws_to_larray(vars_pattern:str='*',
     # Copy values from KV_WS to la_res
     for i in range(len(var_list)):
         v = var_list[i]
-        la_data[i] = var_to_ndarray(v, copy=False)
+        la_data[i] = get_var_as_ndarray(v, copy = False)
           
     if split_axis_names != '':
-        # split_axes(self, axes=None, sep='_', names=None, regex=None, sort=False, fill_value=nan)
-        la_res = la_res.split_axes(axis_var, sep=split_sep, names=split_axis_names, regex=regex)
+        # split_axes(self, axes = None, sep = '_', names = None, regex = None, sort = False, fill_value = nan)
+        la_res = la_res.split_axes(axis_var, sep = split_sep, names = split_axis_names, regex = regex)
 
     return la_res
 
 
 # Creates an LArray from the current KV_WS content with **integer** sample (old version)
-#def ws_to_larray_time_as_doubles(vars_pattern='*', varsaxis='vars', time_axis_name='time', split_axis_names='', regex=None, sep=None):
+#def ws_to_larray_time_as_doubles(vars_pattern = '*', varsaxis = 'vars', time_axis_name = 'time', split_axis_names = '', regex = None, sep = None):
 #
-#    return ws_to_larray_gnl(vars_pattern=vars_pattern, varsaxis=varsaxis, time_axis_name=time_axis_name, split_axis_names=split_axis_names, regex=regex, sep=sep, time_as_doubles=True)
+#    return ws_to_larray_gnl(vars_pattern = vars_pattern, varsaxis = varsaxis, time_axis_name = time_axis_name, split_axis_names = split_axis_names, regex = regex, sep = sep, time_as_doubles = True)
  
 
-#def ws_to_larray(vars_pattern = '*', varsaxis = 'vars', time_axis_name = 'time', split_axis_names='', regex=None, sep=None, time_as_doubles=False):
+#def ws_to_larray(vars_pattern = '*', varsaxis = 'vars', time_axis_name = 'time', split_axis_names = '', regex = None, sep = None, time_as_doubles = False):
 #
-#    return ws_to_larray_gnl(vars_pattern=vars_pattern, varsaxis=varsaxis, time_axis_name=time_axis_name, split_axis_names=split_axis_names, regex=regex, sep=sep, time_as_doubles=time_as_doubles)
+#    return ws_to_larray_gnl(vars_pattern = vars_pattern, varsaxis = varsaxis, time_axis_name = time_axis_name, split_axis_names = split_axis_names, regex = regex, sep = sep, time_as_doubles = time_as_doubles)
 
     
-def ws_load_var_to_larray(filename:str, 
-                            vars_pattern='*', 
-                            vars_axis_name='vars', 
-                            time_axis_name='time', 
-                            split_axis_names=None, 
-                            regex=None, 
-                            split_sep=None)->la.Array:
+def ws_load_var_to_larray(filename: str, 
+                            vars_pattern = '*', 
+                            vars_axis_name = 'vars', 
+                            time_axis_name = 'time', 
+                            split_axis_names = None, 
+                            regex = None, 
+                            split_sep = None) -> la.Array:
     
     '''Load an IODE .var file into an Larray object with 2 axes (vars and time) 
     '''
@@ -158,18 +158,18 @@ def ws_load_var_to_larray(filename:str,
         return None
 
     # Create LArray 
-    la_res = ws_to_larray(vars_pattern=vars_pattern, 
-                    vars_axis_name=vars_axis_name, 
-                    time_axis_name=time_axis_name, 
-                    split_axis_names=split_axis_names, 
-                    regex=regex, 
-                    split_sep=split_sep)
+    la_res = ws_to_larray(vars_pattern = vars_pattern, 
+                    vars_axis_name = vars_axis_name, 
+                    time_axis_name = time_axis_name, 
+                    split_axis_names = split_axis_names, 
+                    regex = regex, 
+                    split_sep = split_sep)
     
     return la_res
 
 
 
-def larray_get_sample(la_input, time_axis_name = 'time')->List[Union[str,float]]:
+def larray_get_sample(la_input, time_axis_name = 'time') -> List[Union[str,float]]:
     '''Return the first and last time axis labels as a list of 2 strings'''
     
     time = la_input.axes[time_axis_name]
--- pyiode/pyiode_objs.pyx
+++ pyiode/pyiode_objs.pyx
@@ -3,59 +3,59 @@
 #
 #    @header4iode
 # 
-#  Functions to convert IODE objects from and to python variables.
+#  Functions to convert IODE objects from and to python variables/classes.
 #  
 #  For each object, at least 3 functions are provided: 
-#   - py_to_<object_type>() 
-#   - <object_type>_to_py()
-#   - delete_<object_type>
+#   - py_to_<object_type>()     => get_
+#   - <object_type>_to_py()     => set_
+#   - delete_<object_type>      => del_
 #  
 #  where <object_type> is one of {cmt, eq, idt, lst, scl, tbl, var}.
 #  
 #  The associated C functions are provided in the source file b_api.c which is part of iodeapi.lib.
 #   
-#   delete_objects(pattern:str='*', obj_type:int=K_VAR)  |   delete the objects whose names satisfy the given pattern
+#   delete_objects(pattern: str = '*', obj_type: int = K_VAR)  |   delete the objects whose names satisfy the given pattern
 #
-#   delete_obj(name:str, obj_type:int)->int   | delete the object named name of type obj_type
-#   delete_cmt(name:str)->int                 | delete the comment named name
-#   delete_eqs(name:str)->int                 | delete the equation named name
-#   delete_idt(name:str)->int                 | delete the identity named name
-#   delete_lst(name:str)->int                 | delete the list named name
-#   delete_scl(name:str)->int                 | delete the scalar named name
-#   delete_tbl(name:str)->int                 | delete the table named name
-#   delete_var(name:str)->int                 | delete the variable named name
+#   delete_obj(name: str, obj_type: int) -> int   | delete the object named name of type obj_type
+#   delete_cmt(name: str) -> int                  | delete the comment named name
+#   delete_eqs(name: str) -> int                  | delete the equation named name
+#   delete_idt(name: str) -> int                  | delete the identity named name
+#   delete_lst(name: str) -> int                  | delete the list named name
+#   delete_scl(name: str) -> int                  | delete the scalar named name
+#   delete_tbl(name: str) -> int                  | delete the table named name
+#   delete_var(name: str) -> int                  | delete the variable named name
 #       
-#   cmt_to_py(name)                                         
-#   py_to_cmt(name, cmt)
+#   get_cmt(name)                                         
+#   set_cmt(name, cmt)
 #  
-#   eqs_lec_to_py(eq_name:str)
-#   eqs_to_py(eq_name:str)
-#   py_to_eqs(eq_name:str, lec:str)
+#   get_eqs_lec(eq_name: str)
+#   get_eqs(eq_name: str)
+#   set_eqs(eq_name: str, lec: str)
 #  
-#   idt_to_py(name)
-#   py_to_idt(name, idt)
+#   get_idt(name)
+#   set_idt(name, idt)
 #  
-#   lst_to_py(name)
-#   py_to_lst(name, lst)
+#   get_lst(name)
+#   set_lst(name, lst)
 #  
-#   scl_to_py(name:str)
-#   py_to_scl(py_scl:scl)
+#   get_scl(name: str)
+#   set_scl(py_scl: scl)
 #  
-#   var_to_py(varname)
-#   var_to_ndarray(varname, copy = True) 
-#   py_to_var(varname, py_values)
+#   get_var(varname)
+#   get_var_as_ndarray(varname, copy = True) 
+#   set_var(varname, py_values)
 
 
-def delete_objects(pattern:str='*', obj_type:int=K_VAR)->int:
+def delete_objects(pattern: str = '*', obj_type: int = K_VAR) -> int:
     r'''Delete one or more IODE objects corresponding to the specified pattern.
     
     Parameters
     ----------
-    pattern : str
+    pattern: str
         string containing wildcards characters like '*' or '?'. 
         Default '*', meaning "all objects".
         
-    obj_type : int
+    obj_type: int
         IODE object type (0-6, 0 for comments...)
         Default 6 for variables
         
@@ -68,38 +68,38 @@ def delete_objects(pattern:str='*', obj_type:int=K_VAR)->int:
     Examples
     --------
     >>> iode.wsloadidt("fun.idt")
-    >>> nbobjs = len(iode.ws_content('X*', 2))
-    >>> rc = iode.delete_objects('X*', 2)
-    >>> nbobjs2 = len(iode.ws_content('X*', 2))
-    >>> print(f"iode.delete_objects('X*', 2): before: {nbobjs}. After: {nbobjs2}")
-    iode.delete_objects('X*', 2): before: 13. After: 0
+    >>> nbobjs = len(iode.ws_content('X*', K_IDT))
+    >>> rc = iode.delete_objects('X*', K_IDT)
+    >>> nbobjs2 = len(iode.ws_content('X*', K_IDT))
+    >>> print(f"iode.delete_objects('X*', K_IDT): before: {nbobjs}. After: {nbobjs2}")
+    iode.delete_objects('X*', K_IDT2): before: 13. After: 0
     '''
     return B_DataDelete(cstr(pattern), obj_type)
 
 # delete_<obj_type> functions
 # ---------------------------
-def delete_obj(obj_name:str, obj_type:int)->int:
+def delete_obj(obj_name: str, obj_type: int) -> int:
     return IodeDeleteObj(cstr(obj_name), obj_type)
 
-def delete_cmt(name:str)->int:
+def delete_cmt(name: str) -> int:
     return delete_obj(name, K_CMT)
 
-def delete_eqs(name:str)->int:
+def delete_eqs(name: str) -> int:
     return delete_obj(name, K_EQS)
 
-def delete_idt(name:str)->int:
+def delete_idt(name: str) -> int:
     return delete_obj(name, K_IDT)
     
-def delete_lst(name:str)->int:
+def delete_lst(name: str) -> int:
     return delete_obj(name, K_LST)
    
-def delete_scl(name:str)->int:
+def delete_scl(name: str) -> int:
     return delete_obj(name, K_SCL)
 
-def delete_tbl(name:str)->int:
+def delete_tbl(name: str) -> int:
     return delete_obj(name, K_TBL)
 
-def delete_var(name:str)->int:
+def delete_var(name: str) -> int:
     return delete_obj(name, K_VAR)
    
 
@@ -108,13 +108,14 @@ def delete_var(name:str)->int:
 
 # Comments
 # --------
-def cmt_to_py(name:str)->str:
+
+def get_cmt(name: str) -> str:
     '''Return the text of an IODE comment'''
     
     cmt850 = IodeGetCmt(cstr(name))
     return pystr(cmt850)
 
-def py_to_cmt(name:str, cmt:str)->int:
+def set_cmt(name: str, cmt: str) -> int:
     '''Update or create an IODE comment from a python str'''
     
     return IodeSetCmt(cstr(name), cstr(cmt))
@@ -122,13 +123,13 @@ def py_to_cmt(name:str, cmt:str)->int:
 # Equations
 # ---------
 
-def eqs_lec_to_py(eq_name:str)->str:
+def get_eqs_lec(eq_name: str) -> str:
     '''Return an IODE equation LEC form as a string'''
     
     lec850 = IodeGetEqsLec(cstr(eq_name))
     return pystr(lec850)
 
-def eqs_to_py(eq_name:str)->Equation:
+def get_eqs(eq_name: str) -> Equation:
     '''Return an IODE equation as an iode.eqs class instance'''
     
     cdef char   *lec, 
@@ -159,31 +160,31 @@ def eqs_to_py(eq_name:str)->Equation:
 
 
 # TODO: save other (optional) equation properties like tests, sample, method
-def py_to_eqs(eq_name:str, lec:str)->int:
+def set_eqs(eq_name: str, lec: str) -> int:
     return IodeSetEqs(cstr(eq_name), cstr(lec))
 
 # Identities
 # ----------
-def idt_to_py(name:str)->str:
+def get_idt(name: str) -> str:
     idt850 = IodeGetIdt(cstr(name))
     return pystr(idt850)
 
-def py_to_idt(name:str, idt:str)->int:
+def set_idt(name: str, idt: str) -> int:
     return IodeSetIdt(cstr(name), cstr(idt))
     
 # Lists
 # -----
-def lst_to_py(name:str)->str:
+def get_lst(name: str) -> str:
     lst850 = IodeGetLst(cstr(name))
     return pystr(lst850)
 
-def py_to_lst(name:str, lst:str)->int:
+def set_lst(name: str, lst: str) -> int:
     return IodeSetLst(cstr(name), cstr(lst))
 
 
 # Scalars
 # -------
-def scl_to_py(name:str)->Scalar:
+def get_scl(name: str) -> Scalar:
     #vararray = iodescl_to_ndarray(cstr(name))
     #return vararray
     cdef    double cval
@@ -198,7 +199,7 @@ def scl_to_py(name:str)->Scalar:
         return None
 
 
-def py_to_scl(py_scl:Scalar)->int:
+def set_scl(py_scl: Scalar) -> int:
     
     return IodeSetScl(cstr(py_scl.name), py_scl.value, py_scl.relax, py_scl.std)
 
@@ -207,7 +208,7 @@ def py_to_scl(py_scl:Scalar)->int:
 # ------
 # ... not yet implemented ...
 # TODO:
-#    - tbl_add_line(tbl_name, after_line, [cells], line_type=KT_CELLS)
+#    - tbl_add_line(tbl_name, after_line, [cells], line_type = KT_CELLS)
 #    - tbl_add_lines(tbl_name, after_line, [[cells], [cells]...])
 #    - tbl_del_line(tbl_name, line)
 #    - tbl_create(tbl_name, title, [line1, line2...])
@@ -219,23 +220,23 @@ def py_to_scl(py_scl:Scalar)->int:
 # ---------
 
 # Copy an IODE var into a list
-def var_to_py(varname:str)->List[np.ndarray]:
-    ndarray = var_to_ndarray(varname, True)
+def get_var(varname: str) -> List[np.ndarray]:
+    ndarray = get_var_as_ndarray(varname, True)
     return list(ndarray)
 
 # Copy (or refer to) an IODE var into a ndarray
-def var_to_ndarray(varname:str, copy:bool=True)->List[np.ndarray]:
+def get_var_as_ndarray(varname: str, copy: bool = True) -> List[np.ndarray]:
     vararray = iodevar_to_ndarray(cstr(varname), copy)
     return vararray 
 
 # Copy a ndarray or a list into KV_WS
-def py_to_var(varname:str, py_values)->int:
+def set_var(varname: str, py_values) -> int:
     cdef double  *c_values
     
     if not IodeIsSampleSet():
-        raise RuntimeError("py_to_var impossible : IODE sample not set")
+        raise RuntimeError("py_to_var impossible: IODE sample not set")
         
-    ndarray = np.array(py_values, dtype=np.double) 
+    ndarray = np.array(py_values, dtype = np.double) 
     
     c_values = <double*>np.PyArray_DATA(ndarray)
     IodeSetVector(cstr(varname), c_values, 0, 0, -1)
