669b6ea237f94201c829b7e130d177480f194383
alixdamman <ald@plan.be>
2023-03-23 16:54:07 +0100

    GUI: (EquationsView) made edit equation dialog non-modal

--- gui/iode_objs/views/equations_view.cpp
+++ gui/iode_objs/views/equations_view.cpp
@@ -18,8 +18,13 @@ void EquationsView::edit_obj()
 	int section = selection[0].row();
 	QString name = model()->headerData(section, Qt::Vertical).toString();
 	
-	QIodeEditEquation dialog(name, *this->project_settings_filepath, static_cast<QWidget*>(this->parent()));
-	dialog.exec();
+	if(editEqDialog)
+	{
+		editEqDialog->close();
+		delete editEqDialog;
+	}
+	editEqDialog = new QIodeEditEquation(name, *this->project_settings_filepath, static_cast<QWidget*>(this->parent()));
+	editEqDialog->show();
 	
 	update();
 }
