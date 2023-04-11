0bd80162049708602b9df8b1a6af5869efeddbca
alixdamman <ald@plan.be>
2023-04-11 11:40:12 +0200

    GUI: (IdentitiesView) pressing F7 in the Identities table popup the Execute Identities dialog box

--- gui/iode_objs/views/identities_view.h
+++ gui/iode_objs/views/identities_view.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <QObject>
+#include <QShortcut>
 
+#include "main_window_plot.h"
 #include "abstract_table_view.h"
 #include "iode_objs/models/identities_model.h"
 #include "iode_objs/delegates/identities_delegate.h"
@@ -12,8 +14,23 @@ class IdentitiesView : public TemplateTableView<IdentitiesModel>
 {
 	Q_OBJECT
 
+	QShortcut* executeShortcut;
+
 public:
-	IdentitiesView(QWidget* parent = nullptr) : TemplateTableView(I_IDENTITIES, new IdentitiesDelegate(parent), parent) {};
+	IdentitiesView(QWidget* parent = nullptr) : TemplateTableView(I_IDENTITIES, new IdentitiesDelegate(parent), parent) 
+	{
+		// ---- keyboard shortcuts ----
+		executeShortcut = new QShortcut(QKeySequence(Qt::Key_F7), this);
+		executeShortcut->setContext(Qt::WidgetWithChildrenShortcut);
+	};
+
+	void setup(std::shared_ptr<QString>& settings_filepath) override
+	{
+		AbstractTableView::setup(settings_filepath);
+
+		MainWindowPlot* main_window = static_cast<MainWindowPlot*>(get_main_window_ptr());
+		connect(executeShortcut, &QShortcut::activated, main_window, &MainWindowPlot::open_compute_identities_dialog);
+	}
 
 public slots:
 	void filter() { filter_and_update(); }
