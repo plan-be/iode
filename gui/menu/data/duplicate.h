1082c71b0fccffe442a0b9c3f5b30972e582566a
alixdamman <ald@plan.be>
2023-04-19 09:26:06 +0200

    GUI: (QIodeMenuDataDuplicateObj) implemented Menu Data > Duplicate Objects

---
+++ gui/menu/data/duplicate.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QWidget>
+
+#include "ui_duplicate.h"
+#include "utils.h"
+#include "settings.h"
+#include "wrapper_classes.h"
+#include "custom_widgets/complete_line_edit.h"
+
+
+class QIodeMenuDataDuplicateObj : public QIodeSettings, public Ui::QIodeMenuDataDuplicateObj
+{
+    Q_OBJECT
+
+    WrapperComboBox*    wComboIodeTypes;
+    WrapperQLineEdit*   wObjName;
+    WrapperQLineEdit*   wDupObjName;
+
+    QIodeCompleter* completer;
+    
+public:
+	QIodeMenuDataDuplicateObj(const QString& project_settings_filepath, QWidget* parent = nullptr, Qt::WindowFlags f = Qt::WindowFlags());
+    ~QIodeMenuDataDuplicateObj();
+
+public slots:
+    void duplicate();
+    void help();
+};
