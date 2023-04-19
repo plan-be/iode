478f98da20c2e2e129079e772916222dc6787a9f
alixdamman <ald@plan.be>
2023-04-19 11:43:41 +0200

    GUI: (QIodeCompleteLineEdit) implemented a class to represent a line edit with the auto-complete feature

---
+++ gui/custom_widgets/complete_line_edit.cpp
@@ -0,0 +1,99 @@
+#include "complete_line_edit.h"
+
+
+void QIodeCompleteLineEdit::keyPressEvent(QKeyEvent *event)
+{
+    if(!c) 
+    {
+        QLineEdit::keyPressEvent(event);
+        return;
+    }
+
+    if(c->popup()->isVisible()) 
+    {
+        // The following keys are forwarded by the completer to the widget
+       switch (event->key()) 
+       {
+       case Qt::Key_Enter:
+       case Qt::Key_Return:
+       case Qt::Key_Escape:
+       case Qt::Key_Tab:
+       case Qt::Key_Backtab:
+            event->ignore();
+            return; // let the completer do default behavior
+       case Qt::Key_Space:
+            c->popup()->hide();
+            QLineEdit::keyPressEvent(event);
+            return;
+       default:
+           break;
+       }
+    }
+
+    bool exit = handleSpecialKeys(event);
+    if(exit)
+        return;
+
+    // CTRL + Space forces auto-completion
+    bool completeShortcut = event->modifiers().testFlag(Qt::ControlModifier) && event->key() == Qt::Key_Space;
+    if(!completeShortcut)
+        QLineEdit::keyPressEvent(event);
+    
+    c->setCompletionPrefix(textUnderCursor());
+    if(completeShortcut || c->completionPrefix().size() > 0)
+        c->complete();
+    else
+        c->popup()->hide();
+}
+
+void QIodeCompleteLineEdit::focusInEvent(QFocusEvent* event)
+{
+    if(c)
+        c->setWidget(this);
+    QLineEdit::focusInEvent(event);
+}
+
+void QIodeCompleteLineEdit::insertCompletion(const QString& completion)
+{
+    if (c->widget() != this)
+        return;
+    
+    QString str = this->text();
+    if(str.size() == 0) return;
+
+    // delete completion prefix
+    for(int i=0; i < c->completionPrefix().size(); i++) backspace();
+
+    // replaces with full function or Iode object name
+    insert(completion);
+}
+
+QString QIodeCompleteLineEdit::textUnderCursor()
+{
+    QString line = this->text().trimmed();
+    if(line.size() == 0) return "";
+
+    int pos = cursorPosition();
+
+    // selects the "word" under cursor
+    // WARNING: cursorWordBackward(mark) selects only
+    //          alpha characters (i.e. the selection will not include 
+    //          characters such as $, # and @)
+    cursorWordBackward(true);
+
+    // NOTE: we need to add the lines below because we need to know
+    //       if there is a character $, # or @ before the "word"
+    if(cursorPosition() > 0)
+    {
+        QChar ch = line[cursorPosition()-1];
+        // cursorBackward(mark): Moves the cursor back. If mark is true 
+        // each character moved over is added to the selection
+        if(ch == '$' || ch == '#' || ch == '@') 
+            cursorBackward(true);
+    }
+
+    QString str = selectedText();
+    deselect();
+    setCursorPosition(pos);
+    return str;
+}
---
+++ gui/custom_widgets/complete_line_edit.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "utils.h"
+#include "custom_widgets/qiode_completer.h"
+
+#include <QObject>
+#include <QLineEdit>
+#include <QCompleter>
+#include <QString>
+#include <QKeyEvent>
+#include <QAbstractItemView>
+
+
+/**
+ * @brief Special line field which tries to auto-complete user input.
+ * 
+ *        Auto-completion starts at the beginning of the line 
+ *        or after a whitespace or any characters which is not 
+ *        A-Z, a-z, 0-9, _, $, #, and @. 
+ * 
+ */
+class QIodeCompleteLineEdit: public QLineEdit
+{
+    Q_OBJECT
+
+protected:
+    QIodeCompleter* c;
+
+public:
+    QIodeCompleteLineEdit(QWidget *parent = nullptr, QIodeCompleter* c=nullptr) 
+        : QLineEdit(parent), c(c) {}
+
+    QIodeCompleter* getCompleter() const
+    {
+        return c;
+    }
+
+    void setCompleter(QIodeCompleter* c) 
+    {
+        this->c = c;
+
+        if (!c)
+            return;
+
+        c->setWidget(this);
+        // Signal activated is overloaded in QCompleter. 
+        // To connect to this signal by using the function pointer syntax, Qt provides 
+        // a convenient helper for obtaining the function pointer
+        connect(c, QOverload<const QString &>::of(&QCompleter::activated),
+                this, &QIodeCompleteLineEdit::insertCompletion);
+    }
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+
+    // event handler used to receive keyboard focus events for the widget.
+    void focusInEvent(QFocusEvent* event) override;
+
+    /**
+     * @param event 
+     * @return bool weither or not to exit keyPressEvent() 
+     */
+    virtual bool handleSpecialKeys(QKeyEvent *event) 
+    {
+        return false;
+    }
+
+private slots:
+    void insertCompletion(const QString& completion);
+
+private:
+    QString textUnderCursor();
+
+public slots:
+    void setIodeType(const int iodeType)
+    {
+        if(c)
+            c->setIodeType(iodeType);
+    }
+
+    void includeIodeCommand(const bool ok)
+    {
+        if(c)
+            c->includeIodeCommand(ok);
+    }
+
+    void includeLecFunctions(const bool ok)
+    {
+        if(c)
+            c->includeLecFunctions(ok);
+    }
+};
