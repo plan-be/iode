512eedc9ef9829b1dc8f6d003d6ebcc575a3f9eb
alixdamman <ald@plan.be>
2023-04-27 09:58:21 +0200

    GUI: (QIodePrintFileDialog) implemented box dialog to display when the user wants to print in a file (A2M, CSV, HTML, ...)

---
+++ gui/custom_widgets/print_file_dialog.cpp
@@ -0,0 +1,56 @@
+#include "print_file_dialog.h"
+
+
+const QString KEY_SETTINGS_PRINT_FORMAT = "PRINT_FILE_DIALOG/Print_Format";
+const QString KEY_SETTINGS_PRINT_OUTPUT_FILE = "PRINT_FILE_DIALOG/Output_File";
+
+
+QIodePrintFileDialog::QIodePrintFileDialog(const QString& settings_filepath, QWidget* parent, Qt::WindowFlags f)
+    : QIodeSettings(settings_filepath, parent, f)
+{
+    setupUi(this);
+
+    QList<QString> q_print_format;
+    for(const std::string& format: vPrintFormats) q_print_format << QString::fromStdString(format);
+
+    wPrintFormat = new WrapperComboBox(label_print_format->text(), *comboBox_print_format, REQUIRED_FIELD, q_print_format);
+    wOutputFile = new WrapperFileChooser(label_output_file->text(), *fileChooser_output_file, REQUIRED_FIELD, 
+        EnumIodeFile::I_A2M_FILE, EnumFileMode::FILE_MAY_EXIST);
+
+    comboBox_print_format->setCurrentIndex(0);
+    connect(comboBox_print_format, &QComboBox::currentIndexChanged, this, &QIodePrintFileDialog::updateOutputFileFormat);
+    
+    mapFields["Print_Format"] = wPrintFormat;
+    mapFields["Output_File"] = wOutputFile;
+
+    className = "PRINT_FILE_DIALOG";
+    loadSettings();
+}
+
+QIodePrintFileDialog::~QIodePrintFileDialog()
+{
+    delete wPrintFormat;
+    delete wOutputFile;
+}
+
+void QIodePrintFileDialog::updateOutputFileFormat(int index)
+{
+    fileChooser_output_file->setFileType(static_cast<EnumIodeFile>(I_A2M_FILE + index));
+}
+
+// Will automatically save to settings -> see QIodeSettings::closeEvent()
+void QIodePrintFileDialog::apply()
+{
+    this->accept();
+}
+
+void QIodePrintFileDialog::set_print_options()
+{
+    QIodeMenuFilePrintPref dialog(project_settings->fileName(), static_cast<QWidget*>(parent()));
+    dialog.exec();
+}
+
+void QIodePrintFileDialog::help()
+{
+	QDesktopServices::openUrl(url_manual);
+}
