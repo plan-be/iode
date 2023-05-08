d84d667d2d06d868c3ab832f8d7c81a78f0b4bec
alixdamman <ald@plan.be>
2023-05-08 16:54:01 +0200

    GUI: (QIodeReportWidget) added print() method (not implemented yet)

--- gui/tabs/tab_report.cpp
+++ gui/tabs/tab_report.cpp
@@ -3,78 +3,24 @@
 QIodeReportWidget::QIodeReportWidget(const QString& filepath, QTextEdit* output, std::shared_ptr<QIodeCompleter>& completer, 
     QWidget* parent) : QIodeAbstractEditor(I_REPORTS_FILE, filepath, parent)
 {
+    setupUi(this);
+
     filter = "IODE report files (*" + QString::fromStdString(report_ext) + ")";
-
-    // layout
-    layout = new QGridLayout(this);
-    layout->setObjectName(QString::fromUtf8("layout"));
-    layout->setContentsMargins(0, 0, 0, 0);
-
-    int col = 0;
 
     // autocomplete checkbox
     /*
     QCheckBox* autocomplete = new QCheckBox("autocomplete", this);
     autocomplete->setChecked(true);
-    autocomplete->setObjectName(QString::fromUtf8("autocomplete"));
-    QSizePolicy sizePolicyCompleter(QSizePolicy::Fixed, QSizePolicy::Fixed);
-    sizePolicyCompleter.setHorizontalStretch(0);
-    sizePolicyCompleter.setVerticalStretch(0);
-    sizePolicyCompleter.setHeightForWidth(autocomplete->sizePolicy().hasHeightForWidth());
-    autocomplete->setSizePolicy(sizePolicyCompleter);
-    layout->addWidget(autocomplete, 0, col, Qt::AlignLeft);
-    col++;
     */
 
-    // spacer
-    QSpacerItem* horizontalSpacer = new QSpacerItem(720, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
-    layout->addItem(horizontalSpacer, 0, col);
-    col++;
-
-    // Run button
-    QPushButton* runButton = new QPushButton("Run", this);
-    runButton->setObjectName(QString::fromUtf8("runButton"));
-    QSizePolicy sizePolicyRun(QSizePolicy::Fixed, QSizePolicy::Fixed);
-    sizePolicyRun.setHorizontalStretch(0);
-    sizePolicyRun.setVerticalStretch(0);
-    sizePolicyRun.setHeightForWidth(runButton->sizePolicy().hasHeightForWidth());
-    runButton->setSizePolicy(sizePolicyRun);
-    layout->addWidget(runButton, 0, col, Qt::AlignRight);
-    col++;
-
-    // add report editor
-    editor = new ReportEditor(output, parent);
+    // set report editor
+    editor->setOutput(output);
     editor->setCompleter(completer.get());
-    addEditorToLayout(1);
 
     // ---- Signals and Slots ----
     connect(editor, &TextEditor::modificationChanged, this, &QIodeReportWidget::setModified);
 
-    runShortcut = new QShortcut(QKeySequence(Qt::CTRL | Qt::Key_R), this);
-    connect(runShortcut, &QShortcut::activated, this, &QIodeReportWidget::run);
-    connect(runButton, &QPushButton::clicked, this, &QIodeReportWidget::run);
-
     // connect(autocomplete, &QCheckBox::stateChanged, this, &QIodeReportWidget::toggleAutocomplete);
-}
-
-QIodeReportWidget::~QIodeReportWidget()
-{
-    delete editor;
-    delete runShortcut;
-}
-
-void QIodeReportWidget::addEditorToLayout(int row)
-{
-    editor->setGeometry(QRect(0, 50, 800, 500));
-    QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    sizePolicy.setHorizontalStretch(0);
-    sizePolicy.setVerticalStretch(0);
-    sizePolicy.setHeightForWidth(editor->sizePolicy().hasHeightForWidth());
-    editor->setSizePolicy(sizePolicy);
-    editor->setSizeAdjustPolicy(QAbstractScrollArea::AdjustIgnored);
-
-    // -1 -> span over all rows/columns
-    layout->addWidget(editor, row, 0, -1, -1);
 
     if(!filepath.isEmpty()) 
         load(filepath, true);
