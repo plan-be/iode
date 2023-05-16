7f7a883611767adfb7d381751f0557b131bc98dc
alixdamman <ald@plan.be>
2023-05-16 16:17:00 +0200

    GUI: (QIodeEditTableModel) added computeHash(), appendLine(), insertLine() and removeRows() methods

--- gui/iode_objs/edit/edit_table_model.h
+++ gui/iode_objs/edit/edit_table_model.h
@@ -11,12 +11,25 @@
 #include "utils.h"
 
 
+/* NOTE FOR THE DEVELOPPERS:
+ * 
+ * Read-only model: https://doc.qt.io/qt-6/model-view-programming.html#read-only-access
+ * Editable model:  https://doc.qt.io/qt-6/model-view-programming.html#editable-items
+ * Resizable model: https://doc.qt.io/qt-6/model-view-programming.html#resizable-models
+ * 
+ */
+
 class QIodeEditTableModel : public QAbstractTableModel
 {
 	Q_OBJECT
 
 	QString tableName;
     Table* table;
+	size_t hashBefore;
+	size_t hashAfter;	
+
+signals:
+	void databaseModified();
 
 public:
     QIodeEditTableModel(const QString& tableName, QWidget* parent = Q_NULLPTR);
@@ -39,7 +52,26 @@ public:
 
 	void save();
 
+	void computeHash(const bool before=false)
+	{
+		boost::hash<TBL> table_hasher;
+		if(before)
+    		hashBefore = table_hasher(*table->c_table);
+		else
+		{
+			hashAfter = table_hasher(*table->c_table);
+			if(hashAfter != hashBefore) 
+				emit databaseModified();
+		}
+	}
+
+	int appendLine(EnumLineType lineType);
+
+	int insertLine(EnumLineType lineType, const int position, const bool after);
+
 protected:
+	bool removeRows(int position, int rows, const QModelIndex& index = QModelIndex()) override;
+
 	void resetModel()
 	{
 		beginResetModel();
