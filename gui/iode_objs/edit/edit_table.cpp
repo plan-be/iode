63fcb1b2766d1b83987ee718e39b893ce0427169
alixdamman <ald@plan.be>
2023-05-16 16:18:13 +0200

    GUI: (QIodeEditTable) added and implemented insert_line() and delete_line() slots
    fix #224

--- gui/iode_objs/edit/edit_table.cpp
+++ gui/iode_objs/edit/edit_table.cpp
@@ -1,11 +1,36 @@
 #include "edit_table.h"
 
 
-QIodeEditTable::QIodeEditTable(const QString& tableName, QWidget* parent, Qt::WindowFlags f) : QDialog(parent, f)
+QIodeEditTable::QIodeEditTable(const QString& tableName, QWidget* parent, Qt::WindowFlags f) 
+	: QIodeSettings(parent, f)
 {
 	setupUi(this);
 
 	tableView->setupModel(tableName);
+
+	for(const auto& [line_type, _] : mLineType) 
+		list_insert_types << QString::fromStdString(line_type);
+
+	wInsertLineType = new WrapperComboBox("line_type", *comboBox_insert_line_type, REQUIRED_FIELD, list_insert_types);
+	wInsertWhere = new WrapperComboBox("where", *comboBox_insert_where, REQUIRED_FIELD, list_insert_where);
+
+	mapFields["InsertLineType"]  = wInsertLineType;
+	mapFields["InsertWhere"]     = wInsertWhere;
+
+	shortcutDelete = new QShortcut(QKeySequence(Qt::Key_Delete), this);
+	shortcutDelete->setContext(Qt::WidgetWithChildrenShortcut);
+
+	connect(shortcutDelete, &QShortcut::activated, this, &QIodeEditTable::delete_line);
+
+	loadSettings();
+}
+
+QIodeEditTable::~QIodeEditTable()
+{
+	delete wInsertLineType;
+	delete wInsertWhere;
+
+	delete shortcutDelete;
 }
 
 void QIodeEditTable::edit()
@@ -17,12 +42,73 @@ void QIodeEditTable::edit()
 	}
 	catch (const std::exception& e)
 	{
-		QMessageBox::warning(static_cast<QWidget*>(parent()), tr("WARNING"), tr(e.what()));
+		QMessageBox::warning(static_cast<QWidget*>(parent()), "WARNING", QString(e.what()));
+	}
+}
+
+void QIodeEditTable::insert_line()
+{
+	try
+	{
+		QIodeEditTableModel* tables_model = static_cast<QIodeEditTableModel*>(tableView->model());
+		QModelIndexList selection = tableView->selectionModel()->selectedRows();
+		int position = (selection.count() > 0) ? selection[0].row() : -1;
+
+		int insertLineType = wInsertLineType->extractAndVerify();
+		std::string s_lineType = list_insert_types[insertLineType].toStdString();
+		EnumLineType lineType = mLineType.at(s_lineType);
+
+		int insertWhere = wInsertWhere->extractAndVerify();
+
+		switch(insertWhere)
+		{
+		case INSERT_AT_THE_END:
+			tables_model->appendLine(lineType);
+			break;
+		case INSERT_AFTER_CURRENT:
+			if(position < 0)
+				return;
+			tables_model->insertLine(lineType, position, true);
+			break;
+		case INSERT_BEFORE_CURRENT:
+			if(position < 0)
+				return;
+			tables_model->insertLine(lineType, position, false);
+			break;
+		default:
+			break;
+		}
+	}
+	catch (const std::exception& e)
+	{
+		QMessageBox::warning(static_cast<QWidget*>(parent()), "WARNING", QString(e.what()));
+	}
+}
+
+void QIodeEditTable::delete_line()
+{
+	try
+	{
+		QIodeEditTableModel* tables_model = static_cast<QIodeEditTableModel*>(tableView->model());
+		QModelIndexList selection = tableView->selectionModel()->selectedRows();
+		if(selection.count() == 0)
+			return;
+
+    	int row = selection[0].row();
+		// if diviser line selected -> do nothing
+		if(row == 0)
+			return;
+		
+    	tables_model->removeRow(row, QModelIndex());
+    	update();
+	}
+	catch (const std::exception& e)
+	{
+		QMessageBox::warning(static_cast<QWidget*>(parent()), "WARNING", QString(e.what()));
 	}
 }
 
 void QIodeEditTable::help()
 {
-	
 	QDesktopServices::openUrl(url_manual);
 }
--- gui/iode_objs/edit/edit_table.h
+++ gui/iode_objs/edit/edit_table.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#include <QList>
 #include <QWidget>
 #include <QString>
+#include <QShortcut>
 
 #include <string>
 
 #include "ui_edit_table.h"
 #include "utils.h"
+#include "settings.h"
 #include "wrapper_classes.h"
+
+
+enum EnumInsertWhere
+{
+    INSERT_AT_THE_END,
+    INSERT_AFTER_CURRENT,
+    INSERT_BEFORE_CURRENT
+};
+
+const static QList<QString> list_insert_where = {"At the end", "After current", "Before current"};
 
 
 /* NOTE FOR THE DEVELOPERS:
@@ -16,14 +29,24 @@
  * 2. call the Q_OBJECT macro at the beginning of the class to allow slots and signals (see documentation of Qt).
  */
 
-class QIodeEditTable : public QDialog, public Ui::QIodeEditTable
+class QIodeEditTable : public QIodeSettings, public Ui::QIodeEditTable
 {
     Q_OBJECT
 
+    QShortcut* shortcutDelete;
+
+    WrapperComboBox* wInsertLineType;
+    WrapperComboBox* wInsertWhere;
+
+    QList<QString> list_insert_types;
+
 public:
     QIodeEditTable(const QString& tableName, QWidget* parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
+    ~QIodeEditTable();
 
 public slots:
     void edit();
+    void insert_line();
+    void delete_line();
     void help();
 };
