30787aa7a39e1b9e983c21448ea6265d70f5bc74
alixdamman <ald@plan.be>
2023-05-29 17:09:46 +0200

    TESTS: removed exedir.txt

--- tests/cmd/unittests1.bat
+++ tests/cmd/unittests1.bat
@@ -1,9 +1,7 @@
 @echo %*
 @echo off                            > nul
 @echo ============================   >> unittests.log
-@echo %*                             >>  unittests.log
+@echo %*                             >> unittests.log
 @echo ----------------------------   >> unittests.log
-set exepath=..\..
-if exist ..\exedir.txt set /P exepath=<..\exedir.txt
-set exepath=%exepath:/=\%\cmd
-%exepath%\iodecmd -y -alloclog test.log %*     >> unittests.log
+
+iodecmd -y -alloclog test.log %*     >> unittests.log
