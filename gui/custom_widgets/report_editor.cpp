fadd8bc354ca15df01684417fccc7ce92168e2a9
alixdamman <ald@plan.be>
2023-06-29 16:04:06 +0200

    GUI: (ReportEditor) Do no longer create a dummy report file before to run a report

--- gui/custom_widgets/report_editor.cpp
+++ gui/custom_widgets/report_editor.cpp
@@ -16,7 +16,6 @@ void ReportEditor::run(const QString& filepath, const QString& parameters, const
     QString msg;
     int success = -1;
 
-    QString dummyFilepath;
     QString currentProjectDir = QDir::currentPath();
 
     try
@@ -28,22 +27,8 @@ void ReportEditor::run(const QString& filepath, const QString& parameters, const
             output->setTextColor(Qt::black);
         }
 
-        // Save the current content of the report tab because the user may have 
-        // made some modifications in the report editor which are not saved yet.
-        QDir reportDir = QFileInfo(filepath).absoluteDir();
-        dummyFilepath = reportDir.absoluteFilePath(TMP_FILENAME + ".rep");
-        QFile file(dummyFilepath);
-        if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
-        {
-            QMessageBox::critical(nullptr, "ERROR", file.errorString());
-            return;
-        }
-        QTextStream out(&file);
-        out << this->toPlainText() << "\n";
-        file.close();
-
         // see function C_ReportExec() from the file sb_rep.c from the old GUI. 
-        setPrintDest(dummyFilepath, 'D');
+        setPrintDest(filepath, 'D');
         
         // set the number of decimals
         setNbDecimals(nbDecimals);
@@ -52,10 +37,10 @@ void ReportEditor::run(const QString& filepath, const QString& parameters, const
         setLang(language);
 
         // updates current directory execution (chdir)
-        QDir::setCurrent(reportDir.absolutePath());
+        QDir::setCurrent(QFileInfo(filepath).absolutePath());
 
         // executes IODE report
-        QString args = dummyFilepath + " " + parameters;
+        QString args = filepath + " " + parameters;
         success = B_ReportExec(args.toStdString().data());
 
         W_close();
@@ -63,20 +48,12 @@ void ReportEditor::run(const QString& filepath, const QString& parameters, const
         // reset current directory execution to project directory (chdir)
         QDir::setCurrent(currentProjectDir);
 
-        // removes the temporary file
-        file.remove();
-
         // display error if any
         if(success != 0)
             B_display_last_error();
     }
     catch(const std::exception& e)
-    {
-        // deletes dummy file if any
-        QFile file(dummyFilepath);
-        if(file.exists())
-            file.remove();
-        
+    {        
         // reset current directory execution to project directory (chdir)
         QDir::setCurrent(currentProjectDir);
 
