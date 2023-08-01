345fadae995f52d041aee5d0ecb0d09395bf75a0
alixdamman <ald@plan.be>
2023-08-01 10:17:30 +0200

    GUI:     - (IdentitiesView) Added the executeCurrentIdentity() slot method (connected to the F7 shortcut)     - (IdentitiesModel) Added the executeIdentity() method
    fix #306

--- gui/iode_objs/views/identities_view.cpp
+++ gui/iode_objs/views/identities_view.cpp
@@ -16,3 +16,22 @@ void IdentitiesView::new_obj()
 		emit newObjectInserted();
 	filter_and_update();
 }
+
+void IdentitiesView::executeCurrentIdentity()
+{
+	// get the selected object
+	QModelIndexList selection = this->selectionModel()->selectedRows();
+	if(selection.size() == 0) 
+		return;
+	QModelIndex index = selection[0];
+
+	// check if global sample is defined
+	if(!checkGlobalSample())
+		return;
+
+	// computes the selected identity + updates other IODE objects tabs is necessary
+	MainWindowPlot* main_window = static_cast<MainWindowPlot*>(get_main_window_ptr());
+	main_window->computeHash(true);
+	static_cast<IdentitiesModel*>(model())->executeIdentity(index.row());	
+	main_window->update_tab_and_completer();	
+}
--- gui/iode_objs/views/identities_view.h
+++ gui/iode_objs/views/identities_view.h
@@ -14,24 +14,37 @@ class IdentitiesView : public TemplateTableView<IdentitiesModel>
 {
 	Q_OBJECT
 
-	QShortcut* executeShortcut;
+	QShortcut* shortcutExecuteCurrentIdt;
+	QShortcut* shortcutExecuteIdts;
 
 public:
 	IdentitiesView(QWidget* parent = nullptr) : TemplateTableView(I_IDENTITIES, new IdentitiesDelegate(parent), parent) 
 	{
 		// ---- keyboard shortcuts ----
-		executeShortcut = new QShortcut(QKeySequence(Qt::Key_F7), this);
-		executeShortcut->setContext(Qt::WidgetWithChildrenShortcut);
+		shortcutExecuteCurrentIdt = new QShortcut(QKeySequence(Qt::Key_F7), this);
+		shortcutExecuteIdts = new QShortcut(QKeySequence(Qt::SHIFT | Qt::Key_F7), this);
+
+		shortcutExecuteCurrentIdt->setContext(Qt::WidgetWithChildrenShortcut);
+		shortcutExecuteIdts->setContext(Qt::WidgetWithChildrenShortcut);
 	};
+
+	~IdentitiesView()
+	{
+		delete shortcutExecuteCurrentIdt;
+		delete shortcutExecuteIdts;
+	}
 
 	void setup() override
 	{
+		connect(shortcutExecuteCurrentIdt, &QShortcut::activated, this, &IdentitiesView::executeCurrentIdentity);
+
 		MainWindowPlot* main_window = static_cast<MainWindowPlot*>(get_main_window_ptr());
-		connect(executeShortcut, &QShortcut::activated, main_window, &MainWindowPlot::open_compute_identities_dialog);
+		connect(shortcutExecuteIdts, &QShortcut::activated, main_window, &MainWindowPlot::open_compute_identities_dialog);
 	}
 
 public slots:
 	void filter() { filter_and_update(); }
 	void print();
 	void new_obj();
+	void executeCurrentIdentity();
 };
