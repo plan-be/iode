b539f493af6d55edac56f73911dd4d64ee733540
Jean-Marc Paul <jmarc.paul@gmail.com>
2023-10-11 00:38:14 +0200

    api/iode.h: version reset to 7.0
    api/iodebase.h:
        - declarations of new report functions:
            - int RP_silent(char* arg);
            - U_ch *RPF_IodeVersion(U_ch** args);
            - U_ch* RPF_SimSortNbPasses();
            - U_ch* RPF_SimSortAlgo();
            - U_ch* RPF_SimInitValues();
            - IODE_REAL RPF_SimNormReal(U_ch** args);
            - int RPF_SimNIterInt(U_ch** args);
            - int RPF_SimCpuInt(U_ch** args);
            - U_ch* RPF_SimCpu(U_ch** args);
            - U_ch* RPF_SimCpuSCC();
            - U_ch* RPF_SimCpuSort();
        - signature of T_view_tbl() reset
        - declaration of T_view_tbl_super reset
        - added in comments pro memory the temporarily changed signature of T_view_tbl to T_view_tbl(char* name, char *smpl, char** vars_names)
        - idem for T_view_tbl_super()
        - added declaration of W_printfReplEsc(char* fmt, ...)

--- api/iode.h
+++ api/iode.h
@@ -45,14 +45,14 @@
 #define MAXFLOAT  FLT_MAX
 #define MINFLOAT  FLT_MIN
 #endif
-
+ 
 
 //#include "o_objs.h" // JMP 8/12/2011
 
 /******************************* DEFINES **********************************/
-#define IODE_VERSION "IODE Modeling Software 6.70 - (c) 1990-2023 Federal Planning Bureau - Brussels"
-#define IODE_VERSION_MAJOR 6
-#define IODE_VERSION_MINOR 70
+#define IODE_VERSION "IODE Modeling Software 7.0 - (c) 1990-2023 Federal Planning Bureau - Brussels"
+#define IODE_VERSION_MAJOR 7
+#define IODE_VERSION_MINOR 0
 #define K_VERSION  "1.0"
 #define OK_MAX_NAME  10
 #define K_MAX_NAME   20  /* IODE64K */
