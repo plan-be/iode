0ca890af09402dee1644b356fac3a1e1045faabf
alixdamman <ald@plan.be>
2023-10-20 15:49:19 +0200

    PYTHON: (nanobind) added the Python function eqs_estimate()

--- python_api/pyiode.cpp
+++ python_api/pyiode.cpp
@@ -6,9 +6,9 @@
 namespace nb = nanobind;
 
 #include "cpp_api/iode_cpp_api.h"
+#include "py_numpy.h"
 #include "py_ws.h"
 #include "py_sample.h"
-#include "py_numpy.h"
 
 
 NB_MODULE(iode, m) 
@@ -222,4 +222,13 @@ NB_MODULE(iode, m)
             nb::arg("commands"));
       m.def("reportline_exec", nb::overload_cast<const std::vector<std::string>&>(&execute_report_line), 
             nb::arg("commands"));
+
+      // Estimation
+      doc = "Estimate an equation or a block of equations on the given sample.\n";
+      doc += "The estimation method and the instruments must be specified in the equation before the estimation.";
+      m.def("eqs_estimate", nb::overload_cast<const std::string&, const std::string&, const std::string&>(&eqs_estimate), 
+            nb::arg("eqs"), nb::arg("from") = "", nb::arg("to") = "", doc.c_str());
+      m.def("eqs_estimate", nb::overload_cast<const std::vector<std::string>&, const std::string&, const std::string&>(&eqs_estimate), 
+            nb::arg("eqs"), nb::arg("from") = "", nb::arg("to") = "", doc.c_str());
+
 }
