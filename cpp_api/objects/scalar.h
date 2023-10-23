3014ee48d0502fb78ffc3ff919e7c55d699c7f20
alixdamman <ald@plan.be>
2023-10-23 15:04:13 +0200

    C++ API: (Scalar) made the C++ struct Scalar to inherit from the C struct SCL

--- cpp_api/objects/scalar.h
+++ cpp_api/objects/scalar.h
@@ -4,11 +4,8 @@
 #include <boost/functional/hash.hpp>
 
 
-struct Scalar
+struct Scalar : public SCL
 {
-    SCL* c_scalar;
-    bool allocated;
-
 public:
     Scalar();
 
@@ -18,20 +15,12 @@ public:
 
     Scalar(const IODE_REAL value, const IODE_REAL relax=1.0, const IODE_REAL std=L_NAN);
 
-    Scalar(const Scalar& scl);
-
-    ~Scalar();
+    Scalar(const Scalar& scalar);
 
     // required to be used in std::map
-    Scalar& operator=(const Scalar& scl);
+    Scalar& operator=(const Scalar& scalar);
 
     bool operator==(const Scalar& other) const;
-
-    IODE_REAL& value() { return c_scalar->val; }
-
-    IODE_REAL& relax() { return c_scalar->relax; }
-
-    IODE_REAL& std() { return c_scalar->std; }
 };
 
 
@@ -43,4 +32,14 @@ public:
  * 
  * @return std::size_t 
  */
-std::size_t hash_value(SCL const& c_scalar);
+std::size_t hash_value(Scalar const& scalar);
+
+/**
+ * @brief compute a hash value for an object of type SCL (C API).
+ * 
+ * @note see https://www.boost.org/doc/libs/1_55_0/doc/html/hash/custom.html
+ *       and https://www.boost.org/doc/libs/1_55_0/doc/html/hash/combine.html
+ * 
+ * @return std::size_t 
+ */
+std::size_t hash_value(SCL const& scl);
