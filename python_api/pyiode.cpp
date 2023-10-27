ce4325cd660d2a65cb0794ac1b639f9238d9aee6
alixdamman <ald@plan.be>
2023-10-27 11:54:13 +0200

    PYTHON: (nanobind) moved IODE WS functions to files py_ws.h/cpp

--- python_api/pyiode.cpp
+++ python_api/pyiode.cpp
@@ -22,69 +22,7 @@ NB_MODULE(iode, m)
       std::string doc;
 
       init_constants(m);
-
-      // Workspace functions
-
-      m.def("ws_content", &ws_content, nb::arg("pattern"), nb::arg("iode_type") = (int) I_VARIABLES, 
-            "Return the names of objects of a given IODE data type, satisfying a pattern specification.");
-      m.def("ws_content_cmt", [](const std::string& pattern){ return ws_content(pattern, I_COMMENTS); }, nb::arg("pattern"),
-            "Return the list of comment names corresponding to the given pattern");
-      m.def("ws_content_eqs", [](const std::string& pattern){ return ws_content(pattern, I_EQUATIONS); }, nb::arg("pattern"),
-            "Return the list of equation names corresponding to the given pattern");
-      m.def("ws_content_idt", [](const std::string& pattern){ return ws_content(pattern, I_IDENTITIES); }, nb::arg("pattern"),
-            "Return the list of identity names corresponding to the given pattern");
-      m.def("ws_content_lst", [](const std::string& pattern){ return ws_content(pattern, I_LISTS); }, nb::arg("pattern"),
-            "Return the list of list names corresponding to the given pattern");
-      m.def("ws_content_scl", [](const std::string& pattern){ return ws_content(pattern, I_SCALARS); }, nb::arg("pattern"),
-            "Return the list of scalar names corresponding to the given pattern");
-      m.def("ws_content_tbl", [](const std::string& pattern){ return ws_content(pattern, I_TABLES); }, nb::arg("pattern"),
-            "Return the list of table names corresponding to the given pattern");
-      m.def("ws_content_var", [](const std::string& pattern){ return ws_content(pattern, I_VARIABLES); }, nb::arg("pattern"),
-            "Return the list of variable names corresponding to the given pattern");
-
-      m.def("ws_clear_all", &ws_clear_all, "Clear all workspaces");
-      m.def("ws_clear", &ws_clear, nb::arg("iode_type"), "Clear the workspace for a given IODE data type");
-      m.def("ws_clear_cmt", [](){ Comments.clear(); },   "Clear the workspace of comments");
-      m.def("ws_clear_eqs", [](){ Equations.clear(); },  "Clear the workspace of equations");
-      m.def("ws_clear_idt", [](){ Identities.clear(); }, "Clear the workspace of identities");
-      m.def("ws_clear_lst", [](){ Lists.clear(); },      "Clear the workspace of lists");
-      m.def("ws_clear_scl", [](){ Scalars.clear(); },    "Clear the workspace of scalars");
-      m.def("ws_clear_tbl", [](){ Tables.clear(); },     "Clear the workspace of tables");
-      m.def("ws_clear_var", [](){ Variables.clear(); },  "Clear the workspace of variables");
-
-      m.def("ws_load", &ws_load, nb::arg("filename"), nb::arg("iode_type") = -1, 
-            "Load an IODE file and return the number of read objects");
-      m.def("ws_load_cmt", [](const std::string& filename){ Comments.load(filename);   return Comments.count(); }, nb::arg("filename"), 
-            "Load a comment file and return the number of read objects");
-      m.def("ws_load_eqs", [](const std::string& filename){ Equations.load(filename);  return Equations.count(); }, nb::arg("filename"), 
-            "Load an equation file and return the number of read objects");
-      m.def("ws_load_idt", [](const std::string& filename){ Identities.load(filename); return Identities.count(); }, nb::arg("filename"), 
-            "Load a identity file and return the number of read objects");
-      m.def("ws_load_lst", [](const std::string& filename){ Lists.load(filename);      return Lists.count(); }, nb::arg("filename"), 
-            "Load a list file and return the number of read objects");
-      m.def("ws_load_scl", [](const std::string& filename){ Scalars.load(filename);    return Scalars.count(); }, nb::arg("filename"), 
-            "Load a scalar file and return the number of read objects");
-      m.def("ws_load_tbl", [](const std::string& filename){ Tables.load(filename);     return Tables.count(); }, nb::arg("filename"), 
-            "Load a table file and return the number of read objects");
-      m.def("ws_load_var", [](const std::string& filename){ Variables.load(filename);  return Variables.count(); }, nb::arg("filename"), 
-            "Load a variable file and return the number of read objects");
-
-      m.def("ws_save", &ws_save, nb::arg("filename"), nb::arg("iode_type"), 
-            "Save the current IODE workspace of a given IODE data type");
-      m.def("ws_save_cmt", [](const std::string& filename){ Comments.save(filename); },   nb::arg("filename"), 
-            "Save the current comments workspace");
-      m.def("ws_save_eqs", [](const std::string& filename){ Equations.save(filename); },  nb::arg("filename"), 
-            "Save the current equations workspace");
-      m.def("ws_save_idt", [](const std::string& filename){ Identities.save(filename); }, nb::arg("filename"), 
-            "Save the current identities workspace");
-      m.def("ws_save_lst", [](const std::string& filename){ Lists.save(filename); },      nb::arg("filename"), 
-            "Save the current lists workspace");
-      m.def("ws_save_scl", [](const std::string& filename){ Scalars.save(filename); },    nb::arg("filename"), 
-            "Save the current scalars workspace");
-      m.def("ws_save_tbl", [](const std::string& filename){ Tables.save(filename); },     nb::arg("filename"), 
-            "Save the current tables workspace");
-      m.def("ws_save_var", [](const std::string& filename){ Variables.save(filename); },  nb::arg("filename"), 
-            "Save the current variables workspace");
+      init_ws(m);
 
       // IODE object classes
 
@@ -165,46 +103,6 @@ NB_MODULE(iode, m)
       m.def("exec_lec", nb::overload_cast<const std::string&, const int>(&execute_lec), nb::arg("lec"), nb::arg("t"));
       m.def("exec_lec", nb::overload_cast<const std::string&, const std::string&>(&execute_lec), nb::arg("lec"), nb::arg("period"));
       m.def("exec_lec", nb::overload_cast<const std::string&>(&execute_lec), nb::arg("lec"));
-
-      // High To Low functions
-
-      m.def("ws_htol", nb::overload_cast<const std::string&, const std::string&, const int>(&ws_htol), 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("series_type"));
-      m.def("ws_htol_last", [](const std::string& filename, const std::string& varlist) { ws_htol(filename, varlist, (int) EnumIodeHtoL::HTOL_LAST); }, 
-            nb::arg("filename"), nb::arg("varlist"));
-      m.def("ws_htol_mean", [](const std::string& filename, const std::string& varlist) { ws_htol(filename, varlist, (int) EnumIodeHtoL::HTOL_MEAN); }, 
-            nb::arg("filename"), nb::arg("varlist"));
-      m.def("ws_htol_sum",  [](const std::string& filename, const std::string& varlist) { ws_htol(filename, varlist, (int) EnumIodeHtoL::HTOL_SUM); }, 
-            nb::arg("filename"), nb::arg("varlist"));
-
-      m.def("ws_htol", nb::overload_cast<const std::string&, const std::vector<std::string>&, const int>(&ws_htol), 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("series_type"));
-      m.def("ws_htol_last", [](const std::string& filename, const std::vector<std::string>& varlist) { ws_htol(filename, varlist, (int) EnumIodeHtoL::HTOL_LAST); }, 
-            nb::arg("filename"), nb::arg("varlist"));
-      m.def("ws_htol_mean", [](const std::string& filename, const std::vector<std::string>& varlist) { ws_htol(filename, varlist, (int) EnumIodeHtoL::HTOL_MEAN); }, 
-            nb::arg("filename"), nb::arg("varlist"));
-      m.def("ws_htol_sum",  [](const std::string& filename, const std::vector<std::string>& varlist) { ws_htol(filename, varlist, (int) EnumIodeHtoL::HTOL_SUM); }, 
-            nb::arg("filename"), nb::arg("varlist"));
-
-      // Low To High functions
-
-      m.def("ws_ltoh", nb::overload_cast<const std::string&, const std::string&, const int, const char>(&ws_ltoh), 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("series_type"), nb::arg("method"));
-      m.def("ws_ltoh_flow",  [](const std::string& filename, const std::string& varlist, const char method = 'C') 
-            { ws_ltoh(filename, varlist, EnumIodeLtoH::LTOH_FLOW, method); }, 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("method") = 'C');
-      m.def("ws_ltoh_stock", [](const std::string& filename, const std::string& varlist, const char method = 'C') 
-            { ws_ltoh(filename, varlist, EnumIodeLtoH::LTOH_STOCK, method); }, 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("method") = 'C');
-
-      m.def("ws_ltoh", nb::overload_cast<const std::string&, const std::vector<std::string>&, const int, const char>(&ws_ltoh), 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("series_type"), nb::arg("method"));
-      m.def("ws_ltoh_flow",  [](const std::string& filename, const std::vector<std::string>& varlist, const char method = 'C') 
-            { ws_ltoh(filename, varlist, EnumIodeLtoH::LTOH_FLOW, method); }, 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("method") = 'C');
-      m.def("ws_ltoh_stock", [](const std::string& filename, const std::vector<std::string>& varlist, const char method = 'C') 
-            { ws_ltoh(filename, varlist, EnumIodeLtoH::LTOH_STOCK, method); }, 
-            nb::arg("filename"), nb::arg("varlist"), nb::arg("method") = 'C');
 
       // Sample
 
