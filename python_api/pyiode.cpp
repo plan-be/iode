675fe31c4041083a61060255b47edbc869d345a0
alixdamman <ald@plan.be>
2023-10-27 10:16:39 +0200

    PYTHON: (nanobind) Exported setter functions for Comment, Identity, List, Scalar and Variable objects

--- python_api/pyiode.cpp
+++ python_api/pyiode.cpp
@@ -143,21 +143,56 @@ NB_MODULE(iode, m)
             .def(nb::self == nb::self)
             .def("__repr__", &Scalar::to_string);
 
-      // IODE objects getters and setters
+      // IODE objects getters
 
-      m.def("get_cmt", [](const std::string& name) { return Comments.get(name); }, nb::arg("name"),          "get an IODE comment");
-      // m.def("get_eqs", [](const std::string& name) { return Equations.get(name); }, nb::arg("name"),      "get an IODE equation");
-      m.def("get_eqs_lec", [](const std::string& name) { return Equations.get_lec(name); }, nb::arg("name"), "get LEC of an equation");
-      m.def("get_idt", [](const std::string& name) { return Identities.get_lec(name); }, nb::arg("name"),        "get an IODE identity");
-      m.def("get_lst", [](const std::string& name) { return Lists.get(name); }, nb::arg("name"),             "get an IODE list");
-      m.def("get_scl", [](const std::string& name) { return Scalars.get(name); }, nb::arg("name"),        "get an IODE scalar");
-      // m.def("get_tbl", [](const std::string& name) { return Tables.get(name); }, nb::arg("name"),         "get an IODE table");
-      m.def("get_var", [](const std::string& name) { return Variables.get(name); }, nb::arg("name"),         "get an IODE variable");
+      m.def("get_cmt", [](const std::string& name) { return Comments.get(name); }, nb::arg("name"), "Get an IODE comment");
+      // m.def("get_eqs", [](const std::string& name) { return Equations.get(name); }, nb::arg("name"), "Get an IODE equation");
+      m.def("get_idt", [](const std::string& name) { return Identities.get_lec(name); }, nb::arg("name"),"Get an IODE identity");
+      m.def("get_lst", [](const std::string& name) { return Lists.get(name); }, nb::arg("name"), "Get an IODE list");
+      m.def("get_scl", [](const std::string& name) { return Scalars.get(name); }, nb::arg("name"), "Get an IODE scalar");
+      // m.def("get_tbl", [](const std::string& name) { return Tables.get(name); }, nb::arg("name"),   "Get an IODE table");
+      m.def("get_var", [](const std::string& name) { return Variables.get(name); }, nb::arg("name"), "Get an IODE variable");
+      
+      // IODE objects setters
 
-      m.def("get_var_as_ndarray", &get_var_as_ndarray, nb::rv_policy::reference, nb::arg("name"), 
-            "Get an IODE variable in a numpy ndarray");
-      m.def("get_var_as_ndarray_copy", &get_var_as_ndarray, nb::rv_policy::copy, nb::arg("name"), 
-            "Get a copy of an IODE variable in a numpy ndarray");
+      m.def("set_cmt", [](const std::string& name, const Comment& cmt) { 
+                  if(!Comments.contains(name)) Comments.add(name, cmt); 
+                  else Comments.update(name, cmt); 
+            }, nb::arg("name"), nb::arg("cmt"), "Add or create an IODE comment");
+      // m.def("set_eqs", [](const std::string& name, const Equation& eq) { 
+      //            if(!Equations.contains(name)) Equations.add(name, eq); 
+      //            else Equations.update(name, eq); 
+      //      }, nb::arg("name"), nb::arg("eq"), "Get an IODE equation");
+      m.def("set_idt", [](const std::string& name, const std::string& idt) { 
+                  if(!Identities.contains(name)) Identities.add(name, idt); 
+                  else Identities.update(name, idt); 
+            }, nb::arg("name"), nb::arg("idt"), "Add or create an IODE identity");
+      m.def("set_lst", [](const std::string& name, const List& lst) { 
+                  if(!Lists.contains(name)) Lists.add(name, lst); 
+                  else Lists.update(name, lst); 
+            }, nb::arg("name"), nb::arg("lst"), "Add or create an IODE list");
+      m.def("set_scl", [](const std::string& name, const Scalar& scl) { 
+                  if(!Scalars.contains(name)) Scalars.add(name, scl); 
+                  else Scalars.update(name, scl.val, scl.relax, scl.std); 
+            }, nb::arg("name"), nb::arg("scl"), "Add or create an IODE scalar");
+      // m.def("set_tbl", [](const std::string& name, const Table& tbl) { 
+      //            if(!Tables.contains(name)) Tables.add(name, tbl); 
+      //            else Tables.update(name, tbl); 
+      //         }, nb::arg("name"), nb::arg("tbl"), "Add or create an IODE table");
+      m.def("set_var", [](const std::string& name, const Variable& var) { 
+                  if(!Variables.contains(name)) Variables.add(name, var); 
+                  else Variables.update(name, var);  
+            }, nb::arg("name"), nb::arg("var"), "Add or create an IODE variable");
+
+      // Equations
+
+      m.def("get_eqs_lec", [](const std::string& name) { return Equations.get_lec(name); }, nb::arg("name"),  "Get LEC of an equation");
+      m.def("set_eqs_lec", [](const std::string& name) { return Equations.get_lec(name); }, nb::arg("name"),  "Update LEC of an equation");
+
+      // Variables
+
+      m.def("get_var_as_ndarray", &get_var_as_ndarray, nb::rv_policy::reference, nb::arg("name"),     "Get an IODE variable in a numpy ndarray");
+      m.def("get_var_as_ndarray_copy", &get_var_as_ndarray, nb::rv_policy::copy, nb::arg("name"),     "Get a copy of an IODE variable in a numpy ndarray");
 
       // IODE objects delete
 
