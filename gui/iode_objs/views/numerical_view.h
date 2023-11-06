ad4e61ab347eccd71ea3eacf5bd8fe5d570aade5
alixdamman <ald@plan.be>
2023-11-06 14:08:47 +0100

    GUI: (TemplateNumericalTableView) added new slot method paste_vars() to copy variable name + values from Excel to Iode
    fix #381

--- gui/iode_objs/views/numerical_view.h
+++ gui/iode_objs/views/numerical_view.h
@@ -45,6 +45,7 @@ class _NUMERICAL_VIEW_CLASS_NAME_ : public IodeAbstractTableView
     QShortcut* shortcutCopy;
     QShortcut* shortcutCopyIncludingHeaders;
     QShortcut* shortcutPast;
+    QShortcut* shortcutPastVars;
 
 protected:
     QMenu* contextMenu;
@@ -81,6 +82,9 @@ private:
 #ifndef _GSAMPLE_
         action = addAction("Paste", "Paste values contained in the clipboard", shortcutPast->key());
         QObject::connect(action, &QAction::triggered, this, &_NUMERICAL_VIEW_CLASS_NAME_::paste);
+
+        action = addAction("Paste Special", "Paste values according to variable names and optionally periods", shortcutPastVars->key());
+        QObject::connect(action, &QAction::triggered, this, &_NUMERICAL_VIEW_CLASS_NAME_::paste_vars);        
 #endif
     }
 
@@ -104,6 +108,10 @@ public:
         shortcutPast = new QShortcut(QKeySequence::Paste, this);
         shortcutPast->setContext(Qt::WidgetWithChildrenShortcut);
         QObject::connect(shortcutPast, &QShortcut::activated, this, &_NUMERICAL_VIEW_CLASS_NAME_::paste);
+
+        shortcutPastVars = new QShortcut(QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_V), this);
+        shortcutPastVars->setContext(Qt::WidgetWithChildrenShortcut);
+        QObject::connect(shortcutPastVars, &QShortcut::activated, this, &_NUMERICAL_VIEW_CLASS_NAME_::paste_vars);
 #endif
 
         // context menu = popup menu displayed when a user right clicks
@@ -118,6 +126,7 @@ public:
         delete shortcutCopyIncludingHeaders;
 #ifndef _GSAMPLE_
         delete shortcutPast;
+        delete shortcutPastVars;
 #endif
     }
 
@@ -197,6 +206,84 @@ public:
             QMessageBox::warning(nullptr, "WARNING", "Can't paste values\n\n" + QString::fromStdString(e.what()));
         }
     }
+
+    void paste_vars()
+    {
+        try
+        {
+            QModelIndex start = this->selectionModel()->selectedIndexes().first();
+            QAbstractItemModel* model_ = this->model();
+
+            QClipboard* clipboard = QApplication::clipboard();
+            QString text = clipboard->text();
+
+            // If the text from clipboard ends with the new-line character (which is the case when we copy 
+            // from Excel), the QString::split() function returns a list ending by an empty element. 
+            // This empty element is then interpreted as a new NaN value by the setData function.
+            if(text.endsWith("\n"))
+                text.chop(1);
+
+            QList<QString> lines = text.split("\n");
+            if(lines.count() == 0)
+                return;
+
+            // ---- extract columns ----
+            int start_column;
+            QString from;
+            QString to;
+            Sample sample_vars = Variables.get_sample();
+            
+            // check if the first line represents the periods 
+            QList<QString> cells = lines[0].split("\t");
+            if(cells[0].isEmpty())
+            {
+                from = cells[1];
+                to = cells.last();
+                lines.removeFirst();
+            }
+            else
+                ask_sample(from, to);
+            
+            // check that the passed sample has the size equal to the number of cells per line.
+            // We compare to cells.count() -1 because the first cell is supposed to contains 
+            // the variable name, not a value
+            Sample sample(from.toStdString(), to.toStdString());
+            if(sample.nb_periods() != cells.count() - 1)
+            {
+                QMessageBox::warning(nullptr, "WARNING", QString("Can't paste values.\n") + 
+                    "The passed sample " + QString::fromStdString(sample.to_string()) + 
+                    " does not correspond to number of values per variable: '" + 
+                    QString::number(cells.count()) + "'");
+                return;
+            }
+            
+            start_column = sample_vars.get_period_position(from.toStdString());
+
+            int row=0; 
+            int column=0;
+            foreach(const QString& line, lines)
+            {
+                QList<QString> cells = line.split("\t");
+
+                std::string var_name = cells.takeFirst().toStdString();
+                // raises an error if the the variable name is not found
+                row = Variables.get_position(var_name);
+
+                column = start_column;
+                foreach(const QString& value, cells)
+                {
+                    QModelIndex index = model_->index(row, column);
+                    if(index.isValid())
+                        model_->setData(index, value, Qt::EditRole);
+                    column++;
+                }
+            }
+        }
+        catch(const std::exception& e)
+        {
+            QMessageBox::warning(nullptr, "WARNING", "Can't paste values\n\n" + QString::fromStdString(e.what()));
+        }
+    }
 #endif
 
 protected:
@@ -245,4 +332,23 @@ protected:
         
         return values;
     }
+
+#ifndef _GSAMPLE_
+    void ask_sample(QString& from, QString& to)
+    {
+        try
+        {
+            EditIodeSampleDialog dialog(this);
+            if (dialog.exec() == QDialog::Accepted)
+            {
+                from = dialog.get_from();
+                to = dialog.get_to();
+            }
+        }
+        catch(const std::exception& e)
+        {
+            QMessageBox::warning(nullptr, tr("WARNING"), tr(e.what()));
+        }
+    }
+#endif
 };
