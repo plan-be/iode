5682409d850b1bab771b745816b57d66c4dde64e
alixdamman <ald@plan.be>
2023-11-08 18:23:29 +0100

    TESTS C++: (IdentityTest) added CreateDeleteCopy unittest

--- tests/test_cpp_api/test_identity.cpp
+++ tests/test_cpp_api/test_identity.cpp
@@ -14,23 +14,39 @@ protected:
     {
         load_global_kdb(I_EQUATIONS, input_test_dir + "fun.eqs");
         load_global_kdb(I_IDENTITIES, input_test_dir + "fun.idt");
-    
-        idt_coeffs = new Identity(name_idt_coeffs, nullptr);
-        idt_vars = new Identity(name_idt_vars, nullptr);
     }
 
-    void TearDown() override
-    {
-        delete idt_coeffs;
-        delete idt_vars;
-    }
+    // void TearDown() override {}
 };
+
+
+TEST_F(IdentityTest, CreateDeleteCopy)
+{
+    load_global_kdb(I_SCALARS, input_test_dir + "fun.scl");
+    load_global_kdb(I_VARIABLES, input_test_dir + "fun.var");
+
+    Identity* idt = nullptr;
+    Identity* idt_copy = nullptr;
+    
+    idt = new Identity(0, nullptr);
+    delete idt;
+
+    idt = new Identity(name_idt_coeffs, nullptr);
+    delete idt;
+
+    idt = new Identity(name_idt_coeffs, nullptr);
+    idt_copy = new Identity(*idt);
+    delete idt_copy;
+    delete idt;
+}
 
 
 TEST_F(IdentityTest, GetCoefficients)
 {
+    Identity idt_coeffs(name_idt_coeffs, nullptr);
+
     std::vector<std::string> expected_coefs_list = {"gamma2", "gamma3", "gamma4", "gamma"};
-    std::vector<std::string> coefs_list = idt_coeffs->get_coefficients_list();
+    std::vector<std::string> coefs_list = idt_coeffs.get_coefficients_list();
     EXPECT_EQ(coefs_list, expected_coefs_list);
 
     // check that coeffs have been created
@@ -44,13 +60,15 @@ TEST_F(IdentityTest, GetCoefficients)
 
 TEST_F(IdentityTest, GetVariables)
 {
+    Identity idt_vars(name_idt_vars, nullptr);
+
     Equation eq("ACAF");
     Sample eq_sample = eq.get_sample();
     KDBVariables kdb_var;
     kdb_var.set_sample(eq_sample.start_period(), eq_sample.end_period());
 
     std::vector<std::string> expected_vars_list = {"WCRH", "QL", "VAFF", "VM", "PM"};
-    std::vector<std::string> vars_list = idt_vars->get_variables_list();
+    std::vector<std::string> vars_list = idt_vars.get_variables_list();
     EXPECT_EQ(vars_list, expected_vars_list);
 
     // check that variables have been created
