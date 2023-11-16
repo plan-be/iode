f8c626da755dfe98cff78937895161f4c3993877
alixdamman <ald@plan.be>
2023-11-16 14:20:19 +0100

    PYTHON: (nanobind) - constants: added constants associated with the IODE table objects

--- python_api/py_constants.cpp
+++ python_api/py_constants.cpp
@@ -38,6 +38,50 @@ void init_constants(nb::module_ &m)
       m.attr("IE_DW")      = (int) IE_DW;
       m.attr("IE_LOGLIK")  = (int) IE_LOGLIK;
 
+      // EnumCellType
+      m.attr("IT_LEC")    = (int) IT_LEC;
+      m.attr("IT_STRING") = (int) IT_STRING;
+
+      // EnumCellAttribute
+      m.attr("IT_NORMAL")    = (int) IT_NORMAL;
+      m.attr("IT_BOLD")      = (int) IT_BOLD;
+      m.attr("IT_ITALIC")    = (int) IT_ITALIC;
+      m.attr("IT_UNDERLINE") = (int) IT_UNDERLINE;
+      //-----------------
+      m.attr("IT_CENTER")  = (int) IT_CENTER;
+      m.attr("IT_LEFT")    = (int) IT_LEFT;
+      m.attr("IT_RIGHT")   = (int) IT_RIGHT;
+      m.attr("IT_DECIMAL") = (int) IT_DECIMAL;
+
+      // EnumLineType
+      m.attr("IT_FILES") = (int) IT_FILES;
+      m.attr("IT_MODE")  = (int) IT_MODE;
+      m.attr("IT_CELL")  = (int) IT_CELL;
+      m.attr("IT_LINE")  = (int) IT_LINE;
+      m.attr("IT_TITLE") = (int) IT_TITLE;
+      m.attr("IT_DATE")  = (int) IT_DATE;
+
+      // EnumGraphType
+      m.attr("IG_LINE")    = (int) IG_LINE;
+      m.attr("IG_SCATTER") = (int) IG_SCATTER;
+      m.attr("IG_BAR")     = (int) IG_BAR;
+
+      // EnumGraphGrid
+      m.attr("IG_MAJOR") = (int) IG_MAJOR;
+      m.attr("IG_NONE")  = (int) IG_NONE;
+      m.attr("IG_MINOR") = (int) IG_MINOR;
+
+      // EnumGraphAlign
+      m.attr("IG_LEFT")   = (int) IG_LEFT;
+      m.attr("IG_CENTER") = (int) IG_CENTER;
+      m.attr("IG_RIGHT")  = (int) IG_RIGHT;
+
+      // EnumGraphAxis
+      m.attr("IG_VALUES")  = (int) IG_VALUES;
+      m.attr("IG_LOG")     = (int) IG_LOG;
+      m.attr("IG_SEMILOG") = (int) IG_SEMILOG;
+      m.attr("IG_PERCENT") = (int) IG_PERCENT;
+
       // Simulation parameters
       m.attr("SORT_CONNEX") = (int) SORT_CONNEX;
       m.attr("SORT_BOTH")   = (int) SORT_BOTH;
