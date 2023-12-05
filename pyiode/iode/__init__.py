350155d1374ae2000ed759e37ad4a2ea09c39b2f
alixdamman <ald@plan.be>
2023-12-05 19:10:21 +0100

    PYTHON: import only public Python API in __init__.py

--- pyiode/iode/__init__.py
+++ pyiode/iode/__init__.py
@@ -1,1 +1,47 @@
 from ._version import __version__
+from .iode_python import (# ------ constants ------
+                          K_CMT, K_EQS, K_IDT, K_LST, K_SCL, K_TBL, K_VAR,  
+                          KV_INIT_ASIS, KV_INIT_EXTRA, KV_INIT_EXTRA_A, KV_INIT_EXTRA_NA,  
+                          KV_INIT_TM1, KV_INIT_TM1_A, KV_INIT_TM1_NA,  
+                          SORT_BOTH, SORT_CONNEX, SORT_NONE, 
+                          HTOL_LAST, HTOL_MEAN, HTOL_SUM,  
+                          LTOH_CS, LTOH_FLOW, LTOH_LIN, LTOH_STEP, LTOH_STOCK,  
+                          W_A2M, W_CSV, W_DUMMY, W_GDI, W_HTML, W_MIF, W_RTF,  
+                          # ------ classes ------ 
+                          Equation, List, Scalar,  
+                          # ------ functions ------ 
+                          # --- IODE objects ---
+                          get_cmt, get_eqs, get_eqs_lec, get_idt, get_lst, get_scl, get_var, get_var_as_ndarray, 
+                          set_cmt, set_eqs, set_idt, set_lst, set_scl, set_var,  
+                          data_update, data_update_cmt, data_update_eqs, data_update_idt,  
+                          data_update_lst, data_update_scl, data_update_var,  
+                          delete_cmt, delete_eqs, delete_idt, delete_lst, delete_obj,  
+                          delete_objects, delete_scl, delete_tbl, delete_var,  
+                          # --- workspace ---
+                          ws_load, ws_load_cmt, ws_load_eqs, ws_load_idt, ws_load_lst,  
+                          ws_load_scl, ws_load_tbl, ws_load_var,  
+                          ws_save, ws_save_cmt, ws_save_eqs, ws_save_idt, ws_save_lst,  
+                          ws_save_scl, ws_save_tbl, ws_save_var, ws_to_df,  
+                          ws_content, ws_content_cmt, ws_content_eqs, ws_content_idt,  
+                          ws_content_lst, ws_content_scl, ws_content_tbl, ws_content_var,  
+                          ws_clear, ws_clear_all, ws_clear_cmt, ws_clear_eqs, ws_clear_idt,  
+                          ws_clear_lst, ws_clear_scl, ws_clear_tbl, ws_clear_var,  
+                          df_to_ws, larray_to_ws, ws_to_larray,  
+                          # --- low to high vs high to low ---
+                          ws_htol, ws_htol_last, ws_htol_mean, ws_htol_sum,  
+                          ws_load_var_to_larray, ws_ltoh, ws_ltoh_flow, ws_ltoh_stock,  
+                          # --- sample ---
+                          ws_sample_get, ws_sample_nb_periods, ws_sample_set, ws_sample_to_list, 
+                          ws_sample_to_string, ws_sample_to_larray_axis, larray_get_sample, 
+                          # --- LEC ---
+                          exec_lec, idt_execute,  
+                          # --- estimate + simulate ---
+                          eqs_estimate, model_simulate,  
+                          # --- report ---
+                          report_exec, reportline_exec, 
+                          # --- write ---
+                          w_close, w_dest, w_flush, w_print, w_print_cmd, w_print_enum,  
+                          w_print_par, w_print_pg_footer, w_print_pg_header, w_print_tit,  
+                          # --- misc ---
+                          reset_msgs, suppress_msgs, version
+                          )
