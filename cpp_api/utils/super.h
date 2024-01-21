714e7c9c3b8ebb466687a984f418f9e6c8978d30
alixdamman <ald@plan.be>
2024-01-21 14:55:02 +0100

    C++ API: (super)     - implemented kwarning_super()

--- cpp_api/utils/super.h
+++ cpp_api/utils/super.h
@@ -2,8 +2,11 @@
 #include "common.h"
 #include "iode_exceptions.h"
 #include <iostream>
+#include <string>
 
 
 void cpp_panic_super();
+
+void cpp_warning(const char* msg);
 
 void cpp_assign_super_API();
