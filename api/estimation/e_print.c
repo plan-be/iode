6fa31ed36c79bbc18c87cff92a37ddc2a6007b0e
Jean-Marc Paul <jmarc.paul@gmail.com>
2024-02-19 01:26:46 +0100

    api/estimation/e_dftest.c:     - W_printf() replaced by W_printfRepl() when the arg contains the char '&': W_printfRepl() replaces '&' by the value defined in "iode.ini", section [A2MGNL]SEP (by default '@').
    api/estimation/e_print.c:
        - E_printf() replaced by W_printf() (or W_printfRepl() when and '&' is present in the text) : same as in e_dftest.c
        - E_BeginTbl() replaced by W_print_tb()

--- api/estimation/e_print.c
+++ api/estimation/e_print.c
@@ -11,9 +11,6 @@
 
 #include "iode.h"
 
-#define E_printf   W_printf
-#define E_BeginTbl W_print_tb
-
 // Declarations
 static void E_print_parms();
 static void E_print_eqs();
@@ -35,21 +32,21 @@ static void E_print_parms()
 {
     char    from[20], to[20];
 
-    E_BeginTbl("Parameters", 2);
+    W_print_tb("Parameters", 2);
 
     PER_pertoa(&(E_SMPL->s_p1), from);
     PER_pertoa(&(E_SMPL->s_p2), to);
 
-    E_printf("&1NEstimation period        &1L%s:%s\n", from, to);
-    E_printf("&1NEstimation method        &1L%c\n", "LZIG"[E_MET]);
-    E_printf("&1LNumber of observations   &1L%d\n", E_T);
-    E_printf("&1LNumber of equations      &1L%d\n", E_NEQ);
-    E_printf("&1LNumber of coefficients   &1L%d\n", E_NCE);
-    E_printf("&1LNumber of instruments    &1L%d\n", E_NINSTR);
-    E_printf("&1LMax number of iterations &1L%d\n", E_MAXIT);
-    E_printf("&1LConvergence limit        &1L%lf\n", (double)E_EPS);
+    W_printfRepl("&1NEstimation period        &1L%s:%s\n", from, to);
+    W_printfRepl("&1NEstimation method        &1L%c\n", "LZIG"[E_MET]);
+    W_printfRepl("&1LNumber of observations   &1L%d\n", E_T);
+    W_printfRepl("&1LNumber of equations      &1L%d\n", E_NEQ);
+    W_printfRepl("&1LNumber of coefficients   &1L%d\n", E_NCE);
+    W_printfRepl("&1LNumber of instruments    &1L%d\n", E_NINSTR);
+    W_printfRepl("&1LMax number of iterations &1L%d\n", E_MAXIT);
+    W_printfRepl("&1LConvergence limit        &1L%lf\n", (double)E_EPS);
 
-    E_printf(".te\n");
+    W_printf(".te\n");
 }
 
 /**
@@ -60,10 +57,10 @@ static void E_print_eqs()
     int     i;
 
     W_print_tit(2);
-    E_printf("Equations\n");
+    W_printf("Equations\n");
     for(i = 0 ; i < E_NEQ ; i++) {
         W_print_enum(2);
-        E_printf("%s\n", E_LECS[i]);
+        W_printf("%s\n", E_LECS[i]);
     }
 }
 
@@ -77,10 +74,10 @@ static void E_print_instrs()
 
     if(E_NINSTR == 0 || E_MET == 0 || E_MET == 1) return;
     W_print_tit(2);
-    E_printf("Instruments\n");
+    W_printf("Instruments\n");
     for(i = 0 ; i < E_NINSTR ; i++) {
         W_print_enum(2);
-        E_printf("%s\n", E_INSTRS[i]);
+        W_printf("%s\n", E_INSTRS[i]);
     }
 }
 
@@ -94,20 +91,20 @@ static void E_print_coefs()
     int     i;
     SCL     *scl;
 
-    E_BeginTbl("Coefficients and tests", 5);
-    E_printf("&1CName&1CValue&1CStandard Error&1CT-Statistic&1CRelax\n");
-    E_printf(".tl\n");
+    W_print_tb("Coefficients and tests", 5);
+    W_printfRepl("&1CName&1CValue&1CStandard Error&1CT-Statistic&1CRelax\n");
+    W_printf(".tl\n");
     for(i = 0 ; i < E_NC ; i++) {
         scl = KSVAL(E_DBS, E_C_NBS[i]);
         //   if(scl->relax == 0) continue; /* JMP 12-03-98 */
-        E_printf("&1L%s&1D%lf&1D%lf&1D%lf&1D%lf\n",
+        W_printfRepl("&1L%s&1D%lf&1D%lf&1D%lf&1D%lf\n",
                  KONAME(E_DBS, E_C_NBS[i]),
                  (double) scl->val,
                  (double) scl->std,
                  (double) E_div_0(scl->val, scl->std),
                  (double) scl->relax);
     }
-    E_printf(".te\n");
+    W_printf(".te\n");
 }
 
 
@@ -118,26 +115,26 @@ static void E_print_mcorr()
 {
     int     i, j, ic, jc;
 
-    E_BeginTbl("Correlation matrix of coefficients", E_NCE + 1);
+    W_print_tb("Correlation matrix of coefficients", E_NCE + 1);
     // E_printf(".twidth 2 2\n"); // JMP 06/07/2022 => to keep .twidth empty as long as possible :-)
 
-    E_printf("&1C ");
+    W_printfRepl("&1C ");
     for(i = 0 ; i < E_NC ; i++)
-        if(MATE(E_SMO, i, 0)) E_printf("&1C%s", KONAME(E_DBS, E_C_NBS[i]));
+        if(MATE(E_SMO, i, 0)) W_printfRepl("&1C%s", KONAME(E_DBS, E_C_NBS[i]));
 
-    E_printf("\n.tl\n");
+    W_printf("\n.tl\n");
     for(i = 0, ic = 0 ; i < E_NC ; i++) {
         if(MATE(E_SMO, i, 0) == 0) continue;
-        E_printf("&1L%s", KONAME(E_DBS, E_C_NBS[i]));
+        W_printfRepl("&1L%s", KONAME(E_DBS, E_C_NBS[i]));
         for(j = 0, jc = 0 ; j < E_NC ; j++) {
             if(MATE(E_SMO, j, 0) == 0) continue;
-            E_printf("&1D%lf", (double) MATE(E_MCORR, ic, jc));
+            W_printfRepl("&1D%lf", (double) MATE(E_MCORR, ic, jc));
             jc++;
         }
-        E_printf("\n");
+        W_printf("\n");
         ic++;
     }
-    E_printf(".te\n");
+    W_printf(".te\n");
 }
 
 
@@ -148,17 +145,17 @@ static void E_print_mcorru()
 {
     int     i, j;
 
-    E_BeginTbl("Correlation of residuals", E_NEQ + 1);
+    W_print_tb("Correlation of residuals", E_NEQ + 1);
 
-    E_printf("&1C ");
-    for(i = 0 ; i < E_NEQ ; i++) E_printf("&1C%s", E_ENDOS[i]);
-    E_printf("\n.tl\n");
+    W_printfRepl("&1C ");
+    for(i = 0 ; i < E_NEQ ; i++) W_printfRepl("&1C%s", E_ENDOS[i]);
+    W_printfRepl("\n.tl\n");
     for(i = 0 ; i < E_NEQ ; i++) {
-        E_printf("&1L%s", E_ENDOS[i]);
-        for(j = 0 ; j < E_NEQ ; j++) E_printf("&1D%lf", (double) MATE(E_MCORRU, i, j));
-        E_printf("\n");
+        W_printfRepl("&1L%s", E_ENDOS[i]);
+        for(j = 0 ; j < E_NEQ ; j++) W_printfRepl("&1D%lf", (double) MATE(E_MCORRU, i, j));
+        W_printfRepl("\n");
     }
-    E_printf(".te\n");
+    W_printfRepl(".te\n");
     /* JMP 13-07-96 */
 }
 
@@ -170,28 +167,28 @@ static void E_print_mcorru()
  */
 static void E_print_eqres_1(int eq_nb)
 {
-    E_BeginTbl("", 2);
+    W_print_tb("", 2);
 
-    E_printf("&1C &1CValue\n");
-    E_printf(".tl\n");
-    E_printf("&1LNumber of coefficients      &1D%d\n", (int)MATE(E_NBCE, 0, eq_nb));
-    E_printf("&1LNumber of observations      &1D%d\n", E_T);
-    E_printf("&1LStandard deviation on YOBS  &1D%lf\n", (double) MATE(E_STDEV, 0, eq_nb));
-    E_printf("&1LMean of YOBS                &1D%lf\n", (double) MATE(E_MEAN_Y, 0, eq_nb));
-    E_printf(".tl\n");
+    W_printfRepl("&1C &1CValue\n");
+    W_printfRepl(".tl\n");
+    W_printfRepl("&1LNumber of coefficients      &1D%d\n", (int)MATE(E_NBCE, 0, eq_nb));
+    W_printfRepl("&1LNumber of observations      &1D%d\n", E_T);
+    W_printfRepl("&1LStandard deviation on YOBS  &1D%lf\n", (double) MATE(E_STDEV, 0, eq_nb));
+    W_printfRepl("&1LMean of YOBS                &1D%lf\n", (double) MATE(E_MEAN_Y, 0, eq_nb));
+    W_printfRepl(".tl\n");
 
-    E_printf("&1CTests&1CValue\n");
-    E_printf(".tl\n");
-    E_printf("&1LSum of square of residuals  &1D%lf\n", (double) MATE(E_SSRES, 0, eq_nb));
-    E_printf("&1LStandard error              &1D%lf\n", (double) MATE(E_STDERR, 0, eq_nb));
-    E_printf("&1LStandard error in %%        &1D%lf\n", (double) MATE(E_STD_PCT, 0, eq_nb));
-    E_printf("&1LF-Stat                      &1D%lf\n", (double) MATE(E_FSTAT, 0, eq_nb));
-    E_printf("&1LR2                          &1D%lf\n", (double) MATE(E_RSQUARE, 0, eq_nb));
-    E_printf("&1LR2 adjusted                 &1D%lf\n", (double) MATE(E_RSQUARE_ADJ, 0, eq_nb));
-    E_printf("&1LDurbin-Watson test          &1D%lf\n", (double) MATE(E_DW, 0, eq_nb));
-    E_printf("&1LLog likelihood              &1D%lf\n", (double) MATE(E_LOGLIK, 0, eq_nb));
-    E_printf(".tl\n");
-    E_printf(".te\n");
+    W_printfRepl("&1CTests&1CValue\n");
+    W_printfRepl(".tl\n");
+    W_printfRepl("&1LSum of square of residuals  &1D%lf\n", (double) MATE(E_SSRES, 0, eq_nb));
+    W_printfRepl("&1LStandard error              &1D%lf\n", (double) MATE(E_STDERR, 0, eq_nb));
+    W_printfRepl("&1LStandard error in %%        &1D%lf\n", (double) MATE(E_STD_PCT, 0, eq_nb));
+    W_printfRepl("&1LF-Stat                      &1D%lf\n", (double) MATE(E_FSTAT, 0, eq_nb));
+    W_printfRepl("&1LR2                          &1D%lf\n", (double) MATE(E_RSQUARE, 0, eq_nb));
+    W_printfRepl("&1LR2 adjusted                 &1D%lf\n", (double) MATE(E_RSQUARE_ADJ, 0, eq_nb));
+    W_printfRepl("&1LDurbin-Watson test          &1D%lf\n", (double) MATE(E_DW, 0, eq_nb));
+    W_printfRepl("&1LLog likelihood              &1D%lf\n", (double) MATE(E_LOGLIK, 0, eq_nb));
+    W_printfRepl(".tl\n");
+    W_printfRepl(".te\n");
     /* JMP 13-07-96 */
 }
 
@@ -209,14 +206,14 @@ static void E_print_eqres_2(int eq_nb)
     PERIOD  *per;
     double  respct;
 
-    E_BeginTbl("Actual and fitted values", 5);
-    E_printf("&1CPeriod&1CObservations&1CFitted Values&1CResiduals&1CResiduals (%%)\n");
-    E_printf(".tl\n");
+    W_print_tb("Actual and fitted values", 5);
+    W_printfRepl("&1CPeriod&1CObservations&1CFitted Values&1CResiduals&1CResiduals (%%)\n");
+    W_printfRepl(".tl\n");
     for(i = 0 ; i < E_T ; i++) {
         per = PER_addper(&(E_SMPL->s_p1), i);
         PER_pertoa(per, c_date);
         respct = 100 * E_div_0(MATE(E_U, eq_nb, i), MATE(E_LHS, eq_nb, i));
-        E_printf("&1L%s&1C%lf&1C%lf&1C%lf&1C%lf\n",
+        W_printfRepl("&1L%s&1C%lf&1C%lf&1C%lf&1C%lf\n",
                  c_date,
                  (double) MATE(E_LHS, eq_nb, i),
                  (double) MATE(E_RHS, eq_nb, i),
@@ -224,7 +221,7 @@ static void E_print_eqres_2(int eq_nb)
                  respct);
     }
 
-    E_printf(".te\n");
+    W_printfRepl(".te\n");
     /* JMP 13-07-96 */
 }
 /*
@@ -302,12 +299,12 @@ static void E_print_eqres(int obs)
     int     i;
 
     W_print_tit(2);
-    E_printf("Results by equation\n");
+    W_printf("Results by equation\n");
     for(i = 0 ; i < E_NEQ ; i++) {
         W_print_tit(3);
-        E_printf("Equation : %s\n", E_ENDOS[i]);
+        W_printf("Equation : %s\n", E_ENDOS[i]);
         W_print_par(3);
-        E_printf("%s\n", E_LECS[i]);
+        W_printf("%s\n", E_LECS[i]);
         E_print_eqres_1(i);
         if(obs) E_print_eqres_2(i);
         /*        E_print_eqres_3(i); */
@@ -433,14 +430,14 @@ int E_graph(char** titles, SAMPLE* smpl, MAT* mlhs, MAT* mrhs, int view, int res
 int E_print_results(int corr, int corru, int obs, int grobs, int grres) 
 {
     W_print_tit(1);
-    E_printf("Estimation\n");
+    W_printf("Estimation\n");
 
     E_print_eqs();
     E_print_parms();
     E_print_instrs();
 
     if(E_CONV == 0) {
-        E_printf(".par1 parb\nTHE PROCESS DOES NOT CONVERGE\n");
+        W_printf(".par1 parb\nTHE PROCESS DOES NOT CONVERGE\n");
         return(0);
     }
 
