14e868ebe219955467d9b151d5d42cea3ef52b6d
Jean-Marc Paul <jmarc.paul@gmail.com>
2024-03-08 15:57:55 +0100

    pyiode/pyiode_data.pyx:     -  data_update_var(): bug fixed         - first and last chars of repr(values) ("[" and "]") removed to generate the iode command

--- pyiode/pyiode_data.pyx
+++ pyiode/pyiode_data.pyx
@@ -138,9 +138,7 @@ def data_update_var(varname: str, values, operation: str = "L", per_from: str = 
         per_from = smpl[0]
         
     cmd = cmd + " " + per_from
-    cmd = cmd + " " + repr(values)
-    #for x in values:
-    #    cmd += f" {x}"
+    cmd = cmd + " " + repr(values)[1:-1]
  
     #print(f"Command:{cmd}")
     if B_DataUpdate(cstr(cmd), K_VAR) != 0:
