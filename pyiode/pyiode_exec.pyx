342939d4a0110b0fef8f3253f9a31dc3f45f7b61
Jean-Marc Paul <jmarc.paul@gmail.com>
2024-03-14 13:51:24 +0100

    pyiode/pyiode_exec.pyx:     - idt_execute(): parameter `trace: int = 0` replaced by `trace: bool = False`     - idt_execute(): in examples, printf() replaced by a simple comment

--- pyiode/pyiode_exec.pyx
+++ pyiode/pyiode_exec.pyx
@@ -6,7 +6,7 @@
 #  IODE identity execution
 #  -----------------------
 # 
-#   idt_execute(sample: Optional[Union[str, List[str]]] = None, idt_list: Optional[Union[str, List[str]]] = None, var_files: Optional[Union[str, List[str]]] = None, scl_files: Optional[Union[str, List[str]]] = None,trace: int = 0) | Execute a list of identities on a given sample
+#   idt_execute(sample: Optional[Union[str, List[str]]] = None, idt_list: Optional[Union[str, List[str]]] = None, var_files: Optional[Union[str, List[str]]] = None, scl_files: Optional[Union[str, List[str]]] = None, trace: bool = False) | Execute a list of identities on a given sample
 
 cdef extern from "iode.h": 
     cdef int    IodeExecuteIdts(char *smpl, char *idt_list, char *var_files, char *scl_files, int trace)
@@ -37,7 +37,7 @@ def idt_execute(sample: Optional[Union[str, List[str]]] = None,
             list of files the needed scalars must be read from. "WS" means current current WS.
             if empty, only the current workspace is used
                         
-    trace: int = 0
+    trace: bool = False
             optional trace indicating the source of the variables and scalars. For example:
             
             Execution of identities
@@ -53,6 +53,8 @@ def idt_execute(sample: Optional[Union[str, List[str]]] = None,
             
     Examples
     --------
+        IODE_DATA_DIR = "../data."
+        
         # define the "trace" file
         iode.w_dest("test_iode.htm", iode.W_HTML)
     
@@ -60,7 +62,7 @@ def idt_execute(sample: Optional[Union[str, List[str]]] = None,
         iode.ws_clear_var()
         iode.ws_sample_set("1960Y1", "2015Y1")
         iode.ws_load_idt(f"{IODE_DATA_DIR}fun")
-        iode.idt_execute("1961Y1:2000Y1", "AOUC", f"{IODE_DATA_DIR}fun", trace = 1)
+        iode.idt_execute("1961Y1:2000Y1", "AOUC", f"{IODE_DATA_DIR}fun", trace = True)
         AOUC = iode.get_var("AOUC")
         test_eq('iode.idt_execute("1961Y1:2000Y1", "AOUC", f"{IODE_DATA_DIR}fun, trace = 1") -> AOUC[1961Y1]', 0.24783192, AOUC[1])
     
@@ -90,7 +92,7 @@ def idt_execute(sample: Optional[Union[str, List[str]]] = None,
         iode.ws_load_scl(f"{IODE_DATA_DIR}fun")
         iode.delete_idt("SSFFX") # SSFFX contains non existent variables
         AOUC = iode.get_var_as_ndarray("AOUC", False)
-        print(f"AOUC[1]={AOUC[1]}")
+        # AOUC[1] = 0.24783192
         AOUC[1] = 0.1
         iode.idt_execute() # all idts on full sample using current loaded WS
         AOUC = iode.get_var_as_ndarray("AOUC", False)
