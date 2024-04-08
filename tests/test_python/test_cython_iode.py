955cc211308d1cfb70b33887a71f242bb736bf88
alixdamman <ald@plan.be>
2024-04-08 10:08:24 +0200

    TESTS PYTHON: fixed test_cython_iode() -> doctests for XXXDatabase were not tested

--- tests/test_python/test_cython_iode.py
+++ tests/test_python/test_cython_iode.py
@@ -6,24 +6,24 @@ See https://cython.readthedocs.io/en/latest/src/userguide/faq.html#how-do-i-run-
 import iode
 import doctest
 import inspect
+import logging
 
 
 def test_cython_iode():
     iode.__test__ = {}
     for name, value in inspect.getmembers(iode):
-        module = inspect.getmodule(value)
-        module = module.__name__ if module is not None else ''
-        if 'iode' in module:
-            if inspect.isclass(value):
-                for name_, value_ in inspect.getmembers(value):
-                    doc = inspect.getdoc(value_)
-                    if doc is not None:
-                        print(f"add test for method '{name}.{name_}'")
-                        iode.__test__[f'{name}.{name_}'] = doc                 
-            doc = inspect.getdoc(value)
-            if doc is not None:
-                print(f"add test for function/method '{name}'")
-                iode.__test__[name] = doc
+        if "Database" in type(value).__name__:
+            value = type(value)
+        if name != "Path" and inspect.isclass(value):
+            for name_, value_ in inspect.getmembers(value):
+                doc = inspect.getdoc(value_)
+                if doc is not None:
+                    logging.info(f"add test for method '{name}.{name_}'")
+                    iode.__test__[f'{name}.{name_}'] = doc                 
+        doc = inspect.getdoc(value)
+        if doc is not None:
+            logging.info(f"add test for function/method '{name}'")
+            iode.__test__[name] = doc
 
     # run doctests
     failure_count, test_count = doctest.testmod(iode, globs={"iode": iode})
