55f42320dfe87e0f06417a25eadd5db30d6c05c2
alixdamman <ald@plan.be>
2024-04-09 11:58:37 +0200

    C++ API: (KDBScalars)     - added method update(const std::string& name, const Scalar& obj)

--- cpp_api/KDB/kdb_scalars.cpp
+++ cpp_api/KDB/kdb_scalars.cpp
@@ -24,6 +24,12 @@ int KDBScalars::add(const std::string& name, const IODE_REAL value, const IODE_R
 	return KDBTemplate::add(name, static_cast<SCL*>(&scalar));
 }
 
+void KDBScalars::update(const std::string& name, const Scalar& obj)
+{
+	Scalar scalar(obj);
+	KDBTemplate::update(name, static_cast<SCL*>(&scalar));
+}
+
 void KDBScalars::update(const std::string& name, const IODE_REAL value, const IODE_REAL relax, const IODE_REAL std)
 {
 	Scalar scalar(value, relax, std);
