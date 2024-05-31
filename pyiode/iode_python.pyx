e5f9607948c832cdff8d6664669f956ddbe704a0
alixdamman <ald@plan.be>
2024-05-31 17:06:14 +0200

    PYIODE: (cleanup) removed print.pyx file

--- pyiode/iode_python.pyx
+++ pyiode/iode_python.pyx
@@ -65,7 +65,6 @@ include "iode_database/variables_database.pyx"
 include "compute/estimation.pyx"
 include "compute/simulation.pyx"
 
-include "print.pyx"
 include "reports.pyx"
 include "wrt.pyx"
 
