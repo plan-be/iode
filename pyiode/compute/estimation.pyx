1d2764502e675b6f1cb8ba6796b46974e73e1d3b
alixdamman <ald@plan.be>
2024-06-04 08:52:55 +0200

    PYIODE: (Estimation) Exported the C++ class CorrelationMatrix

--- pyiode/compute/estimation.pyx
+++ pyiode/compute/estimation.pyx
@@ -5,6 +5,7 @@ from libcpp.vector cimport vector
 from pyiode.common cimport EnumIodeAdjustmentMethod
 from pyiode.compute.estimation cimport dynamic_adjustment as cpp_dynamic_adjustment
 from pyiode.compute.estimation cimport dickey_fuller_test as cpp_dickey_fuller_test
+from pyiode.compute.estimation cimport CCorrelationMatrix
 from pyiode.iode_database.cpp_api_database cimport KDBScalars as CKDBScalars
 
 
@@ -127,3 +128,112 @@ def dickey_fuller_test(lec: str, drift: bool, trend: bool, order: int) -> Scalar
     cdef CKDBScalars* df_scalars
     df_scalars = cpp_dickey_fuller_test(lec.encode(), <bint>drift, <bint>trend, order)
     return Scalars._from_ptr(df_scalars)
+
+
+# CorrelationMatrix wrapper class
+# see https://cython.readthedocs.io/en/latest/src/userguide/wrapping_CPlusPlus.html#create-cython-wrapper-class 
+cdef class CorrelationMatrix:
+    cdef bint ptr_owner
+    cdef CCorrelationMatrix* c_correlation_matrix
+
+    def __init__(self):
+        # Prevent accidental instantiation from normal Python code
+        # since we cannot pass a struct pointer into a Python constructor.
+        raise TypeError("This class cannot be instantiated directly.")
+
+    def __cinit__(self):
+        self.ptr_owner = False
+        self.c_correlation_matrix = NULL
+
+    def __dealloc__(self):
+        if self.ptr_owner and self.c_correlation_matrix is not NULL:
+            del self.c_correlation_matrix
+            self.c_correlation_matrix = NULL
+
+    # see https://cython.readthedocs.io/en/stable/src/userguide/extension_types.html#instantiation-from-existing-c-c-pointers 
+    @staticmethod
+    cdef CorrelationMatrix from_ptr(CCorrelationMatrix* ptr, bint owner=False):
+        """
+        Factory function to create CorrelationMatrix objects from a given CCorrelationMatrix pointer.
+        """
+        # Fast call to __new__() that bypasses the __init__() constructor.
+        cdef CorrelationMatrix wrapper = CorrelationMatrix.__new__(CorrelationMatrix)
+        wrapper.c_correlation_matrix = ptr
+        wrapper.ptr_owner = owner
+        return wrapper
+
+    @property
+    def shape(self) -> Tuple[int, int]:
+        if self.c_correlation_matrix is not NULL:
+            return self.c_correlation_matrix.nb_coeffs, self.c_correlation_matrix.nb_coeffs
+        else:
+            warnings.warn("The correlation matrix has not been defined yet")
+            return 0, 0
+
+    @property
+    def names(self) -> List[str]:
+        if self.c_correlation_matrix is not NULL:
+            return [self.c_correlation_matrix.get_name(i).decode() for i in range(len(self))]
+        else:
+            warnings.warn("The correlation matrix has not been defined yet")
+            return None
+
+    def name(self, index: int) -> str:
+        if self.c_correlation_matrix is not NULL:
+            if index < 0 or index >= self.c_correlation_matrix.nb_coeffs:
+                raise ValueError(f"'index': Value must be in range [0, {len(self)}]. "
+                                f"Got index value {index} instead")
+            return self.c_correlation_matrix.get_name(index).decode()
+        else:
+            warnings.warn("The correlation matrix has not been defined yet")
+            return None
+
+    def __len__(self) -> int:
+        if self.c_correlation_matrix is not NULL:
+            return self.c_correlation_matrix.nb_coeffs
+        else:
+            warnings.warn("The correlation matrix has not been defined yet")
+            return 0
+
+    def __getitem__(self, key: Tuple[int, int]) -> float:
+        if self.c_correlation_matrix is not NULL:
+            row, column = key
+            if row < 0 or row >= self.c_correlation_matrix.nb_coeffs:
+                raise ValueError(f"CorrelationMatrx[row, column]: Value for rwo and column must be in range "
+                                f"[0, {len(self)}]. Got row value {row} instead")
+            if column < 0 or column >= self.c_correlation_matrix.nb_coeffs:
+                raise ValueError(f"CorrelationMatrx[row, column]: Value for rwo and column must be in range "
+                                f"[0, {len(self)}]. Got column value {column} instead")
+            return self.c_correlation_matrix.get_value(row, column)
+        else:
+            warnings.warn("The correlation matrix has not been defined yet")
+            return None
+
+    def __setitem__(self, key, value):
+        warnings.warn("The values of the correlation matrix are read-only.")
+
+    def __delitem__(self, key):
+        warnings.warn("The values of the correlation matrix are read-only.")
+
+    def __str__(self):
+        if self.c_correlation_matrix is not NULL:
+            coeffs_names = self.names
+            max_column_length = max(10, max(len(name) for name in coeffs_names) + 1)
+
+            # horizontal header
+            top_left = ' ' * max_column_length
+            coeffs_names = [name.rjust(max_column_length) for name in coeffs_names]
+            s = top_left + ' | ' + '  '.join(coeffs_names) + '\n'
+            s += '-' * len(s) + '\n'
+            # rows
+            for row, name in enumerate(coeffs_names):
+                values = [f"{self.c_correlation_matrix.get_value(row, column):>10.6g}".rjust(max_column_length) 
+                          for column in range(len(self))]
+                s += name.ljust(max_column_length) + ' | ' + '  '.join(values) + '\n'
+            return s
+        else:
+            warnings.warn("The correlation matrix has not been defined yet")
+            return None
+
+    def __repr__(self):
+        return str(self)
