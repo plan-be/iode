f6dca62bf559b8c2cef573f4ef5a62533c493cd8
alixdamman <ald@plan.be>
2024-07-11 11:15:50 +0200

    C++ API: (Scalar) removed constructors taking a KDB pointer as argument

--- cpp_api/objects/scalar.cpp
+++ cpp_api/objects/scalar.cpp
@@ -8,47 +8,18 @@ Scalar::Scalar()
     this->std = IODE_NAN;
 }
 
-Scalar::Scalar(const int pos, KDB* kdb)
-{
-    if (!kdb) 
-        kdb = K_WS[SCALARS];
-
-    if (pos < 0 || pos > kdb->k_nb)
-    {
-        IodeExceptionInvalidArguments error("Cannot extract Scalar", "Scalar position must be in range [0, " + 
-            std::to_string(kdb->k_nb - 1) + "])");
-        error.add_argument("scalar position", std::to_string(pos));
-        throw error;
-    }
-
-    // Note: KSVAL does not allocate a new pointer SCL*
-    SCL* c_scalar = KSVAL(kdb, pos);
-    this->val = c_scalar->val;
-    this->relax = c_scalar->relax;
-    this->std = c_scalar->std;
-}
-
-Scalar::Scalar(const std::string& name, KDB* kdb)
-{
-    if (!kdb) 
-        kdb = K_WS[SCALARS];
-
-    int pos = K_find(kdb, to_char_array(name));
-    if (pos < 0) 
-        throw IodeExceptionFunction("Cannot extract Scalar", "Scalar with name " + name + " does not exist.");
-    
-    // Note: KSVAL does not allocate a new pointer SCL*
-    SCL* c_scalar = KSVAL(kdb, pos);
-    this->val = c_scalar->val;
-    this->relax = c_scalar->relax;
-    this->std = c_scalar->std;
-}
-
 Scalar::Scalar(const double value, const double relax, const double std)
 {
     this->val = value;
     this->relax = relax;
     this->std = std;
+}
+
+Scalar::Scalar(const SCL* c_scalar)
+{
+    this->val = c_scalar->val;
+    this->relax = c_scalar->relax;
+    this->std = c_scalar->std;
 }
 
 Scalar::Scalar(const Scalar& scalar)
