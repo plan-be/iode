13d35645fcaa075ed7b1b8bb2b515b520d81fb9f
alixdamman <ald@plan.be>
2024-08-02 13:41:59 +0200

    GUI PYTHON: (settings.py) translated ProjectSettings and IodeSettingsDialog from C++

--- gui/settings.py
+++ gui/settings.py
@@ -16,3 +16,43 @@ class ProjectSettings:
         filepath = project_dir.absoluteFilePath("iode_gui_settings.ini")
         cls.project_settings = QSettings(filepath, QSettings.Format.IniFormat, parent)
         return cls.project_settings
+
+
+# Mixin class for handling settings in derived class.
+class IodeSettingsDialog(QDialog):
+    def __init__(self, class_name: str, parent: QWidget=None, 
+                 f=Qt.WindowType.WindowMinimizeButtonHint | Qt.WindowType.WindowCloseButtonHint):
+        super().__init__(parent, f)
+        self.class_name = class_name
+        self.map_fields = {}
+
+    def save_settings(self):
+        project_settings = ProjectSettings.project_settings
+        project_settings.beginGroup(self.class_name)
+        for key, field in self.map_fields.items():
+            project_settings.setValue(key, field.value)
+        project_settings.endGroup()
+
+    def load_settings(self):
+        project_settings = ProjectSettings.project_settings
+        project_settings.beginGroup(self.class_name)
+        for key, field in self.map_fields.items():
+            if project_settings.contains(key):
+                field.value = project_settings.value(key)
+        project_settings.endGroup()
+
+    def closeEvent(self, event: QCloseEvent):
+        self.save_settings()
+        event.accept()
+
+    @pyqtSlot()
+    def help(self):
+        QDesktopServices.openUrl(URL_MANUAL)
+
+    def accept(self):
+        self.save_settings()
+        super().accept()
+
+    def reject(self):
+        self.save_settings()
+        super().reject()
