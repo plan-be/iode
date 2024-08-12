8597a77e828579068b12bca0a6d1c8f746d0b6ec
alixdamman <ald@plan.be>
2024-08-12 11:30:39 +0200

    GUI PYTHON: (plot.py) added fake PlotDialog class

--- gui/plot/plot.py
+++ gui/plot/plot.py
@@ -2,8 +2,4 @@ from PyQt6.QtWidgets import QDialog
 
 
 class PlotDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-    def plot(self) -> None:
-        pass
+    pass
