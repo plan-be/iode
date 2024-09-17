d33af824361a4f464b9d825f5a0937cf66fa4e70
alixdamman <ald@plan.be>
2024-09-17 14:40:24 +0200

    GUI PYTHON: (variables_delegate.py) implemented the VariablesDelegate class in Python

---
+++ gui/iode_objs/delegates/variables_delegate.py
@@ -0,0 +1,38 @@
+from base_delegate import BaseDelegate
+from util.double_validator import IodeDoubleValidator
+
+from PyQt6.QtCore import QModelIndex
+from PyQt6.QtWidgets import QLineEdit, QWidget, QStyleOptionViewItem
+
+from iode import IodeTypes
+
+
+class VariablesDelegate(BaseDelegate):
+    """
+    A delegate for handling variables in a model-view structure.
+
+    This delegate creates a QLineEdit widget for editing variables,
+    and sets a validator to ensure that the input is a double.
+    """
+
+    def __init__(self, parent=None):
+        """
+        Initializes a new instance of the VariablesDelegate class.
+
+        :param parent: The parent object of this VariablesDelegate instance.
+        """
+        super().__init__(IodeTypes.VARIABLES, parent)
+
+    # override BaseDelegate method
+    def createEditor(self, parent: QWidget, option: QStyleOptionViewItem, index: QModelIndex):
+        """
+        Creates a QLineEdit widget for editing variables.
+
+        :param parent: The parent widget for the editor.
+        :param option: The style options for the editor.
+        :param index: The model index of the item being edited.
+        :return: The QLineEdit widget for editing variables.
+        """
+        editor = QLineEdit(parent)
+        editor.setValidator(IodeDoubleValidator(editor))
+        return editor
