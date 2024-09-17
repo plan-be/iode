d1ea13b363906d8b8e3ce047fd5955186ea7f2ce
alixdamman <ald@plan.be>
2024-09-17 14:47:28 +0200

    GUI PYTHON: (lists_delegate.py) implemented the ListsDelegate class in Python

---
+++ gui/iode_objs/delegates/lists_delegate.py
@@ -0,0 +1,17 @@
+from base_delegate import BaseDelegate
+
+from iode import IodeTypes
+
+
+class ListsDelegate(BaseDelegate):
+    """
+    ListsDelegate class that inherits from BaseDelegate.
+    """
+
+    def __init__(self, parent=None):
+        """
+        Constructor for ListsDelegate.
+
+        :param parent: The parent object.
+        """
+        super().__init__(IodeTypes.LISTS, parent)
