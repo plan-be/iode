51473b86b21e9122bd5b32eae8d8cb5ab42d0b4e
alixdamman <ald@plan.be>
2024-11-05 16:12:33 +0100

    PYTHON: (Period)         - changed signature of the constructor         - made nb_periods_per_year a property         - fixed periodicity returning an int instead of a string character         - added doctests for properties year, periodicity and step         - updated __str__() and __repr__()

--- pyiode/time/period.pyx
+++ pyiode/time/period.pyx
@@ -16,7 +16,7 @@ cdef class Period:
 
     cdef CPeriod c_period
 
-    def __init__(self, *args: Union[str, Tuple[int, str, int]]) -> Period:
+    def __init__(self, period_or_year: Union[str, int, Period], periodicity: str='Y', step: int=1) -> Period:
         """
         An IODE period is defined by a year, a periodicity and by a position in the year.
 
@@ -31,23 +31,14 @@ cdef class Period:
 
         Parameters
         ----------
-        *args: str or (int, str, int)
-            Either a string representing the period or values representing the year, 
-            the periodicity and the position of the period in the year (step):
-
-            * period: str
-                String representing the period.
-
-            or
-
-            * year: int
-                Year of the period.
-
-            * periodicity: {'Y', 'S', 'Q', 'M', 'W', 'D'}, optional
-                Periodicity of the period. 
+        period_or_year: str or int
+            If str, it is consided as a string representing the year, the periodicity 
+            and the position of the period in the year (step).
+            If int, it is considered as the year of the period.
+        periodicity: str, optional
+                Periodicity of the period. Possible values are 'Y', 'S', 'Q', 'M', 'W' and 'D'.
                 Default to 'Y' (yearly).
-
-            * step: int, optional
+        step: int, optional
                 Position of the period in the year. 
                 Default to 1.
 
@@ -64,61 +55,40 @@ cdef class Period:
         >>> # passing a string
         >>> period = Period("2000Y1")
         >>> period
-        '2000Y1'
+        Period("2000Y1")
         >>> # passing year, periodicity and step
         >>> period = Period(2010, 'Q', 1)
         >>> period
-        '2010Q1'
+        Period("2010Q1")
         >>> # passing only year
         >>> period = Period(2010)
         >>> period
-        '2010Y1'
+        Period("2010Y1")
         >>> # passing year and periodicity
         >>> period = Period(2010, 'Q')
         >>> period
-        '2010Q1'
+        Period("2010Q1")
         >>> # copy a period
         >>> period_2 = Period(period)
         >>> period_2
-        '2010Q1'
+        Period("2010Q1")
         """
-        cdef int year_ = 0
-        cdef char periodicity_ = b'Y'
-        cdef int step_ = 1
-        cdef str str_period = ''
+        cdef char ch_periodicity
 
-        def check_periodicity(value: str) -> int:
-            if len(value) != 1:
+        if isinstance(period_or_year, Period):
+            period_or_year, periodicity, step = period_or_year.year, period_or_year.periodicity, period_or_year.step
+        
+        if isinstance(period_or_year, str):
+            self.c_period = CPeriod(<string>period_or_year.encode())
+        else:
+            if len(periodicity) != 1:
                 raise ValueError("'periodicity': Expected string of length 1")
-            if value not in PERIODICITY_LIST:
-                raise ValueError(f"Wrong periodicity. Valid periodicity are: 'Y', 'S', 'Q', 'M', 'W' or 'D'")
-            return value.encode()[0]
+            if periodicity not in PERIODICITY_LIST:
+                raise ValueError(f"Wrong periodicity. Valid values for the periodicity are: {', '.join(PERIODICITY_LIST)}")
+            ch_periodicity = periodicity.encode()[0]
+            self.c_period = CPeriod(<int>period_or_year, ch_periodicity, <int>step)
 
-        if len(args) == 1:
-            arg = args[0]
-            if isinstance(arg, int):
-                year_ = arg
-            elif isinstance(arg, Period):
-                year_, periodicity_, step_ = arg.year, arg.periodicity, arg.step
-            elif isinstance(arg, str):
-                str_period = arg
-            else:
-                raise ValueError("Expected argument of type int, 'Period' or string.\nGot argument of type '" + type(arg).__name__ + "'")
-        elif len(args) == 2:
-            year_ = args[0]
-            periodicity_ = check_periodicity(args[1])
-        elif len(args) == 3:
-            year_ = args[0]
-            periodicity_ = check_periodicity(args[1])
-            step_ = args[2]
-        else:
-            raise ValueError("Wrong number of arguments to create a new Period")
-
-        if str_period:
-            self.c_period = CPeriod(<string>str_period.encode())
-        else:
-            self.c_period = CPeriod(year_, periodicity_, step_)
-
+    @property
     def nb_periods_per_year(self) -> int:
         """
         Number of periods in a year according to the periodicity.
@@ -127,7 +97,7 @@ cdef class Period:
         --------
         >>> from iode import Period
         >>> period = Period(2010, 'Q', 1)
-        >>> period.nb_periods_per_year()
+        >>> period.nb_periods_per_year
         4
         """
         return self.c_period.nb_periods_per_year()
@@ -178,15 +148,15 @@ cdef class Period:
         >>> from iode import Period
         >>> period = Period(2000, 'Q', 1)
         >>> period
-        '2000Q1'
+        Period("2000Q1")
         >>> # shift forward
         >>> shifted_period = period.shift(7)
         >>> shifted_period
-        '2001Q4'
+        Period("2001Q4")
         >>> # shift backward
         >>> shifted_period = period.shift(-7)
         >>> shifted_period
-        '1998Q2'
+        Period("1998Q2")
         """
         shifted_period = self.c_period.shift(nb_periods)
         return Period(shifted_period.p_y, chr(shifted_period.p_p), shifted_period.p_s)
@@ -217,6 +187,13 @@ cdef class Period:
     def year(self) -> int:
         """
         Corresponding year of the period
+
+        Examples
+        --------
+        >>> from iode import Period
+        >>> period = Period(2000, 'Q', 3)
+        >>> period.year
+        2000
         """
         return self.c_period.p_y
 
@@ -231,13 +208,27 @@ cdef class Period:
             - M: monthly
             - W: weekly
             - D: daily
+        
+        Examples
+        --------
+        >>> from iode import Period
+        >>> period = Period(2000, 'Q', 3)
+        >>> period.periodicity
+        'Q'
         """
-        return self.c_period.p_p
+        return chr(self.c_period.p_p)
 
     @property
     def step(self) -> int:
         """
         Position of the period in the year
+
+        Examples
+        --------
+        >>> from iode import Period
+        >>> period = Period(2000, 'Q', 3)
+        >>> period.step
+        3
         """
         return self.c_period.p_s
 
@@ -247,7 +238,13 @@ cdef class Period:
         return self.c_period == other.c_period
 
     def __str__(self) -> str:
-        return self.c_period.to_string().decode()
+        if self.year == 0:
+            return ""
+        else:
+            return f"{self.year}{self.periodicity}{self.step}"
 
     def __repr__(self) -> str:
-        return repr(str(self))
+        if self.year == 0:
+            return ""
+        else:
+            return f'Period("{self.year}{self.periodicity}{self.step}")'
