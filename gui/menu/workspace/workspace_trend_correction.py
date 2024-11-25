6c047132ba501deaa5074cbb78eff576d0d0d649
alixdamman <ald@plan.be>
2024-11-25 11:59:59 +0100

    GUI PYTHON: (workspace_trend_correction.py) implemented the             MenuWorkspaceTrendCorrection class

--- gui/menu/workspace/workspace_trend_correction.py
+++ gui/menu/workspace/workspace_trend_correction.py
@@ -1,7 +1,35 @@
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QWidget, QMessageBox
 
-from PySide6.QtWidgets import QDialog, QWidget
+from settings import MixinSettingsDialog
+from util.widgets.file_chooser import EnumFileMode
+from .ui_workspace_trend_correction import Ui_MenuWorkspaceTrendCorrection
+
+from iode import IodeFileType, variables
 
 
-class MenuWorkspaceTrendCorrection(QDialog):
-    def __init__(self, parent: QWidget = None) -> None:
+class MenuWorkspaceTrendCorrection(MixinSettingsDialog):
+    def __init__(self, parent: QWidget=None):
         super().__init__(parent)
+        self.ui = Ui_MenuWorkspaceTrendCorrection()
+        self.ui.setupUi(self)
+        self.prepare_settings(self.ui)
+
+        self.ui.fileChooser_input_file.enum_file_type = IodeFileType.FILE_VARIABLES
+        self.ui.fileChooser_input_file.enum_file_mode = EnumFileMode.EXISTING_FILE
+
+    @Slot()
+    def trend_correction(self):
+        try:
+            filepath: str = self.ui.fileChooser_input_file.filepath
+            series: str = ";".join(self.ui.textEdit_series.toPlainText().splitlines()).strip()
+            lambda_value: float = self.ui.doubleSpinBox_lambda.value()
+            log: bool = self.ui.checkBox_log.isChecked()
+
+            if not len(filepath):
+                raise ValueError("'input file' is empty")
+
+            variables.trend_correction(filepath, lambda_value, series, log)
+            self.accept()
+        except Exception as e:
+            QMessageBox.warning(self, "WARNING", "Failed to compute trend correction.\n" + str(e))
