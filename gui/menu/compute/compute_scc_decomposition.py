98d23e0c1a9585077e71b31d88b427f006d6cc92
alixdamman <ald@plan.be>
2024-12-05 10:33:39 +0100

    GUI PYTHON: (compute_scc_decomposition.py) implemented the MenuComputeSCCDecomposition class

--- gui/menu/compute/compute_scc_decomposition.py
+++ gui/menu/compute/compute_scc_decomposition.py
@@ -1,7 +1,43 @@
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QWidget, QMessageBox
 
-from PySide6.QtWidgets import QDialog, QWidget
+from settings import MixinSettingsDialog
+from .ui_compute_scc_decomposition import Ui_MenuComputeSCCDecomposition
+
+from iode import IodeType, Simulation
 
 
-class MenuComputeSCCDecomposition(QDialog):
-    def __init__(self, parent: QWidget = None) -> None:
+class MenuComputeSCCDecomposition(MixinSettingsDialog):
+    def __init__(self, parent: QWidget=None):
         super().__init__(parent)
+        self.ui = Ui_MenuComputeSCCDecomposition()
+        self.ui.setupUi(self)
+        self.prepare_settings(self.ui)  
+
+        self.ui.textEdit_equations_list.handle_iode_type(IodeType.EQUATIONS)        
+        self.ui.textEdit_equations_list.include_iode_command(False)        
+        self.ui.textEdit_equations_list.include_lec_functions(False)
+
+        self.ui.spinBox_triangulation_iterations.setValue(5)
+        self.ui.lineEdit_pre_recursive_list_name.setText("_PRE")
+        self.ui.lineEdit_inter_recursive_list_name.setText("_INTER")
+        self.ui.lineEdit_post_recursive_list_name.setText("_POST")
+
+        self.load_settings()
+
+    @Slot()
+    def compute(self):
+        try:
+            equations_list: str = self.ui.textEdit_equations_list.toPlainText().strip()
+            nb_iterations: int = self.ui.spinBox_triangulation_iterations.value()
+            pre_recursive_list_name: str = self.ui.lineEdit_pre_recursive_list_name.text().strip()
+            inter_recursive_list_name: str = self.ui.lineEdit_inter_recursive_list_name.text().strip()
+            post_recursive_list_name: str = self.ui.lineEdit_post_recursive_list_name.text().strip()
+
+            simu = Simulation()
+            simu.model_calculate_SCC(nb_iterations, pre_recursive_list_name, inter_recursive_list_name, 
+                                     post_recursive_list_name, equations_list)
+
+            self.accept()
+        except Exception as e:
+            QMessageBox.warning(self, "WARNING", str(e))
