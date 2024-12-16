a4623a716e83446172e0a35ca844a1b3800f988e
alixdamman <ald@plan.be>
2024-12-16 12:23:07 +0100

    GUI PYTHON: (graph_variables.py) implemented the MenuGraphVariables class

--- gui/plot/plot.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QDialog
-
-
-class PlotDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-    def plot(self) -> None:
-        pass
