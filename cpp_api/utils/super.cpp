8717098dbeb7f086634abcd8f82f2adac354ea94
alixdamman <ald@plan.be>
2025-01-01 22:09:50 +0100

    C++ API: (super.h/cpp) removed cpp_panic_super() and cpp_warning()

--- cpp_api/utils/super.cpp
+++ cpp_api/utils/super.cpp
@@ -2,26 +2,7 @@
 #include "super.h"
 
 
-void cpp_panic_super()
-{
-	char* error_msg = "Swap Panic!";
-	std::cout << error_msg << std::endl;
-	std::cout << "Attempting to save Data saved in ws.*files... ";
-	K_end_ws(1);
-	std::cout << "OK" << std::endl;
-	std::cout << "To restart iode with the last data, type :" << std::endl;
-	std::cout << "    iode -ws" << std::endl;
-	exit(2);
-}
-
-void cpp_warning(const char* msg)
-{
-	std::cout << "WARNING: " + std::string(msg) << std::endl;	
-}
-
 void cpp_assign_super_API()
 {
     IODE_assign_super_API();
-	kwarning_super = cpp_warning;
-    kpanic_super = cpp_panic_super;
 }
