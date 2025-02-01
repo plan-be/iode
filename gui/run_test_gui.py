6632de89e0a280b90eae73b3a726357aae7a4f0b
alixdamman <ald@plan.be>
2025-02-01 21:38:56 +0100

    GUI PYTHON: (main.py) fixed main() function + renamed it as start()

--- gui/run_test_gui.py
+++ gui/run_test_gui.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from iode import comments, equations, identities, lists, scalars, tables, variables
 from iode import SAMPLE_DATA_DIR
-from iode_gui import main, open_application
+from iode_gui import start, open_application
 
 
 global_var = 10
@@ -17,10 +17,14 @@ def run_view_workspace():
     variables.load(f"{SAMPLE_DATA_DIR}/fun.var")
     local_var = [1, 2, 3, 4, 5]
     # will open a viewer showing all IODE databases
-    project_dir = Path('.').resolve()
+    project_dir = Path(__file__).parent.resolve()
     open_application(project_dir, called_from_python_script=True, depth=1)
 
 
 if __name__ == "__main__":
-    # run_view_workspace()
-    main()
+    project_dir = Path('.').resolve()
+    start()
+
+    run_view_workspace()
+    
+    run_view_workspace()
