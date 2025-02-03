98b477479899bd38b4fe062ade423e6e5aefb655
alixdamman <ald@plan.be>
2025-02-03 22:00:02 +0100

    GUI PYTHON: (IodeReportEditor) get self.project_path from             the MainWindow object

--- gui/iode_gui/text_edit/report_editor.py
+++ gui/iode_gui/text_edit/report_editor.py
@@ -26,12 +26,14 @@ class IodeReportEditor(IodeTextEditor):
         super().__init__(parent)
         self.highlighter: IodeHighlighter = IodeHighlighter(self.document())
         self.output: QTextEdit = None
+        self.project_path: str = None
 
     def setup(self, main_window: AbstractMainWindow):
         """
         Setup the IodeReportEditor.
         """
         self.output: QTextEdit = main_window.output
+        self.project_path: str = main_window.project_path
 
     def run(self, filepath: str, parameters: Union[str, List[str]], nb_decimals: int, language: TableLang):
         """
@@ -45,13 +47,17 @@ class IodeReportEditor(IodeTextEditor):
             nb_decimals (int): The number of decimals to use.
             language (TableLang): The language to use.
         """
-        current_project_dir = QDir.currentPath()
+        saved_current_dir: str = QDir.currentPath()
+        
         project_settings = ProjectSettings.project_settings
         if project_settings:
             run_from_project_dir = project_settings.value(RUN_REPORTS_FROM_PROJECT_DIR, 
                                                           defaultValue=True, type=bool)
         else:
             run_from_project_dir = True
+        
+        if self.project_path:
+            QDir.setCurrent(self.project_path)
 
         if not isinstance(parameters, str) and not all(isinstance(p, str) for p in parameters):
             raise ValueError("'parameters' must be a string or a list of strings")
@@ -60,8 +66,13 @@ class IodeReportEditor(IodeTextEditor):
             parameters = ";".join(parameters)
 
         try:
+            # updates current directory execution (chdir)
+            if not run_from_project_dir:
+                QDir.setCurrent(QFileInfo(filepath).absolutePath())
+            
             if self.output:
                 self.output.setTextColor(Qt.GlobalColor.darkBlue)
+                self.output.append(f"\nRoot execution directory: {QDir.currentPath()}\n")
                 self.output.append(f"\nRunning report {filepath} ...\n")
                 self.output.setTextColor(Qt.GlobalColor.black)
 
@@ -69,22 +80,16 @@ class IodeReportEditor(IodeTextEditor):
             execute_command(f"$PrintNbDec  {nb_decimals}")
             execute_command(f"$PrintLang  {language.name.title()}")
 
-            # updates current directory execution (chdir)
-            if not run_from_project_dir:
-                QDir.setCurrent(QFileInfo(filepath).absolutePath())
-
             # executes IODE report
             execute_report(filepath, parameters)
 
             # reset current directory execution to project directory (chdir)
-            if not run_from_project_dir:
-                QDir.setCurrent(current_project_dir)
+            QDir.setCurrent(saved_current_dir)
 
             success = True
         except Exception as e:
             # reset current directory execution to project directory (chdir)
-            if not run_from_project_dir:
-                QDir.setCurrent(current_project_dir)
+            QDir.setCurrent(saved_current_dir)
 
             success = False
             msg = str(e)
