GUI PYTHON: (NumericalTableView)             - fixed paste_vars()             - renamed paste_vars() as paste_special()
fix #856

gui/iode_gui/iode_objs/models/table_model.py
@@ -4,7 +4,7 @@ from .abstract_table_model import IodeAbstractTableModel
 from .numerical_table_model import IodeNumericalTableModel
 from iode_gui.utils import MAX_PRECISION_NUMBERS, NAN_REP
 
-from typing import Union, List, Any
+from typing import Union, List, Tuple, Any
 import numpy as np
 from iode import (IodeType, VarsMode, comments, equations, identities, lists, 
                   scalars, tables, variables, Scalar, Table, NA, is_NA)
@@ -206,6 +206,10 @@ class VariablesModel(IodeAbstractTableModel, IodeNumericalTableModel):
         except Exception as e:
             QMessageBox.warning(None, "WARNING", str(e))
             return False
+    
+    def paste(self, names: List[str], periods: Union[slice, List[Any]], 
+              values: Union[np.ndarray, List[List[float]]]):
+        variables[names, periods] = values
 
     @Slot()
     def update_display_values(self):

gui/iode_gui/iode_objs/views/table_view.py
@@ -1,10 +1,8 @@
 from PySide6.QtCore import Qt, Slot, Signal, QSettings
 from PySide6.QtWidgets import QDialog, QMessageBox, QAbstractItemView, QLineEdit
 from PySide6.QtGui import QShortcut, QKeySequence, QContextMenuEvent, QAction
-from PySide6.QtPrintSupport import QPrinter, QPrintPreviewDialog
 
 from iode_gui.abstract_main_window import AbstractMainWindow
-from iode_gui.settings import ProjectSettings, PRINT_TO_FILE
 
 from iode_gui.iode_objs.delegates.comments_delegate import CommentsDelegate
 from iode_gui.iode_objs.delegates.equations_delegate import EquationsDelegate
@@ -15,7 +13,7 @@ from iode_gui.iode_objs.delegates.tables_delegate import TablesDelegate
 from iode_gui.iode_objs.delegates.variables_delegate import VariablesDelegate
 
 from iode_gui.iode_objs.new.add_object import (AddCommentDialog, AddIdentityDialog, AddListDialog, 
-                                      AddScalarDialog, AddTableDialog, AddVariableDialog)
+                                               AddScalarDialog, AddTableDialog, AddVariableDialog)
 from iode_gui.iode_objs.edit.edit_iode_obj import EditCommentDialog, EditIdentityDialog, EditListDialog
 from iode_gui.iode_objs.edit.edit_equation import EditEquationDialog
 from iode_gui.iode_objs.edit.edit_table import EditTableDialog
@@ -31,9 +29,10 @@ from iode_gui.plot.plot_vars import PlotVariablesDialog
 from .numerical_view import NumericalTableView
 from .abstract_table_view import IodeAbstractTableView
 
-from typing import Tuple, Union, Dict, Any
 from pathlib import Path
-from iode import IodeType, equations, scalars, tables, variables, Table, Sample
+import numpy as np
+from typing import Union, Tuple, List, Dict, Any
+from iode import IodeType, equations, scalars, tables, variables, Table, Sample, Period
 
 
 class CommentsView(IodeAbstractTableView):
@@ -381,6 +380,37 @@ class VariablesView(IodeAbstractTableView, NumericalTableView):
         if not periods:
             periods = None
         return (names, periods)
+
+    # override method from NumericalTableView
+    def _paste_special(self, names: List[str], columns: List[str], values: List[List[str]]):
+        nb_periods = len(columns)
+        try:
+            # columns = periods
+            periods = [Period(period.strip()) for period in columns]
+            # if range of contiguous periods -> tuple (from_period, to_period)
+            if columns == Sample(periods[0], periods[-1]).periods:
+                periods = slice(periods[0], periods[-1], 1)
+        # columns = values of the first variable ?
+        except:
+            sample = self._ask_sample()
+            if sample is None:
+                return
+            from_period, to_period = sample
+            paste_sample = Sample(from_period, to_period)
+            # check that the passed sample has the size equal to len(cells) - 1
+            if len(periods) != nb_periods:
+                QMessageBox.warning(None, "WARNING", "Can't paste values.\n" + 
+                                    f"The sample '{str(paste_sample)}' of the values to paste does not " + 
+                                    f"correspond to the number of values per variable: '{len(nb_periods)}'")
+                return
+            periods = slice(from_period, to_period, 1)
+        
+        # if contiguous periods -> numpy array
+        if isinstance(periods, slice):
+            values = np.asarray(values, dtype=np.float64)
+        
+        model: VariablesModel = self.model()
+        model.paste(names, periods, values)
 
     # override QTableView method
     @Slot(QContextMenuEvent)