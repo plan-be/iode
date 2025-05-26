c3a03a483265edb7d1ba4963107158d92ced6dea
alixdamman <ald@plan.be>
2025-05-26 13:44:18 +0200

    C API: removed no longer used chart manipulation functions from the API

--- api/iodeapi.h
+++ api/iodeapi.h
@@ -19,17 +19,6 @@ extern "C" {
 extern int IodeInit();
 extern int IodeEnd();
 
-extern double IodeExecLecT(char* lec, int t);
-extern double *IodeExecLec(char* lec);
-
-extern int IodeGetChart(char *name, char *gsmpl);
-extern int IodeFreeChart(int hdl);
-extern int IodeChartNl(int hdl);
-extern int IodeChartNc(int hdl);
-extern char IodeChartType(int hdl, int i);
-extern int IodeChartAxis(int hdl, int i);
-extern char *IodeChartTitle(int hdl, int i);
-extern double *IodeChartData(int hdl, int i);
 
 extern void kmsg_null(const char* msg);
 extern void IodeSuppressMsgs();
