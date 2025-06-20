f5ec5adcf31b91f5b58d184d01e94780a7cc9b0d
alixdamman <ald@plan.be>
2025-06-20 11:20:22 +0200

    C API: (b_rep_engine.c) added global variable RP_SDTOUT to force        report/command line output to be written to stdout

--- cpp_api/report/report.cpp
+++ cpp_api/report/report.cpp
@@ -3,6 +3,8 @@
 
 void execute_report(const std::string& filepath, const std::string& parameters)
 {
+    if(RP_DEBUG == 0)
+        RP_STDOUT = 1;                  // write the report output to stdout
     B_PrintDest("dummy D");
 
     std::string filepath_ = check_filepath(filepath, FILE_REP, "execute_report", true);
@@ -11,8 +13,17 @@ void execute_report(const std::string& filepath, const std::string& parameters)
     std::string args = "\"" + filepath_ + "\" " + parameters;
     int success = B_ReportExec(to_char_array(args));
     W_close();
+
+    RP_STDOUT = 0;                      // reset global variable RP_STDOUT to default value (write to file)
+
     if(success != 0)
-        throw std::runtime_error("Execution of report " + filepath_ + " " + parameters + " has failed");
+    {
+        std::string error_msg = "Execution of report " + filepath_ + " " + parameters + " has failed";
+        std::string last_error = get_last_error();
+        if (!last_error.empty())
+            error_msg += ":\n" + last_error;
+        throw std::runtime_error(error_msg);
+    }
 }
 
 void execute_report(const std::string& filepath, const std::vector<std::string>& parameters)
@@ -23,9 +34,23 @@ void execute_report(const std::string& filepath, const std::vector<std::string>&
 
 void execute_report_line(const std::string& commands)
 {
+    if(RP_DEBUG == 0)
+        RP_STDOUT = 1;                  // force output of the report line to be written to stdout
+    B_PrintDest("dummy D");
+
     int success = B_ReportLine(to_char_array(commands), 0);
+    W_close();
+
+    RP_STDOUT = 0;                      // restore default output to file
+
     if(success != 0)
-        throw std::runtime_error("Execution of report command(s):\n" + commands + "\n\nhas failed");
+    {
+        std::string error_msg = "Execution of report command(s):\n" + commands + "\n\nhas failed";
+        std::string last_error = get_last_error();
+        if (!last_error.empty())
+            error_msg += ":\n" + last_error;
+        throw std::runtime_error(error_msg);
+    }
 }
 
 void execute_report_line(const std::vector<std::string>& commands)
