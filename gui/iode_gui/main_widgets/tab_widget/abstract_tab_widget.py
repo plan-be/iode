05132d2850d252b7c4f206b42183a3b0b912f2d1
alixdamman <ald@plan.be>
2025-10-14 21:16:51 +0200

    GUI: refactored class IodeTabWidget and implemented the dedicated      class DialogDatabaseSubset to support cascading tabs
    fix #1074

---
+++ gui/iode_gui/main_widgets/tab_widget/abstract_tab_widget.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QTabWidget
+
+from typing import List            
+from iode import IodeType
+
+
+class AbstractIodeTabWidget(QTabWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+    
+    def show_database_subset(self, iode_type: IodeType, obj_names: List[str] = None):
+        raise NotImplementedError("show_database_subset() method must be implemented in subclass")
