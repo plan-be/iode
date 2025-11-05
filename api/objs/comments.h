686f49cc50d73c29afcc3c115f817e3eb85f2777
alixdamman <ald@plan.be>
2025-11-05 19:59:10 +0100

    C API: replace KCPTR and KCVAL macros with inline functions

--- api/objs/comments.h
+++ api/objs/comments.h
@@ -1,13 +1,21 @@
 #pragma once
 
 #include "api/constants.h"
+#include "api/objs/kdb.h"
 
 /*----------------------- TYPEDEF ----------------------------*/
 
 // CMT = Comment (string)
 using CMT = char*;
 
-/*----------------------- MACROS ----------------------------*/
+/*----------------------- FUNCS ----------------------------*/
 
-#define KCPTR(name)         K_optr0(KC_WS, name)            // Returns a pointer to the CMT in swap (not allocated -> do not free)
-#define KCVAL(kdb, pos)     ((CMT )   K_oval0(kdb, pos))
+inline CMT KCPTR(const char* name) 
+{
+    return K_optr0(KC_WS, (char*) name);
+}
+
+inline CMT KCVAL(KDB* kdb, int pos) 
+{
+    return (CMT)K_oval0(kdb, pos);
+}
