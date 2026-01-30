e1ff7e6c9a57a1b9110c4c5354a4171a77863e30
alixdamman <ald@plan.be>
2026-01-30 16:41:29 +0100

    C++ TESTS: add tests when a GSample is expanded as column names            in computed tables

--- tests/test_cpp_api/test_computed_table.cpp
+++ tests/test_cpp_api/test_computed_table.cpp
@@ -38,6 +38,127 @@ protected:
 };
 
 
+TEST_F(ComputedTableTest, GSample)
+{
+    COLS* cls;
+    std::string column_name;
+    std::vector<std::string> cols_names;
+    std::vector<std::string> expected_cols_names;
+    char* expand  = "#s";
+    int nb_files;
+    int dim;
+    int res;
+    std::string gsample;
+
+    std::vector<std::string> variables = {"Q_I", "Q_F", "Q_I/Q_F", "KNFF[-1]"};
+    std::string title = "";
+    int nb_lines = 4;
+    std::string sample;
+    Table* ref_table = new Table(2, "", variables, false, false, false);
+
+    /*
+    // simple time series (current workspace) - 5 observations
+    gsample = "2000:5";
+    nb_files = 1;
+    cls = COL_cc((char*) gsample.c_str());
+    EXPECT_TRUE(cls != NULL);
+    EXPECT_EQ(cls->cl_nb, 5);
+    dim = COL_resize(ref_table, cls);
+    EXPECT_EQ(dim, 6);
+    res = COL_exec(ref_table, 5, cls);
+    EXPECT_EQ(res, 0);
+
+    expected_cols_names = {"00", "01", "02", "03", "04"};
+    for(int col=1; col < cls->cl_nb; col+=2)
+    {
+        column_name = std::string(COL_text(&cls->cl_cols[col], expand, nb_files));
+        cols_names.push_back(column_name);
+    }
+    EXPECT_EQ(cols_names.size(), expected_cols_names.size());
+    EXPECT_EQ(cols_names, expected_cols_names);
+
+    expected_cols_names.clear();
+    cols_names.clear();
+    COL_free_cols(cls); 
+
+    // two time series (current workspace) - 5 observations
+    gsample = "(2010;2010/2009):5";
+    nb_files = 1;
+    cls = COL_cc((char*) gsample.c_str());
+    EXPECT_TRUE(cls != NULL);
+    EXPECT_EQ(cls->cl_nb, 2 * 5);
+    dim = COL_resize(ref_table, cls);
+    EXPECT_EQ(dim, 11);
+    res = COL_exec(ref_table, 5, cls);
+    EXPECT_EQ(res, 0);
+
+    expected_cols_names = {"10", "10/09", "11", "11/10", "12", "12/11", "13", "13/12", 
+                           "14", "14/13"};
+    for(int col=1; col < cls->cl_nb; col+=2)
+    {
+        column_name = std::string(COL_text(&cls->cl_cols[col], expand, nb_files));
+        cols_names.push_back(column_name);
+    }
+    EXPECT_EQ(cols_names.size(), expected_cols_names.size());
+    EXPECT_EQ(cols_names, expected_cols_names);
+
+    expected_cols_names.clear();
+    cols_names.clear();
+    COL_free_cols(cls);
+
+    // simple time series (one extra file) - 5 observations
+    gsample = "2010[1;2]:5";
+    nb_files = 2;
+    cls = COL_cc((char*) gsample.c_str());
+    EXPECT_TRUE(cls != NULL);
+    EXPECT_EQ(cls->cl_nb, 2 * 5);
+    dim = COL_resize(ref_table, cls);
+    EXPECT_EQ(dim, 11);
+    res = COL_exec(ref_table, 5, cls);
+    EXPECT_EQ(res, 0);
+
+    expected_cols_names = {"10[1]", "10[2]", "11[1]", "11[2]", "12[1]", "12[2]",
+                           "13[1]", "13[2]", "14[1]", "14[2]"};
+    for(int col=1; col < cls->cl_nb; col+=2)
+    {
+        column_name = std::string(COL_text(&cls->cl_cols[col], expand, nb_files));
+        cols_names.push_back(column_name);
+    }
+    EXPECT_EQ(cols_names.size(), expected_cols_names.size());
+    EXPECT_EQ(cols_names, expected_cols_names);
+
+    expected_cols_names.clear();
+    cols_names.clear();
+    COL_free_cols(cls);
+    */
+
+    // list of patterns (one extra file) - 6 observations
+    gsample = "2000;2002;2004//2003;2006[1;2];2008[1+2];2010/2009[1^2]";
+    nb_files = 2;
+    cls = COL_cc((char*) gsample.c_str());
+    EXPECT_TRUE(cls != NULL);
+    // "2000;2002;2004//2003" + "2006[1;2]" + "2008[1+2];2010/2009[1^2]"
+    EXPECT_EQ(cls->cl_nb, 3 + 2 + 2);
+    dim = COL_resize(ref_table, cls);
+    EXPECT_EQ(dim, 8);
+    res = COL_exec(ref_table, 5, cls);
+    EXPECT_EQ(res, 0);
+
+    expected_cols_names = {"00[1]", "02[1]", "04//03[1]", "06[1]", "06[2]", 
+                           "08[1+2]", "10/09[1^2]"};
+    for(int col=1; col < cls->cl_nb; col+=2)
+    {
+        column_name = std::string(COL_text(&cls->cl_cols[col], expand, nb_files));
+        cols_names.push_back(column_name);
+    }
+    EXPECT_EQ(cols_names.size(), expected_cols_names.size());
+    EXPECT_EQ(cols_names, expected_cols_names); 
+    
+    expected_cols_names.clear(); 
+    cols_names.clear();
+    COL_free_cols(cls);
+}
+
 TEST_F(ComputedTableTest, BuildFromTable)
 {
     std::string gsample;
@@ -159,6 +280,54 @@ TEST_F(ComputedTableTest, BuildFromTable)
     EXPECT_DOUBLE_EQ(round(values[0] * 100) / 100, 1.1);
     values = graph_2_files.get_series_values(3, 1);
     EXPECT_DOUBLE_EQ(round(values[2] * 100) / 100, 1.1);
+
+    // list of patterns (one extra file) - 6 observations
+    gsample = "2000;2002;2004//2003;2006[1;2];2008[1+2];2010/2009[1^2]";
+    sample = "2000Y1:2010Y1";
+    ComputedTable table_multi_patterns(ref_table, gsample);
+    EXPECT_EQ(table_multi_patterns.get_nb_lines(), nb_lines);
+    EXPECT_EQ(table_multi_patterns.get_nb_columns(), 7);
+    EXPECT_EQ(table_multi_patterns.get_nb_files(), 2);
+    EXPECT_EQ(table_multi_patterns.get_list_files()[0], var_file);
+    EXPECT_EQ(table_multi_patterns.get_list_files()[1], ref_file);
+    EXPECT_EQ(table_multi_patterns.get_sample()->to_string(), sample);
+    EXPECT_EQ(table_multi_patterns.get_nb_files_ops(), 4);
+    EXPECT_EQ(table_multi_patterns.get_column_name(0), "00[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(1), "02[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(2), "04//03[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(3), "06[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(4), "06[2]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(5), "08[1+2]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(6), "10/09[1^2]");
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 0), 5495.21);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 1), 5748.78);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 2), 1.85);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 3), 6275.47);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 4), 6149.96);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 5), 13177.87);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 6), 1.74);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 0), 0.99);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 1), 1.01);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 2), 1.0);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 3), 1.05);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 4), 1.03);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 5), 2.13);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 6), 1.0);
+
+    ComputedTableGraph graph_multi_patterns(ref_table, gsample);
+    EXPECT_EQ(graph_multi_patterns.get_nb_series(), nb_lines * 4);
+    EXPECT_EQ(graph_multi_patterns.get_series_name(0, 0), "Output potentiel [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(0, 1), "Output potentiel [2]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(1, 0), "Stock de capital [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(1, 1), "Stock de capital [2]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(2, 0), "Intensité de capital [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(2, 1), "Intensité de capital [2]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(3, 0), "Productivité totale des facteurs [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(3, 1), "Productivité totale des facteurs [2]");
+    values = graph_multi_patterns.get_series_values(0, 0);
+    EXPECT_DOUBLE_EQ(round(values[0] * 100) / 100, 5495.21);
+    values = graph_multi_patterns.get_series_values(3, 0);
+    EXPECT_DOUBLE_EQ(round(values[0] * 100) / 100, 0.99);
 
     // wrong number of decimals (must in range [0, 99])
     EXPECT_THROW(ComputedTable(ref_table, gsample, -1), std::invalid_argument);
@@ -286,6 +455,54 @@ TEST_F(ComputedTableTest, BuildFromVariables)
     EXPECT_DOUBLE_EQ(round(values[0] * 100) / 100, 11293.85);
     values = graph_2_files.get_series_values(3, 1);
     EXPECT_DOUBLE_EQ(round(values[2] * 100) / 100, 11502.05);
+
+    // list of patterns (one extra file) - 6 observations
+    gsample = "2000;2002;2004//2003;2006[1;2];2008[1+2];2010/2009[1^2]";
+    sample = "2000Y1:2010Y1";
+    ComputedTable table_multi_patterns(&ref_table, gsample);
+    EXPECT_EQ(table_multi_patterns.get_nb_lines(), nb_lines);
+    EXPECT_EQ(table_multi_patterns.get_nb_columns(), 7);
+    EXPECT_EQ(table_multi_patterns.get_nb_files(), 2);
+    EXPECT_EQ(table_multi_patterns.get_list_files()[0], var_file);
+    EXPECT_EQ(table_multi_patterns.get_list_files()[1], ref_file);
+    EXPECT_EQ(table_multi_patterns.get_sample()->to_string(), sample);
+    EXPECT_EQ(table_multi_patterns.get_nb_files_ops(), 4);
+    EXPECT_EQ(table_multi_patterns.get_column_name(0), "2000[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(1), "2002[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(2), "2004//2003[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(3), "2006[1]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(4), "2006[2]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(5), "2008[1+2]");
+    EXPECT_EQ(table_multi_patterns.get_column_name(6), "2010/2009[1^2]"); 
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 0), 916.50);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 1), 949.49);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 2), 1.54);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 3), 1019.72);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 4), 999.32);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 5), 2111.62);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(0, 6), 0.89);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 0), 8083.55);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 1), 8647.94);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 2), 2.98);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 3), 9822.45);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 4), 9626.00);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 5), 21009.68);
+    EXPECT_DOUBLE_EQ(table_multi_patterns.get_value(3, 6), 2.82);
+
+    ComputedTableGraph graph_multi_patterns(&ref_table, gsample);
+    EXPECT_EQ(graph_multi_patterns.get_nb_series(), nb_lines * 4);
+    EXPECT_EQ(graph_multi_patterns.get_series_name(0, 0), "Q_I [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(0, 1), "Q_I [2]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(1, 0), "Q_F [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(1, 1), "Q_F [2]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(2, 0), "Q_I/Q_F [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(2, 1), "Q_I/Q_F [2]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(3, 0), "KNFF[-1] [1]");
+    EXPECT_EQ(graph_multi_patterns.get_series_name(3, 1), "KNFF[-1] [2]");
+    values = graph_multi_patterns.get_series_values(0, 0);
+    EXPECT_DOUBLE_EQ(round(values[0] * 100) / 100, 916.5);
+    values = graph_multi_patterns.get_series_values(3, 0);
+    EXPECT_DOUBLE_EQ(round(values[0] * 100) / 100, 8083.55);
 
     // wrong number of decimals (must in range [0, 99])
     EXPECT_THROW(ComputedTable(&ref_table, gsample, -1), std::invalid_argument);
