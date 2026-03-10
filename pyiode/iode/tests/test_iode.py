0fb812d3bced364c0faf1f0ff0a334003181689f
alixdamman <ald@plan.be>
2026-03-10 08:57:35 +0100

    PYTHON: removed functions and objects from the deprecated API from __init__.py
    fix #1144

--- pyiode/iode/tests/test_iode.py
+++ pyiode/iode/tests/test_iode.py
@@ -1342,3 +1342,54 @@ def test_to_frame_timeit():
     t = timeit.timeit(stmt, globals={"iode": iode, "variables": variables}, number=nb_times)
     logging.info(f"{stmt} (shape ({len(variables)}, {variables.nb_periods})): "
                  f"{t} ({nb_times} times) -> {t / nb_times} per loop")
+
+def test_deprecated_not_in_dir():
+    """Test that deprecated functions and objects are not visible in dir(iode)."""
+    import iode
+    
+    # List of deprecated functions that should NOT be in dir(iode)
+    deprecated_items = [
+        # time
+        'ws_sample_get', 'ws_sample_nb_periods', 'ws_sample_set', 'ws_sample_to_list', 'ws_sample_to_string',
+        
+        # IODE databases
+        'ws_clear', 'ws_clear_all', 'ws_clear_cmt', 'ws_clear_eqs', 'ws_clear_idt', 'ws_clear_lst', 'ws_clear_scl',
+        'ws_clear_tbl', 'ws_clear_var', 'ws_content', 'ws_content_cmt', 'ws_content_eqs', 'ws_content_idt',
+        'ws_content_lst', 'ws_content_scl', 'ws_content_tbl', 'ws_content_var', 'ws_load', 'ws_load_cmt', 'ws_load_eqs',
+        'ws_load_idt', 'ws_load_lst', 'ws_load_scl', 'ws_load_tbl', 'ws_load_var', 'ws_save', 'ws_save_cmt',
+        'ws_save_eqs', 'ws_save_idt', 'ws_save_lst', 'ws_save_scl', 'ws_save_tbl', 'ws_save_var',
+        'ws_htol', 'ws_htol_last', 'ws_htol_mean', 'ws_htol_sum', 'ws_ltoh', 'ws_ltoh_flow', 'ws_ltoh_stock',
+        
+        # IODE objects
+        'get_cmt', 'get_eqs', 'get_eqs_lec', 'get_idt', 'get_lst', 'get_scl', 'get_var', 'get_var_as_ndarray',
+        'set_cmt', 'set_eqs', 'set_idt', 'set_lst', 'set_scl', 'set_var',
+        'data_update', 'data_update_cmt', 'data_update_eqs', 'data_update_idt', 'data_update_lst', 'data_update_scl',
+        'data_update_var', 'delete_obj', 'delete_objects', 'delete_cmt', 'delete_eqs', 'delete_idt', 'delete_lst',
+        'delete_scl', 'delete_tbl', 'delete_var', 'idt_execute',
+        
+        # lec
+        'exec_lec',
+        
+        # estimation
+        'eqs_estimate',
+        
+        # model
+        'model_simulate', 'model_calc_scc', 'model_simulate_scc', 'model_simulate_save_parms',
+        'model_simulate_maxit', 'model_simulate_eps', 'model_simulate_relax', 'model_simulate_nb_passes',
+        'model_simulate_sort_algo', 'model_simulate_init_values', 'model_simulate_niter', 'model_simulate_norm',
+        
+        # Reports
+        'report_exec', 'reportline_exec',
+        
+        # compatibility with other libraries
+        'df_to_ws', 'ws_to_df', 'larray_to_ws', 'ws_to_larray', 'ws_load_var_to_larray', 'ws_sample_to_larray_axis'
+    ]
+    
+    # Get the current iode directory listing
+    iode_items = dir(iode)
+    
+    # Check that none of the deprecated items are visible
+    for deprecated_item in deprecated_items:
+        msg = f"Deprecated function '{deprecated_item}' should not be visible in dir(iode)"
+        assert deprecated_item not in iode_items, msg
+    
