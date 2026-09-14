674c1e8fff4596be8fe6db89fdc050765cf4a594
alixdamman <ald@plan.be>
2026-09-14 16:19:05 +0200

    C++ API: - move functions int initialize_columns() and extract_files_tbl()            to gsample.h          - delete source file k_print.cpp

--- api/gsample/c_calc.cpp
+++ api/gsample/c_calc.cpp
@@ -253,6 +253,33 @@ err:
 
 
 /**
+ *  Compiles a GSample into a std::vector<COL> struct and resizes std::vector<COL> according to the nb of cols in Table.
+ *
+ *  @param [in] std::shared_ptr<Table> tbl_ptr   table to compute
+ *  @param [in] const std::string&     gsample   GSample
+ *  @param [out] std::vector<COL>& columns  column definitions for computing the table
+ *  @return     int                              total number of columns for the computed table
+ *
+ */
+int initialize_columns(std::shared_ptr<Table> tbl_ptr, const std::string& gsample, std::vector<COL>& columns)
+{
+    if(!tbl_ptr)
+        return -1;
+
+    columns = compile_gsample((char*) gsample.c_str());
+    if(columns.empty())
+    {
+        std::string error_msg = "Illegal sample '" + gsample + "': syntax error";
+        error_manager.append_error(error_msg);
+        return -1;
+    }
+
+    int dim = resize_tbl_columns(*tbl_ptr, columns);
+    return dim;
+}
+
+
+/**
  *  After the compilation of a GSample into a std::vector<COL> structure, multiply the resulting number of COL's
  *  by the number of columns in the Table definition (usually 2).
  *
@@ -349,4 +376,45 @@ int execute_tbl_columns(const Table& tbl, const TableLine& line, std::vector<COL
         }
     }
     return 0;
+}
+
+
+/**
+ *  Retrieves the filenames used in the std::vector<COL> (from GSample) needed to print the special table line TABLE_LINE_FILES.
+ *
+ *  @param [in] const std::vector<COL>& columns  compiled GSample columns
+ *  @return     char**          NULL if one of the ref files is not loaded in global_ref_xxx
+ *                              table of filenames in the form "[<file number>] <filename>" if all files are in mem
+ */
+std::vector<std::string> extract_files_tbl(const std::vector<COL>& columns)
+{
+    std::vector<bool> files(K_MAX_FREF + 1, false);
+    for(const COL& column : columns)
+    {
+        files[column.cl_fnb[0]] = true;
+        files[column.cl_fnb[1]] = true;
+    }
+
+    KDBVariablesPtr kdb;
+    std::string filename;
+    std::vector<std::string> filenames;
+    for(int i = 1; i < K_MAX_FREF + 1; i++)
+    {
+        if(!files[i])
+            continue;
+
+        kdb = global_ref_var[i - 1];
+        if(!kdb)
+        {
+            std::string error_msg = "File " + std::to_string(i) + " not present";
+            error_manager.append_error(error_msg);
+            return std::vector<std::string>();
+        }
+
+        filename = "[" + std::to_string(i) + "] " + kdb->filepath;
+        std::replace(filename.begin(), filename.end(), '\\', '/');
+        filenames.push_back(filename);
+    }
+
+    return filenames;
 }
